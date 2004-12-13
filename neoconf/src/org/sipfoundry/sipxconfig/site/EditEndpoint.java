8a8949e9ce2e35f40416eefc363053cef8fab796
Douglas Hubler <douglas@hubler.us>
2004-12-13 12:17:59 +0000

    Fix neoconf web page runtime errors
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3258 a612230a-c5fa-0310-af8b-88eea846685b

--- neoconf/src/org/sipfoundry/sipxconfig/site/EditEndpoint.java
+++ neoconf/src/org/sipfoundry/sipxconfig/site/EditEndpoint.java
@@ -11,18 +11,12 @@
  */
 package org.sipfoundry.sipxconfig.site;
 
-import org.sipfoundry.sipxconfig.phone.Endpoint;
 
 /**
  * Editing Endpoints 
  */
 public class EditEndpoint {
     
-    /**
-     * delegate business object
-     */
-    private Endpoint m_endpoint = new Endpoint();
-       
     /**
      * Save endpoint into the database
      * 
@@ -31,31 +25,4 @@ public class EditEndpoint {
     public String save() {
         return "success";
     }
-    
-    /**
-     * TODO: Delegate to endpoint object, invesigate better way
-     * 
-     * @return
-     */
-    public String getSerialNumber() {
-        return m_endpoint.getSerialNumber();
-    }
-    
-    /**
-     * TODO: Delegate to endpoint object, invesigate better way
-     * 
-     * @return
-     */
-    public void setSerialNumber(String serialNumber) {
-        m_endpoint.setSerialNumber(serialNumber);
-    }
-    
-    public String getPhoneId() {
-        return m_endpoint.getPhoneId();
-    }
-    
-    public void setPhoneId(String phoneId) {
-        m_endpoint.setPhoneId(phoneId);
-    }
-       
 }
