676233b272a8b90ad0206cadc808e4e11ebe0558
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-01-23 02:26:47 +0000

    Editing custom dial rules - Tapestry component for common - model for Enum classes that can declared used as a bean - Border component added - defines common stylesheet - pages and components relevan to dial plan configuration moved to a separate directory
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3406 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ web/src/org/sipfoundry/sipxconfig/site/dialplan/DialingRuleCommon.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.site.dialplan;
+
+import org.apache.tapestry.BaseComponent;
+
+/**
+ * DialingRuleCommon
+ */
+public class DialingRuleCommon extends BaseComponent {
+
+}
---
+++ web/src/org/sipfoundry/sipxconfig/site/dialplan/EditCustomDialRule.java
@@ -0,0 +1,26 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.site.dialplan;
+
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.html.BasePage;
+
+/**
+ * EditCustomeDialRule
+ */
+public class EditCustomDialRule extends BasePage {
+    public void save(IRequestCycle cycle_) {
+    }
+    
+    public void cancel(IRequestCycle cycle_) {
+    }   
+}
