90bd88a1a33c5d8ca3f0f7cba88f414a50ba55b8
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-02-08 23:07:03 +0000

    staleLinkDetected function added - standard way of displaying a message about out-of-sync pages
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3502 a612230a-c5fa-0310-af8b-88eea846685b

--- web/src/org/sipfoundry/sipxconfig/components/TapestryUtils.java
+++ web/src/org/sipfoundry/sipxconfig/components/TapestryUtils.java
@@ -11,21 +11,26 @@
  */
 package org.sipfoundry.sipxconfig.components;
 
+import org.apache.tapestry.PageRedirectException;
+import org.apache.tapestry.html.BasePage;
+import org.apache.tapestry.valid.ValidationDelegate;
+import org.apache.tapestry.valid.ValidatorException;
 
 /**
  * Utility method for tapestry pages and components
  */
 public final class TapestryUtils {
-    
+
     /**
-     * restrict construction 
+     * restrict construction
      */
     private TapestryUtils() {
+        // intentionally empty
     }
-    
+
     /**
-     * Utility method to provide more descriptive unchecked exceptions for unmarshalling
-     * object from Tapestry service Parameters.
+     * Utility method to provide more descriptive unchecked exceptions for unmarshalling object
+     * from Tapestry service Parameters.
      * 
      * @throws IllegalArgumentException if parameter is not there is wrong class type
      */
@@ -33,16 +38,30 @@ public final class TapestryUtils {
         if (params == null || params.length < index) {
             throw new IllegalArgumentException("Missing parameter at position " + index);
         }
-         
+
         if (params[index] != null) {
             Class actualClass = params[index].getClass();
             if (!expectedClass.isAssignableFrom(actualClass)) {
-                throw new IllegalArgumentException("Object of class type " + expectedClass.getName() 
-                        + " was expected at position " + index + " but class type was " 
-                        + actualClass.getName());
+                throw new IllegalArgumentException("Object of class type "
+                        + expectedClass.getName() + " was expected at position " + index
+                        + " but class type was " + actualClass.getName());
             }
         }
-        
+
         return params[index];
+    }
+
+    /**
+     * Helper method to display standard "nice" stale link message
+     * 
+     * @param page page on which stale link is discovered
+     * @param validatorName name of the validator delegate bean used to record validation errors
+     */
+    public static void staleLinkDetected(BasePage page, String validatorName) {
+        ValidationDelegate validator = (ValidationDelegate) page.getBeans()
+                .getBean(validatorName);
+        validator.setFormComponent(null);
+        validator.record(new ValidatorException("The page is out of date. Please refresh."));
+        throw new PageRedirectException(page);
     }
 }
