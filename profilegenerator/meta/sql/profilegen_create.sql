1457a2276e17fe9325b1e9090a2810348517e872
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-02-17 16:48:38 +0000

    Appended changes from v8_mods-2.sql
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3544 a612230a-c5fa-0310-af8b-88eea846685b

--- profilegenerator/meta/sql/profilegen_create.sql
+++ profilegenerator/meta/sql/profilegen_create.sql
@@ -367,4 +367,31 @@ alter table line
 add constraint fk_line_3
 foreign key (user_id) references users (id);
 
+/** 
+ *   W A R N I N G  :   
+ *   exact copy maintained in v8_mods_2.sql
+ */
+
+
+/** 
+ * R I N G
+ */
+create table ring(
+  ring_id int4 not null primary key,
+  user_id int4 not null,
+  number varchar(256),
+  expiration int4,
+  position int4 not null,
+  ring_type varchar(40)
+);
+
+create unique index idx_ring_user_ring on ring (user_id, ring_id);
+create index idx_ring_user on ring (user_id);
+
+create sequence ring_seq;
+
+alter table ring
+add constraint fk_user_1
+foreign key (user_id) references users (id);
+
 
