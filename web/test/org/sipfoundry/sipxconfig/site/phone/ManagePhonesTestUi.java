3b5d0709e6cefc587d012f737578116cd73348a4
Douglas Hubler <douglas@hubler.us>
2005-02-22 14:01:15 +0000

    actually add manage phones unittest this time
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3562 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ web/test/org/sipfoundry/sipxconfig/site/phone/ManagePhonesTestUi.java
@@ -0,0 +1,52 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2004 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2004 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.site.phone;
+
+import junit.framework.Test;
+import net.sourceforge.jwebunit.WebTestCase;
+
+import org.sipfoundry.sipxconfig.site.SiteTestHelper;
+
+public class ManagePhonesTestUi extends WebTestCase {
+
+    public static Test suite() throws Exception {
+        return SiteTestHelper.webTestSuite(NewPhoneTestUi.class);
+    }
+
+    protected void setUp() throws Exception {
+        getTestContext().setBaseUrl(SiteTestHelper.getBaseUrl());
+        beginAt("/");
+        clickLink("ManagePhones");  
+    }
+    
+    public void testGenerateProfiles() {
+        //seedNewPhone();
+        
+        setFormElement("selectedRow", "true");
+        clickButton("phone:sendProfiles");
+        // present if no exception
+        assertLinkPresent("NewPhone");
+    }
+    
+    private void seedNewPhone() {
+        clickLink("NewPhone");
+        setFormElement("serialNumber", "000000000000");
+        setFormElement("phoneModel", "1");
+        clickButton("phone:ok");
+        String[][] table = new String[][] {
+            { "000000000000", "", "SoundPoint IP 500" },                
+        };
+        assertTextInTable("phone:list", table[0]);    
+
+        // verify files were generated        
+    }
+}
