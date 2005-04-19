a9c6f38c2157301a165beb38f8b95a1f7aea8e44
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-04-19 15:20:00 +0000

    Empty call groups list page added
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3799 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ web/src/org/sipfoundry/sipxconfig/site/admin/ListCallGroups.java
@@ -0,0 +1,24 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.site.admin;
+
+import org.apache.tapestry.event.PageEvent;
+import org.apache.tapestry.event.PageRenderListener;
+import org.apache.tapestry.html.BasePage;
+
+public abstract class ListCallGroups extends BasePage implements PageRenderListener {
+
+    public void pageBeginRender(PageEvent event_) {
+        // initialize properties        
+    }
+
+}
--- web/test/org/sipfoundry/sipxconfig/site/admin/ListCallGroupsTestUi.java
+++ web/test/org/sipfoundry/sipxconfig/site/admin/ListCallGroupsTestUi.java
@@ -12,28 +12,24 @@
 package org.sipfoundry.sipxconfig.site.admin;
 
 import junit.framework.Test;
-import net.sourceforge.jwebunit.WebTestCase;
 
 import org.sipfoundry.sipxconfig.site.SiteTestHelper;
 
-public class BackupPageTestUi extends WebTestCase {
+import net.sourceforge.jwebunit.WebTestCase;
+
+public class ListCallGroupsTestUi extends WebTestCase {
     public static Test suite() throws Exception {
-        return SiteTestHelper.webTestSuite(BackupPageTestUi.class);
+        return SiteTestHelper.webTestSuite(ListCallGroupsTestUi.class);
     }
 
     public void setUp() {
         getTestContext().setBaseUrl(SiteTestHelper.getBaseUrl());
         SiteTestHelper.home(getTester());
         clickLink("BackupPage");
-    }    
+    }
     
-    /**
-     * Does not check if backup was successful - just checks if no Tapestry exceptions show up
-     *
-     */
     public void testDisplay() {
         SiteTestHelper.assertNoException(getTester());
-        clickButton("backup:now");
-        SiteTestHelper.assertNoException(getTester());
     }
+    
 }
