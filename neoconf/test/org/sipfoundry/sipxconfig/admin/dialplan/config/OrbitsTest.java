8a463ceac20533f6b74ad7859d4d866f62114aef
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-05-10 18:51:35 +0000

    XCF-324 generating orbits.xml implemented
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3889 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ neoconf/test/org/sipfoundry/sipxconfig/admin/dialplan/config/OrbitsTest.java
@@ -0,0 +1,62 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.admin.dialplan.config;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.custommonkey.xmlunit.XMLTestCase;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.sipfoundry.sipxconfig.admin.callgroup.ParkOrbit;
+
+public class OrbitsTest extends XMLTestCase {
+    private Collection m_parkOrbits;
+
+    private static String[][] DATA = {
+        {
+            "disable", "101", "x.wav", "this one should not be generated"
+        }, {
+            "sales", "400", "basia.wav", "test description"
+        }, {
+            "CEO orbit", "179", "marketing.wav", ""
+        }
+    };
+
+    protected void setUp() throws Exception {
+        XMLUnit.setIgnoreWhitespace(true);
+
+        m_parkOrbits = new ArrayList(DATA.length);
+        for (int i = 0; i < DATA.length; i++) {
+            ParkOrbit orbit = new ParkOrbit();
+            orbit.setName(DATA[i][0]);
+            orbit.setExtension(DATA[i][1]);
+            orbit.setMusicOnHold(DATA[i][2]);
+            orbit.setDescription(DATA[i][3]);
+            // the first one is disabled
+            // the remaining orbits will be enabled
+            orbit.setEnabled(i > 0);
+            m_parkOrbits.add(orbit);
+        }
+    }
+
+    public void testGenerate() throws Exception {
+        Orbits orbits = new Orbits();
+        orbits.setAudioDirectory("/var/sipxdata/parkserver/music");
+        orbits.generate(m_parkOrbits);
+        String generatedXml = orbits.getFileContent();
+        InputStream referenceXml = OrbitsTest.class.getResourceAsStream("orbits.test.xml");
+        assertXMLEqual(new InputStreamReader(referenceXml), new StringReader(generatedXml));
+    }
+}
