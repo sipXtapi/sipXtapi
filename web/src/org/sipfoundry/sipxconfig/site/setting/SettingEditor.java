a12337292b7e7aa7f633d1362eaa6999416f63a6
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-05-20 14:48:21 +0000

    A new component - SettingEditor - for rendering and editing a single setting.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/old/sipXconfig/main@3937 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ web/src/org/sipfoundry/sipxconfig/site/setting/SettingEditor.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.site.setting;
+
+import org.apache.tapestry.BaseComponent;
+
+public class SettingEditor extends BaseComponent {
+
+}
