4d6b2e1d2d7fda17c9ecfa3b19e5de53999ee39d
Dong Hsu <dhsu@pingtel.com>
2005-07-12 17:59:53 +0000

    Created the UtlContainable wrappers for bool and OsDateTime.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@4823 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXportLib/src/utl/UtlDateTime.cpp
@@ -0,0 +1,98 @@
+// $Id$
+//
+// Copyright (C) 2005 SIPfoundry Inc.
+// License by SIPfoundry under the LGPL license.
+// 
+// Copyright (C) 2005 Pingtel Corp.
+// Licensed to SIPfoundry under a Contributor Agreement.
+//
+// $$
+//////////////////////////////////////////////////////////////////////////////
+
+// SYSTEM INCLUDES
+
+
+// APPLICATION INCLUDES
+#include "utl/UtlDateTime.h"
+
+// EXTERNAL FUNCTIONS
+// EXTERNAL VARIABLES
+// CONSTANTS
+// STATIC VARIABLE INITIALIZATIONS
+UtlContainableType UtlDateTime::TYPE = "UtlDateTime" ;
+
+/* //////////////////////////// PUBLIC //////////////////////////////////// */
+
+/* ============================ CREATORS ================================== */
+
+// Constructor accepting an optional default value.
+UtlDateTime::UtlDateTime(OsDateTime value)
+{
+    mTime = value ;
+} 
+
+
+// Copy constructor
+
+
+
+// Destructor
+UtlDateTime::~UtlDateTime()
+{
+}
+
+/* ============================ MANIPULATORS ============================== */
+
+void UtlDateTime::setTime(const OsDateTime& value)
+{
+    mTime = value ;
+}
+
+/* ============================ ACCESSORS ================================= */
+
+void UtlDateTime::getTime(OsDateTime& value) const 
+{
+    value = mTime ; 
+}
+
+
+unsigned UtlDateTime::hash() const
+{
+   return mTime.getSecsSinceEpoch() ; 
+}
+
+
+UtlContainableType UtlDateTime::getContainableType() const
+{
+    return UtlDateTime::TYPE ;
+}
+
+/* ============================ INQUIRY =================================== */
+
+int UtlDateTime::compareTo(UtlContainable const * inVal) const
+{
+   int result ; 
+   
+   if (inVal->isInstanceOf(UtlDateTime::TYPE))
+    {
+        UtlDateTime* temp = (UtlDateTime*)inVal ;
+        OsDateTime inTime;
+        temp->getTime(inTime);
+        int tempValue = inTime.getSecsSinceEpoch() ;
+        result = tempValue - mTime.getSecsSinceEpoch() ;
+    }
+    else
+    {
+        result = 1 ; 
+    }
+
+    return result ;
+}
+
+
+
+/* //////////////////////////// PROTECTED ///////////////////////////////// */
+
+/* //////////////////////////// PRIVATE /////////////////////////////////// */
+
+/* ============================ FUNCTIONS ================================= */
