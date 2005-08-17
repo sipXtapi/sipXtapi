aa90aceb0693c74a579b27d176642216b4f57892
Nils Ohlmeier <nils@iptel.org>
2005-08-17 20:01:34 +0000

    style fixes + removed two obsoleted settings
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main/sipXconfig@4885 a612230a-c5fa-0310-af8b-88eea846685b

--- neoconf/src/org/sipfoundry/sipxconfig/phone/snom/SnomModel.java
+++ neoconf/src/org/sipfoundry/sipxconfig/phone/snom/SnomModel.java
@@ -1,43 +1,54 @@
+/*
+ *
+ *
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 snom technology AG
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
 package org.sipfoundry.sipxconfig.phone.snom;
 
 import org.apache.commons.lang.enums.Enum;
 
 public final class SnomModel extends Enum {
 
-	public static final SnomModel MODEL_360 = new SnomModel("snom360", 12);
+    public static final SnomModel MODEL_360 = new SnomModel("snom360", 12);
 
-	public static final SnomModel MODEL_320 = new SnomModel("snom320", 12);
+    public static final SnomModel MODEL_320 = new SnomModel("snom320", 12);
 
-	public static final SnomModel UNKNOWN = new SnomModel("unknown", 0);
+    public static final SnomModel UNKNOWN = new SnomModel("unknown", 0);
 
-	private String m_modelId;
+    private String m_modelId;
 
-	private int m_maxLines;
+    private int m_maxLines;
 
-	private SnomModel(String modelId, int maxLines) {
-		super(modelId);
-		setModelId(modelId);
-		setMaxLines(maxLines);
-	}
+    private SnomModel(String modelId, int maxLines) {
+        super(modelId);
+        setModelId(modelId);
+        setMaxLines(maxLines);
+    }
 
-	public static SnomModel getModel(String modelId) {
-		SnomModel model = (SnomModel) getEnumMap(SnomModel.class).get(modelId);
-		return model == null ? UNKNOWN : model;
-	}
+    public static SnomModel getModel(String modelId) {
+        SnomModel model = (SnomModel) getEnumMap(SnomModel.class).get(modelId);
+        return model == null ? UNKNOWN : model;
+    }
 
-	public int getMaxLines() {
-	  return m_maxLines;
-	}
+    public int getMaxLines() {
+        return m_maxLines;
+    }
 
-	public void setMaxLines(int maxLines) {
-		m_maxLines = maxLines;
-	}
+    public void setMaxLines(int maxLines) {
+        m_maxLines = maxLines;
+    }
 
-	public String getModelId() {
-		return m_modelId;
-	}
+    public String getModelId() {
+        return m_modelId;
+    }
 
-	public void setModelId(String modelId) {
-		m_modelId = modelId;
-	}
+    public void setModelId(String modelId) {
+        m_modelId = modelId;
+    }
 }
--- neoconf/src/org/sipfoundry/sipxconfig/phone/snom/SnomPhone.java
+++ neoconf/src/org/sipfoundry/sipxconfig/phone/snom/SnomPhone.java
@@ -1,3 +1,14 @@
+/*
+ *
+ *
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 snom technology AG
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
 package org.sipfoundry.sipxconfig.phone.snom;
 
 import java.io.File;
@@ -16,71 +27,70 @@ import org.sipfoundry.sipxconfig.setting.XmlModelBuilder;
 
 public class SnomPhone extends GenericPhone {
 
-	public static final String FACTORY_ID = "snom";
+    public static final String FACTORY_ID = "snom";
 
-	public SnomPhone() {
-	  setLineFactoryId(SnomLine.FACTORY_ID);
-	  setPhoneTemplate("snom/snom.vm");
-	  setModelFile("snom/phone.xml");
-	}
+    public SnomPhone() {
+        setLineFactoryId(SnomLine.FACTORY_ID);
+        setPhoneTemplate("snom/snom.vm");
+        setModelFile("snom/phone.xml");
+    }
 
-	public Setting getSettingModel() {
-	  File sysDir = new File(getPhoneContext().getSystemDirectory());
-	  File modelDefsFile = new File(sysDir, getModelFile());
-	  Setting all = new XmlModelBuilder(sysDir).buildModel(modelDefsFile);
-	  Setting model = all.getSetting(getModel().getModelId());
-	  return model;
-	}
+    public Setting getSettingModel() {
+        File sysDir = new File(getPhoneContext().getSystemDirectory());
+        File modelDefsFile = new File(sysDir, getModelFile());
+        Setting all = new XmlModelBuilder(sysDir).buildModel(modelDefsFile);
+        Setting model = all.getSetting(getModel().getModelId());
+        return model;
+    }
 
-	public String getPhoneFilename() {
-	  String phoneFilename = getModel() + "-" + getPhoneData().getSerialNumber().toUpperCase();
-	  return getWebDirectory() + "/" + phoneFilename + ".htm";
-	}
+    public String getPhoneFilename() {
+        String phoneFilename = getModel() + "-" + getPhoneData().getSerialNumber().toUpperCase();
+        return getWebDirectory() + "/" + phoneFilename + ".htm";
+    }
 
-	public String getPhoneFactoryIds() {
-	  return FACTORY_ID;
-	}
+    public String getPhoneFactoryIds() {
+        return FACTORY_ID;
+    }
 
-	public SnomModel getModel() {
-	  return SnomModel.getModel(getPhoneData().getFactoryId());
-	}
+    public SnomModel getModel() {
+        return SnomModel.getModel(getPhoneData().getFactoryId());
+    }
 
-	public int getMaxLineCount() {
-	  return getModel().getMaxLines();
-	}
+    public int getMaxLineCount() {
+        return getModel().getMaxLines();
+    }
 
-	public Object getAdapter(Class c) {
-	  Object o = null;
-	  if (c == PhoneSettings.class) {
-		SettingBeanAdapter adapter = new SettingBeanAdapter(c);
-		adapter.setSetting(getSettings());
-		o = adapter.getImplementation();
-	  }
-	  else {
-		o = super.getAdapter(c);
-	  }
-	  return o;
-	}
+    public Object getAdapter(Class c) {
+        Object o = null;
+        if (c == PhoneSettings.class) {
+            SettingBeanAdapter adapter = new SettingBeanAdapter(c);
+            adapter.setSetting(getSettings());
+            o = adapter.getImplementation();
+        } else {
+            o = super.getAdapter(c);
+        }
+        return o;
+    }
 
-	public Collection getProfileLines() {
-	  ArrayList linesSettings = new ArrayList(getMaxLineCount());
+    public Collection getProfileLines() {
+        ArrayList linesSettings = new ArrayList(getMaxLineCount());
 
-	  Collection lines = getLines();
-	  int i = 0;
-	  Iterator ilines = lines.iterator();
-	  for (; ilines.hasNext() && (i < getMaxLineCount()); i++) {
-		linesSettings.add(((Line) ilines.next()).getSettings());
-	  }
+        Collection lines = getLines();
+        int i = 0;
+        Iterator ilines = lines.iterator();
+        for (; ilines.hasNext() && (i < getMaxLineCount()); i++) {
+            linesSettings.add(((Line) ilines.next()).getSettings());
+        }
 
-	  for (; i < getMaxLineCount(); i++) {
-		SnomLine line = new SnomLine();
-		line.setPhone(this);
-		line.setLineData(new LineData());
-		line.getLineData().setPosition(i);
-		line.setDefaults(new PhoneDefaults());
-		linesSettings.add(line.getSettings());
-	  }
+        for (; i < getMaxLineCount(); i++) {
+            SnomLine line = new SnomLine();
+            line.setPhone(this);
+            line.setLineData(new LineData());
+            line.getLineData().setPosition(i);
+            line.setDefaults(new PhoneDefaults());
+            linesSettings.add(line.getSettings());
+        }
 
-	  return linesSettings;
-	}
+        return linesSettings;
+    }
 }
