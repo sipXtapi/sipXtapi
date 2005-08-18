a821b205429d91c8ab4d4f6ecd03b1394fbe1dbd
Douglas Hubler <douglas@hubler.us>
2005-08-18 20:29:55 +0000

    FIX BUILD: Resolve subversion evil twin issue, File was not shown as modified however it had been. This file should have been commited as part of r4886 merge
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main/sipXconfig@4888 a612230a-c5fa-0310-af8b-88eea846685b

--- web/src/org/sipfoundry/sipxconfig/site/phone/EditPhoneForm.java
+++ web/src/org/sipfoundry/sipxconfig/site/phone/EditPhoneForm.java
@@ -12,26 +12,7 @@
 package org.sipfoundry.sipxconfig.site.phone;
 
 import org.apache.tapestry.BaseComponent;
-import org.apache.tapestry.event.PageEvent;
-import org.apache.tapestry.event.PageRenderListener;
-import org.apache.tapestry.form.IPropertySelectionModel;
-import org.sipfoundry.sipxconfig.components.MapSelectionModel;
-import org.sipfoundry.sipxconfig.phone.PhoneContext;
 
-/**
- * Editing a phone is reused for creating new phones and editing existing ones
- */
-public abstract class EditPhoneForm extends BaseComponent implements PageRenderListener {
-
-    /**
-     * all the phoneModels available to the system ready for UI selection 
-     */
-    public abstract void setPhoneSelectionModel(IPropertySelectionModel phoneModels);
-     
-    public abstract PhoneContext getPhoneContext();
-        
-    public void pageBeginRender(PageEvent event_) {
-        PhoneContext phoneContext = getPhoneContext();
-        setPhoneSelectionModel(new MapSelectionModel(phoneContext.getPhoneFactoryIds()));
-    }    
+public abstract class EditPhoneForm extends BaseComponent {
+    
 }
