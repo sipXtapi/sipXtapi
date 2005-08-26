685ff63d19641e98f0be35d0efd1445d8fdc0423
Dale Worley <dworley@pingtel.com>
2005-08-26 19:35:42 +0000

    Improve copyright notices.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@4916 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXtackLib/siplog2siptrace/main.cpp
+++ sipXtackLib/siplog2siptrace/main.cpp
@@ -2,8 +2,8 @@
 //
 // Copyright (c) PingTel Corp. (work in progress)
 //
-// This is an unpublished work containing PingTel Corporation's confidential
-// and proprietary information.  Disclosure, use or reproduction without
+// This is an unpublished work containing PingTel Corporation's
+// proprietary information.  Disclosure, use or reproduction without
 // written authorization of PingTel Corp. is prohibited.
 //
 //! author="Daniel Petrie"
