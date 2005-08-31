4e6a384fc53713a4a6a2300490336a2cbabb0620
Douglas Hubler <douglas@hubler.us>
2005-08-31 13:13:37 +0000

    Apply patch from Hannu regardin cisco ata and grandstream updates
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main/sipXconfig@4944 a612230a-c5fa-0310-af8b-88eea846685b

--- neoconf/src/org/sipfoundry/sipxconfig/phone/grandstream/GrandstreamPhone.java
+++ neoconf/src/org/sipfoundry/sipxconfig/phone/grandstream/GrandstreamPhone.java
@@ -15,11 +15,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.apache.commons.lang.StringUtils;
+
 import org.sipfoundry.sipxconfig.phone.Line;
 import org.sipfoundry.sipxconfig.phone.LineSettings;
 import org.sipfoundry.sipxconfig.phone.Phone;
 import org.sipfoundry.sipxconfig.phone.PhoneSettings;
+import org.sipfoundry.sipxconfig.setting.Setting;
 import org.sipfoundry.sipxconfig.setting.SettingBeanAdapter;
+import org.sipfoundry.sipxconfig.setting.SettingFilter;
+import org.sipfoundry.sipxconfig.setting.SettingUtil;
 
 /**
  * Support for Grandstream BudgeTone / HandyTone
@@ -30,6 +35,26 @@ public class GrandstreamPhone extends Phone {
 
     public static final String SIP = "sip";
 
+    public static final String EMPTY = "";
+
+    public static final int KOLME = 3;
+
+    private static final SettingFilter S_REALSETTINGS = new SettingFilter() {
+            public boolean acceptSetting(Setting root, Setting setting) {
+                boolean firstGeneration = setting.getParentPath().equals(root.getPath());
+                boolean isLeaf = setting.getValues().isEmpty();            
+                boolean isVirtual = (setting.getName().startsWith("_"));            
+                return firstGeneration && isLeaf && !isVirtual;
+            }
+        };        
+
+    private static final SettingFilter S_IPSETTINGS = new SettingFilter() {
+            public boolean acceptSetting(Setting root_, Setting setting) {
+                boolean isLeaf = setting.getValues().isEmpty();            
+                boolean isBitmapped = (setting.getName().startsWith("__"));            
+                return  isLeaf && isBitmapped;
+            }
+        };        
     public GrandstreamPhone() {
         super(BEAN_ID);
         init();
@@ -104,5 +129,46 @@ public class GrandstreamPhone extends Phone {
 
         return linesSettings;
     }
-    
+
+    public void generateProfiles() {
+        splitIpSettings();
+        super.generateProfiles();
+    }
+
+    public Collection getRealSettings(Setting setting) {
+        return SettingUtil.filter(S_REALSETTINGS, setting);
+    }
+
+    public Collection getIpSettings() {
+        return SettingUtil.filter(S_IPSETTINGS, getSettings());
+    }
+
+    private void splitIpSettings() {
+        Collection bitmaps = getIpSettings();
+        Iterator bmi = bitmaps.iterator();
+        while (bmi.hasNext()) {
+            Setting bset = (Setting) bmi.next();
+            String bname = bset.getName();
+            int bpoint = bname.indexOf('-');
+
+            if (bpoint < KOLME || bpoint == bname.length() - 1) {
+                continue;
+            }
+
+            Setting btgt = getSettings().getSetting(bset.getParentPath().substring(1));
+            
+            int bofs = Integer.parseInt(bname.substring(bpoint + 1));
+            
+            String ipa = bset.getValue();
+            String[] ipn = {EMPTY, EMPTY, EMPTY, EMPTY};
+
+            if (!StringUtils.isBlank(ipa)) {
+                ipn = ipa.split("\\.");
+            }
+
+            for (int i = 0; i < ipn.length; i++) {
+                btgt.getSetting("P" + Integer.toString(bofs + i)).setValue(ipn[i]);
+            }
+        }
+    }
 }
