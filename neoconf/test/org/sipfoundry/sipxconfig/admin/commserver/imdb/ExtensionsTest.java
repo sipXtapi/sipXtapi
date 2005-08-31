66355ffa40ac1282ebf15cf7f7beb47d25b3ae79
Walter Gillett <wgillett@pingtel.com>
2005-08-31 22:47:20 +0000

    Finish fixing XCF-560
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main/sipXconfig@4951 a612230a-c5fa-0310-af8b-88eea846685b

--- neoconf/test/org/sipfoundry/sipxconfig/admin/commserver/imdb/ExtensionsTest.java
+++ neoconf/test/org/sipfoundry/sipxconfig/admin/commserver/imdb/ExtensionsTest.java
@@ -72,8 +72,7 @@ public class ExtensionsTest extends XMLTestCase {
         control.verify();
     }
     
-    /** FIXME XCF-560 */
-    public void _testGenerate() throws Exception {
+    public void testGenerate() throws Exception {
         MockControl control = MockControl.createControl(CoreContext.class);
         CoreContext coreContext = (CoreContext) control.getMock();
         coreContext.getDomainName();
