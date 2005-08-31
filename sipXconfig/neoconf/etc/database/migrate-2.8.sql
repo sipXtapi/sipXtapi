fe340754bdf43af53443040b8b8a3fd86d2db64b
Douglas Hubler <douglas@hubler.us>
2005-08-31 16:26:17 +0000

    cleanup user group tree migration and add phone group tree migration
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@4947 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/neoconf/etc/database/migrate-2.8.sql
+++ sipXconfig/neoconf/etc/database/migrate-2.8.sql
@@ -1,3 +1,8 @@
+-- notes:
+--  Dont use apostrophes in comments
+--  Dont use variable names that match column names when setting variable (see my_* usages)
+--  primative logging by raising notices. level debug is useless w/o modifications to postgres.conf
+
 -- todo, bail on any error
 
 delete from phone_group;
@@ -20,17 +25,17 @@ begin
 
   for usrgrp in select * from dblink(''select id, name from user_groups'') as (id int, name text) loop
 
-    raise debug ''importing user group %...'', usrgrp.name;
+    raise notice ''importing user group %...'', usrgrp.name;
  
     insert into value_storage (value_storage_id) values (usrgrp.id);
 
-    -- todo: weights ought to be the lower the depth, the higher to weight
+    -- todo weights ought to be the lower the depth, the higher to weight
     -- how do you find the depth though?
 
     insert into group_storage (group_id, name, weight, resource)
         values (usrgrp.id, usrgrp.name, nextval(''group_weight_seq''), ''user'');
 
-    -- todo: user permissions
+    -- todo user permissions
     
   end loop; 
 
@@ -46,14 +51,13 @@ end;
 create or replace function migrate_users() returns integer as '
 declare
   usr record;
-  grp record;
   next_id int;
 begin
 
   -- avoid SDS and superadmin users
   for usr in select * from dblink(''select id, first_name, password, last_name, display_id, extension, ug_id from users where ug_id is not null'') as (id int, first_name text, password text, last_name text, display_id text, extension text, ug_id int) loop
 
-   raise debug ''importing user %...'', usr.display_id;
+   raise notice ''importing user %...'', usr.display_id;
 
    insert into users (user_id, first_name, pintoken, last_name, user_name)
       values (usr.id, usr.first_name, usr.password, usr.last_name, usr.display_id);
@@ -65,6 +69,8 @@ begin
 
    -- todo update user_seq
 
+   -- todo add all groups that was parent to current group
+
   end loop; 
 
   next_id := max(user_id) + 1 from users;
@@ -74,24 +80,64 @@ begin
 end;
 ' language plpgsql;
 
+
 -- U S E R  G R O U P  H I E R A R C H Y
-create or replace function migrate_phone_groups() returns integer as '
+create or replace function migrate_user_group_tree() returns integer as '
 declare
+  not_supported_notice record;
 begin
-  -- add user to 1st parent group
-  for grp in select * from dblink(''select u.id, g.parent_ug_id from users u, user_groups g where u.ug_id = g.id and g.parent_ug_id is not null'') as (user_id int, group_id int) loop
-    insert into user_group (user_id, group_id) values (grp.user_id, grp.group_id);
-  end loop;
 
-  -- add user to 2nd parent group
-  for grp in select * from dblink(''select u.id, g.parent_ug_id from users u, user_groups g, user_groups g1 where u.ug_id = g1.id and g1.parent_ug_id = g.id and g.parent_ug_id is not null'') as (user_id int, group_id int) loop
-    insert into user_group (user_id, group_id) values (grp.user_id, grp.group_id);
-  end loop;
+  -- level 1
+  perform insert_users_into_groups(''
+  select 
+    u.id, g1.id from users u, user_groups g, user_groups g1
+  where 
+    u.ug_id = g.id and g.parent_ug_id = g1.id'');
 
-  -- todo add 3rd and 4th level user groups
+  -- level 2
+  perform insert_users_into_groups(''
+  select 
+    u.id, g2.id from users u, user_groups g, user_groups g1, user_groups g2
+  where 
+    u.ug_id = g.id and g.parent_ug_id = g1.id and g1.parent_ug_id = g2.id'');
+
+  -- level 3
+  perform insert_users_into_groups(''
+  select 
+    u.id, g3.id from users u, user_groups g, user_groups g1, user_groups g2, user_groups g3
+  where 
+    u.ug_id = g.id and g.parent_ug_id = g1.id and g1.parent_ug_id = g2.id and 
+    g2.parent_ug_id = g3.id'');
+
+  -- level 4 and above not supported
+  select into not_supported_notice * from dblink(''
+  select 
+    u.id, g4.id from users u, user_groups g, user_groups g1, user_groups g2, user_groups g3, user_groups g4
+  where 
+    u.ug_id = g.id and g.parent_ug_id = g1.id and g1.parent_ug_id = g2.id and 
+    g2.parent_ug_id = g3.id and g3.parent_ug_id = g4.id'') 
+  as (user_id int, group_id int);
+  if found then
+    raise exception ''cannot support group levels 4 parents or deeper'';
+  end if;
+
+  return 1;
 end;
 ' language plpgsql;
 
+-- help function for user tree function
+create or replace function insert_users_into_groups(varchar) returns integer as '
+declare
+  user_select alias for $1;
+  grp record;
+begin
+  for grp in select * from dblink(user_select) as (user_id int, group_id int) loop
+    insert into user_group (user_id, group_id) values (grp.user_id, grp.group_id);
+  end loop;
+
+  return 1;
+end;
+' language plpgsql;
 
 -- P H O N E  G R O U P S
 create or replace function migrate_phone_groups() returns integer as '
@@ -100,6 +146,8 @@ declare
   next_id int;
 begin
 
+  -- cannot insert phone group id directly into new database, ids would conflict with
+  -- user groups as the share same tables
   create temporary table phone_group_migration (pds_group_id int not null, group_id int not null);
 
   for phonegrp in select * from dblink(''select id, name from phone_groups'') as (id int, name text) loop
@@ -118,6 +166,68 @@ begin
   return 1;
 end;
 ' language plpgsql;
+
+-- P H O N E   G R O U P  H I E R A R C H Y
+create or replace function migrate_phone_group_tree() returns integer as '
+declare
+  not_supported_notice record;
+begin
+
+  -- level 1
+  perform insert_phones_into_groups(''
+  select 
+    p.id, g1.id from logical_phones p, phone_groups g, phone_groups g1
+  where 
+    p.pg_id = g.id and g.parent_pg_id = g1.id'');
+
+  -- level 2
+  perform insert_phones_into_groups(''
+  select 
+    p.id, g2.id from logical_phones p, phone_groups g, phone_groups g1, phone_groups g2
+  where 
+    p.pg_id = g.id and g.parent_pg_id = g1.id and g1.parent_pg_id = g2.id'');
+
+  -- level 3
+  perform insert_phones_into_groups(''
+  select 
+    p.id, g3.id from logical_phones p, phone_groups g, phone_groups g1, phone_groups g2, phone_groups g3
+  where 
+    p.pg_id = g.id and g.parent_pg_id = g1.id and g1.parent_pg_id = g2.id and
+    g2.parent_pg_id = g3.id'');
+
+  -- level 4 and above not supported
+  select into not_supported_notice * from dblink(''
+  select 
+    p.id, g4.id from logical_phones p, phone_groups g, phone_groups g1, phone_groups g2, phone_groups g3,
+    phone_groups g4
+  where 
+    p.pg_id = g.id and g.parent_pg_id = g1.id and g1.parent_pg_id = g2.id and
+    g2.parent_pg_id = g3.id and g3.parent_pg_id = g4.id'') 
+  as (phone_id int, group_id int);
+  if found then
+    raise exception ''cannot support group levels 4 parents or deeper'';
+  end if;
+
+  return 1;
+end;
+' language plpgsql;
+
+create or replace function insert_phones_into_groups(varchar) returns integer as '
+declare
+  phone_select alias for $1;
+  grp record;
+  phone_group_id int;
+begin
+
+  for grp in select * from dblink(phone_select) as (phone_id int, group_id int) loop
+    select into phone_group_id group_id from phone_group_migration where pds_group_id = grp.group_id;
+    insert into phone_group (phone_id, group_id) values (grp.phone_id, phone_group_id);
+  end loop;
+
+  return 1;
+end;
+' language plpgsql;
+
 
 -- N O N   P O L Y C O M   P H O N E S
 create or replace function migrate_non_polycom_phones() returns integer as '
@@ -138,7 +248,7 @@ begin
 
     select into my_phone_type * from phone_type_migration where pds_model = phone.pt_model; 
 
-    raise debug ''importing phone %, %, %...'', phone.serial_number, my_phone_type.bean_id;
+    raise notice ''importing phone %, %, %...'', phone.serial_number, my_phone_type.bean_id;
 
     insert into phone (phone_id, serial_number, bean_id, model_id) values (phone.id, phone.serial_number, my_phone_type.bean_id, my_phone_type.model_id);
 
@@ -164,8 +274,10 @@ declare
 begin
 
   for phone in select * from dblink(''select p.phone_id, p.serial_number, p.name, p.factory_id, p.storage_id, p.folder_id from phone p'') as (phone_id int, serial_number text, name text, factory_id text, storage_id int, folder_id int) loop
+   
+   -- todo polycom phone
 
-    -- phone settings
+   -- todo phone settings
     
   end loop; 
 
@@ -180,8 +292,9 @@ select dblink_connect('dbname=PDS');
 
 select migrate_user_groups();
 select migrate_users();
+select migrate_user_group_tree();
 select migrate_phone_groups();
 select migrate_non_polycom_phones();
 select migrate_polycom_phones();
-
+select migrate_phone_group_tree();
 
