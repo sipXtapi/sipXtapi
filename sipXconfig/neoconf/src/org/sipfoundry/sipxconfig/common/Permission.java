4df314f63441277d22efe46c6e2ac86b879768ce
Douglas Hubler <douglas@hubler.us>
2005-09-12 21:22:45 +0000

    coding style error
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5030 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/Permission.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/Permission.java
@@ -15,8 +15,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.lang.enums.EnumUtils;
 import org.apache.commons.lang.enums.Enum;
+import org.apache.commons.lang.enums.EnumUtils;
 import org.sipfoundry.sipxconfig.setting.Group;
 import org.sipfoundry.sipxconfig.setting.Setting;
 
