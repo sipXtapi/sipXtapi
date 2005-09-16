860f2f6fcea1fa5c17c1b4ea0bec6a09420509c3
Douglas Hubler <douglas@hubler.us>
2005-09-16 21:32:30 +0000

    FIX BUILD Forgot files from last checkin
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5058 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/components/AbstractTableModel.java
@@ -0,0 +1,70 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.components;
+
+import java.util.Arrays;
+
+import org.apache.tapestry.IComponent;
+import org.apache.tapestry.contrib.table.model.IBasicTableModel;
+import org.apache.tapestry.contrib.table.model.ITableColumnModel;
+import org.apache.tapestry.contrib.table.model.ITableModel;
+import org.apache.tapestry.contrib.table.model.common.BasicTableModelWrap;
+import org.apache.tapestry.contrib.table.model.ognl.ExpressionTableColumn;
+import org.apache.tapestry.contrib.table.model.simple.SimpleTableColumn;
+import org.apache.tapestry.contrib.table.model.simple.SimpleTableColumnModel;
+
+public abstract class AbstractTableModel implements IBasicTableModel {
+
+    static final int DEFAULT_PAGE_SIZE = 10;
+    private int m_pageSize = DEFAULT_PAGE_SIZE;
+       
+    /**
+     * For some reason if table model is overridden, page size is ignored when
+     * defined on TableFormView
+     *  
+     * @param pageSize default is 10
+     */
+    public void setPageSize(int pageSize) {
+        m_pageSize = pageSize;
+    }
+
+    public abstract ITableModel createTableModel(IComponent component);
+    
+    public ITableModel createTableModel(IComponent component, SimpleTableColumn[] columns) {
+        for (int i = 0; i < columns.length; i++) {
+            columns[i].loadSettings(component);
+        }
+
+        ITableColumnModel colModel = new SimpleTableColumnModel(Arrays.asList(columns));
+        BasicTableModelWrap tableModel = new BasicTableModelWrap(this, colModel);
+        tableModel.getPagingState().setPageSize(m_pageSize);
+
+        return tableModel;        
+    }
+    
+    public static class OrderByTableColumn extends ExpressionTableColumn {
+        private String m_orderBy;
+        
+        public OrderByTableColumn(String name, String expression, String orderBy) {
+            super(name, expression, true);
+            m_orderBy = orderBy;
+        }
+        
+        public OrderByTableColumn(String name, String expression) {
+            super(name, expression, false);
+        }
+        
+        public String getOrderBy() {
+            return m_orderBy;
+        }
+    }
+}
