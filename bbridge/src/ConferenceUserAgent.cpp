56fe1920b69c9834ac06ef1e5fc4c1a155fcf3f4
Jason Fischl <jason.fischl@gmail.com>
2005-09-22 23:11:01 +0000

    added initial files for conferencing - not compiling yet
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5113 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ bbridge/src/ConferenceUserAgent.cpp
@@ -0,0 +1,281 @@
+#include "rutil/Log.hxx"
+#include "rutil/Logger.hxx"
+#include "resip/dum/ServerInviteSession.hxx"
+#include "ConferenceUserAgent.h"
+
+using namespace resip;
+using namespace std;
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::TEST
+
+Participant::Participant(DialogUsageManager& dum, const SipMessage& msg) : AppDialogSet(dum)
+{
+   ConferenceUserAgent& ua = dynamic_cast<ConferenceUserAgent&>(dum);
+   
+}
+
+AppDialogSet* 
+ParticipantFactory::createAppDialogSet(DialogUsageManager& dum, const SipMessage& msg)
+{
+   return new Participant(dum);
+}
+
+ConferenceUserAgent::ConferenceUserAgent(const NameAddr& myAor) :
+   mProfile(new MasterProfile),
+   mSecurity(new Security(mCertPath)),
+   mStack(mSecurity),
+   mDum(mStack),
+   mStackThread(mStack),
+   mDumThread(mDum),
+   mConfigDb(),
+   mMediaFactory(&mConfigDb),
+   mCodecFactory(),
+   mSdpCodecArray(0),
+   mNumCodecs(0)
+{
+   const char* publicAddress = "127.0.0.1";
+   const char* localAddress = "127.0.0.1";
+
+   const int numCodecs = 3;
+   SdpCodec::SdpCodecTypes sdpCodecEnumArray[numCodecs];
+   sdpCodecEnumArray[0] = SdpCodecFactory::getCodecType(CODEC_G711_PCMU);
+   sdpCodecEnumArray[1] = SdpCodecFactory::getCodecType(CODEC_G711_PCMA);
+   sdpCodecEnumArray[2] = SdpCodecFactory::getCodecType(CODEC_DTMF_RFC2833);
+
+   mCodecFactory.buildSdpCodecFactory(numCodecs, sdpCodecEnumArray);
+   mCodecFactory.getCodecs(mNumCodecs, mSdpCodecArray);
+    
+   //Log::initialize(mLogType, mLogLevel, argv[0]);
+   addTransport(UDP, mUdpPort);
+   addTransport(TCP, mTcpPort);
+   addTransport(TLS, mTlsPort, V4, Data::Empty, myAor.host());
+    
+   mProfile->addSupportedMethod(INVITE);
+   mProfile->validateAcceptEnabled() = false;
+   mProfile->validateContentEnabled() = false;
+   mProfile->setDefaultFrom(myAor);
+   
+   if (!mContact.host().empty())
+   {
+      mProfile->setOverrideHostAndPort(mContact);
+   }
+   mProfile->setUserAgent("BostonBridge/0.1");
+   
+   mDum.setMasterProfile(mProfile);
+   mDum.setInviteSessionHandler(this);
+   
+   mStackThread.run(); 
+   mDumThread.run();
+}
+
+ConferenceUserAgent::~ConferenceUserAgent()
+{
+   mDumThread.shutdown();
+   mDumThread.join();
+   mStackThread.shutdown();
+   mStackThread.join();
+   
+}
+
+void
+ConferenceUserAgent::onNewSession(ClientInviteSessionHandle h, InviteSession::OfferAnswerType oat, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onNewSession(ServerInviteSessionHandle h, InviteSession::OfferAnswerType oat, const SipMessage& msg)
+{
+   InfoLog(<< h->myAddr().uri().user() << " INVITE from  " << h->peerAddr().uri().user());
+
+   InfoLog(<< h->myAddr().uri().user() << " 180 from  " << h->peerAddr().uri().user());
+   const Data& aor = msg.header(h_RequestLine).uri().getAor();
+   Participant* part = dynamic_cast<Participant*>(getAppDialogSet().get());
+   assert(part);
+   if (!mConferences.count(aor))
+   {
+      mConferences[aor] = new Conference(aor);
+   }
+   part->assign(mConferences[aor]);
+         
+   SdpContents* sdp = dynamic_cast<SdpContents*>(msg.getContents());
+   h->provideAnswer(*sdp);
+   h->accept();
+}
+
+void
+ConferenceUserAgent::onTerminated(InviteSessionHandle h, InviteSessionHandler::TerminatedReason reason, const SipMessage* msg)
+{
+   if (reason != InviteSessionHandler::PeerEnded)
+   {
+      WarningLog(<< h->myAddr().uri().user() << " call terminated with " << h->peerAddr().uri().user());
+   }
+   else
+   {
+      WarningLog(<< h->myAddr().uri().user() << " ended call with " << h->peerAddr().uri().user());
+   }
+
+   Participant* part = dynamic_cast<Participant*>(getAppDialogSet().get());
+   assert(part);
+   assert(mConferences.count(aor));
+}
+
+void
+ConferenceUserAgent::onAnswer(InviteSessionHandle, const SipMessage& msg, const SdpContents&)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onOffer(InviteSessionHandle handle, const SipMessage& msg, const SdpContents& offer)
+{         
+}
+
+void
+ConferenceUserAgent::onFailure(ClientInviteSessionHandle h, const SipMessage& msg)
+{
+   assert(0);
+}
+      
+void
+ConferenceUserAgent::onEarlyMedia(ClientInviteSessionHandle, const SipMessage&, const SdpContents&)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onProvisional(ClientInviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onConnected(ClientInviteSessionHandle h, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onConnected(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onStaleCallTimeout(ClientInviteSessionHandle)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onRedirected(ClientInviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onOfferRequired(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(false);
+}
+
+void
+ConferenceUserAgent::onOfferRejected(InviteSessionHandle, const SipMessage* msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onDialogModified(InviteSessionHandle, InviteSession::OfferAnswerType oat, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onInfo(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onInfoSuccess(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onInfoFailure(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onRefer(InviteSessionHandle, ServerSubscriptionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onReferAccepted(InviteSessionHandle, ClientSubscriptionHandle, const SipMessage& msg)
+{
+   assert(false);
+}
+
+void
+ConferenceUserAgent::onReferRejected(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onMessage(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onMessageSuccess(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+void
+ConferenceUserAgent::onMessageFailure(InviteSessionHandle, const SipMessage& msg)
+{
+   assert(0);
+}
+
+
+void
+ConferenceUserAgent::onForkDestroyed(ClientInviteSessionHandle)
+{
+   assert(0);
+}
+
+/*
+  Copyright (c) 2005, Jason Fischl
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+  * Neither the name of PurpleComm, Inc. nor the names of its contributors may
+    be used to endorse or promote products derived from this software without
+    specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
---
+++ bbridge/src/ConferenceUserAgent.h
@@ -0,0 +1,151 @@
+#if !defined(DUM_ConferenceUserAgent_hxx)
+#define DUM_ConferenceUserAgent_hxx
+
+#include "resip/dum/DialogUsageManager.hxx"
+#include "resip/dum/DumThread.hxx"
+#include "resip/dum/InviteSessionHandler.hxx"
+#include "resip/dum/MasterProfile.hxx"
+#include "resip/stack/StackThread.hxx"
+
+namespace resip
+{
+
+class ParticipantFactory : public AppDialogSetFactory
+{
+   public:
+      AppDialogSet* createAppDialogSet(DialogUsageManager&, const SipMessage&);      
+};
+
+
+class Participant : public AppDialogSet
+{
+   public:
+      Participant(DialogUsageManager& dum);
+      ~Participant();
+
+      void assign(Conference* conf, int connId);
+      int id() const { return mConnId; }
+      
+   private:
+      Conference* mConference;
+      int mConnId;
+};
+
+class Conference : public CpMediaInterface
+{
+   public:
+      Conference(const Data& aor);
+      ~Conference();
+
+   private:
+      Data mAor;
+};
+
+
+
+class ConferenceUserAgent : public InviteSessionHandler
+{
+   public:
+      UserAgent();
+      virtual ~UserAgent();
+
+      void process();
+      
+   public:
+      // Invite Session Handler /////////////////////////////////////////////////////
+      virtual void onNewSession(ClientInviteSessionHandle h, InviteSession::OfferAnswerType oat, const SipMessage& msg);
+      virtual void onNewSession(ServerInviteSessionHandle h, InviteSession::OfferAnswerType oat, const SipMessage& msg);
+      virtual void onFailure(ClientInviteSessionHandle h, const SipMessage& msg);
+      virtual void onEarlyMedia(ClientInviteSessionHandle, const SipMessage&, const SdpContents&);
+      virtual void onProvisional(ClientInviteSessionHandle, const SipMessage& msg);
+      virtual void onConnected(ClientInviteSessionHandle h, const SipMessage& msg);
+      virtual void onConnected(InviteSessionHandle, const SipMessage& msg);
+      virtual void onStaleCallTimeout(ClientInviteSessionHandle);
+      virtual void onTerminated(InviteSessionHandle h, InviteSessionHandler::TerminatedReason reason, const SipMessage* msg);
+      virtual void onRedirected(ClientInviteSessionHandle, const SipMessage& msg);
+      virtual void onAnswer(InviteSessionHandle, const SipMessage& msg, const SdpContents&);
+      virtual void onOffer(InviteSessionHandle handle, const SipMessage& msg, const SdpContents& offer);
+      virtual void onOfferRequired(InviteSessionHandle, const SipMessage& msg);
+      virtual void onOfferRejected(InviteSessionHandle, const SipMessage* msg);
+      virtual void onDialogModified(InviteSessionHandle, InviteSession::OfferAnswerType oat, const SipMessage& msg);
+      virtual void onInfo(InviteSessionHandle, const SipMessage& msg);
+      virtual void onInfoSuccess(InviteSessionHandle, const SipMessage& msg);
+      virtual void onInfoFailure(InviteSessionHandle, const SipMessage& msg);
+      virtual void onRefer(InviteSessionHandle, ServerSubscriptionHandle, const SipMessage& msg);
+      virtual void onReferAccepted(InviteSessionHandle, ClientSubscriptionHandle, const SipMessage& msg);
+      virtual void onReferRejected(InviteSessionHandle, const SipMessage& msg);
+
+      virtual void onMessage(InviteSessionHandle, const SipMessage& msg);
+      virtual void onMessageSuccess(InviteSessionHandle, const SipMessage& msg);
+      virtual void onMessageFailure(InviteSessionHandle, const SipMessage& msg);
+
+   private:
+      SharedPtr<MasterProfile> mProfile;
+      Security* mSecurity;
+      SipStack mStack;
+      DialogUsageManager mDum;
+      StackThread mStackThread;
+      DumThread mDumThread;
+
+      OsConfigDb mConfigDb;
+      sipXmediaFactoryImpl mMediaFactory;
+      SdpCodecFactory mCodecFactory;
+      SdpCodec** mSdpCodecArray;
+      int mNumCodecs;
+      
+      HashMap<Data, Conference*> mConferences;
+};
+ 
+}
+
+#endif
+
+/* ====================================================================
+ * The Vovida Software License, Version 1.0 
+ * 
+ * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * 3. The names "VOCAL", "Vovida Open Communication Application Library",
+ *    and "Vovida Open Communication Application Library (VOCAL)" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact vocal@vovida.org.
+ *
+ * 4. Products derived from this software may not be called "VOCAL", nor
+ *    may "VOCAL" appear in their name, without prior written
+ *    permission of Vovida Networks, Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
+ * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by Vovida
+ * Networks, Inc. and many individuals on behalf of Vovida Networks,
+ * Inc.  For more information on Vovida Networks, Inc., please see
+ * <http://www.vovida.org/>.
+ *
+ */
