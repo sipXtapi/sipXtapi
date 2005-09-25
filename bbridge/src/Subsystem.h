85f8156b82c529a3e84f4e7a2334114a1c2a1ea0
Jason Fischl <jason.fischl@gmail.com>
2005-09-25 01:24:31 +0000

    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5181 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ bbridge/src/Subsystem.h
@@ -0,0 +1,20 @@
+#if !defined(SIPX_SUBSYSTEM_HXX)
+#define SIPX_SUBSYSTEM_HXX 
+
+#include <iostream>
+#include "rutil/Data.hxx"
+
+namespace bbridge
+{
+
+class Subsystem 
+{
+   public:
+      // Add new systems below
+      static const Subsystem BBRIDGE;
+};
+
+
+}
+
+#endif
