bb59f5cbcab8bb8c269fa6642c661a8689443ae2
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-09-27 17:16:23 +0000

    XCF-626 Patch from Hannu Strang:  Someone had disabled the configuration test, I re-enabled it. Now the configuration passes test and isn't missing any parts.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5206 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/cisco/CiscoAtaPhoneTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/cisco/CiscoAtaPhoneTest.java
@@ -20,26 +20,27 @@ import org.sipfoundry.sipxconfig.common.TestUtil;
 import org.sipfoundry.sipxconfig.phone.PhoneTestDriver;
 
 public class CiscoAtaPhoneTest extends TestCase {
-    
+
     CiscoAtaPhone phone;
-    
+
     PhoneTestDriver tester;
-    
+
     protected void setUp() {
-        phone = new CiscoAtaPhone(CiscoModel.MODEL_ATA18X);               
+        phone = new CiscoAtaPhone(CiscoModel.MODEL_ATA18X);
         tester = new PhoneTestDriver(phone, "cisco/ata-phone.xml");
 
         String testDir = TestUtil.getTestSourceDirectory(getClass());
         phone.setCfgfmtUtility(testDir + "/cfgfmt");
         phone.setPtagDat(testDir + "/ptag.dat");
     }
-    
+
     public void testGenerateProfiles() throws Exception {
         StringWriter profile = new StringWriter();
+        phone.packBitmaps();
         phone.generateProfile(profile);
-        String expected = IOUtils.toString(this.getClass().getResourceAsStream("expected-ata18x.cfg"));
+        String expected = IOUtils.toString(this.getClass().getResourceAsStream(
+                "expected-ata18x.cfg"));
         assertNotNull(expected);
-        // TODO : Order not preserved, format valid, but not optimal. work in progress
-        //assertEquals(expected, profile.toString());
+        assertEquals(expected, profile.toString());
     }
 }
