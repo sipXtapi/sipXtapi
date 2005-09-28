3d6dff5886774a13af4e664ded3f499b576dec89
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-09-28 20:11:24 +0000

    Make FirstUser page look similar to loging page: Add standard banner etc. Remove default description from ConfirmPassword component - it was causing empty table row to be rendered.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main/sipXconfig@5221 a612230a-c5fa-0310-af8b-88eea846685b

--- web/test/org/sipfoundry/sipxconfig/site/user/FirstUserTestUi.java
+++ web/test/org/sipfoundry/sipxconfig/site/user/FirstUserTestUi.java
@@ -17,8 +17,6 @@ import net.sourceforge.jwebunit.WebTestCase;
 import org.sipfoundry.sipxconfig.site.SiteTestHelper;
 
 public class FirstUserTestUi extends WebTestCase {
-    private static final String FIRST_USER_PAGE_TITLE = "New Superadmin User";
-
     public static Test suite() throws Exception {
         return SiteTestHelper.webTestSuite(FirstUserTestUi.class);
     }
@@ -33,19 +31,19 @@ public class FirstUserTestUi extends WebTestCase {
         // Go home. *Don't* log in because that requires creating a user, but we want no users.
         SiteTestHelper.home(getTester(), false);
     }
-    
+
     /**
      * Test that clicking on ManageUsers (or any link that requires login) redirects to FirstUser
-     * when there are no users.  The Border component takes care of this.
+     * when there are no users. The Border component takes care of this.
      */
     public void testBorderRedirect() throws Exception {
         clickLink("ManageUsers");
         assertLandedOnFirstUserPage();
     }
-    
-    /** 
-     * Test that the LoginPage redirects to FirstUser page when there are no users.
-     * LoginPage has no Border, so it is a special case.
+
+    /**
+     * Test that the LoginPage redirects to FirstUser page when there are no users. LoginPage has
+     * no Border, so it is a special case.
      */
     public void testLoginPageRedirect() throws Exception {
         clickLink("toggleNavigation");
@@ -53,24 +51,24 @@ public class FirstUserTestUi extends WebTestCase {
         assertLandedOnFirstUserPage();
     }
 
-    public void testCreateSuperadmin()  throws Exception {
+    public void testCreateSuperadmin() throws Exception {
         clickLink("ManageUsers");
         assertLandedOnFirstUserPage();
-        
+
         // Fill in the PIN and create the superadmin user
         final String PIN = "ch33zeW1z";
         setFormElement("password", PIN);
         setFormElement("confirmPassword", PIN);
-        clickButton("form:apply");        
+        clickButton("form:apply");
         assertCopacetic();
-        
+
         // We should land on the login page
-        assertTextPresent("Please enter user ID and password");
+        assertFormPresent("login:form");
     }
-    
+
     private void assertLandedOnFirstUserPage() {
         assertCopacetic();
-        assertTextPresent(FIRST_USER_PAGE_TITLE);        
+        assertFormPresent("firstuser:form");
     }
 
     /** Assert that we landed on a page with no complaints */
