65658db849b55d7fa7cd86efeff1b01807c30408
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-09-30 19:31:12 +0000

    XCF-556 generate SIP password automatically for newly added users
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main/sipXconfig@5245 a612230a-c5fa-0310-af8b-88eea846685b

--- web/src/org/sipfoundry/sipxconfig/site/user/NewUser.java
+++ web/src/org/sipfoundry/sipxconfig/site/user/NewUser.java
@@ -11,6 +11,7 @@
  */
 package org.sipfoundry.sipxconfig.site.user;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.callback.ICallback;
 import org.apache.tapestry.callback.PageCallback;
@@ -25,13 +26,16 @@ import org.sipfoundry.sipxconfig.site.admin.ExtensionPoolsPage;
 public abstract class NewUser extends PageWithCallback implements PageRenderListener {
 
     public static final String PAGE = "NewUser";
+    private static final int SIP_PASSWORD_LEN = 8;
 
     public abstract CoreContext getCoreContext();
-    
+
     public abstract Integer getUserId();
+
     public abstract void setUserId(Integer userId);
 
-    public abstract User getUser();    
+    public abstract User getUser();
+
     public abstract void setUser(User user);
 
     public void commit(IRequestCycle cycle_) {
@@ -40,9 +44,9 @@ public abstract class NewUser extends PageWithCallback implements PageRenderList
             CoreContext core = getCoreContext();
             User user = getUser();
             core.saveUser(user);
-            
-            // On saving the user, it is given an ID.  Remember that ID so that if this
-            // page gets an Apply rather than OK, then we know that we have already 
+
+            // On saving the user, it is given an ID. Remember that ID so that if this
+            // page gets an Apply rather than OK, then we know that we have already
             // saved the user.
             setUserId(user.getId());
         }
@@ -51,39 +55,46 @@ public abstract class NewUser extends PageWithCallback implements PageRenderList
     /** Wrapper for callback so we can clear state on returning from this page */
     private class CallbackWrapper implements ICallback {
         private ICallback m_callback;
+
         public CallbackWrapper(ICallback callback) {
             m_callback = callback;
         }
+
         public void performCallback(IRequestCycle cycle) {
             setUserId(null);
             m_callback.performCallback(cycle);
-        }        
+        }
     }
 
     public void pageBeginRender(PageEvent event) {
-        if (getUserId() == null) {
-            setUser(new User());            
-        } else {
-            User user = getCoreContext().loadUser(getUserId());
-            setUser(user);            
+        User user = getUser();
+        if (user == null) {
+            if (getUserId() == null) {
+                user = new User();
+                user.setSipPassword(RandomStringUtils.randomAlphanumeric(SIP_PASSWORD_LEN));
+            } else {
+                user = getCoreContext().loadUser(getUserId());
+            }
+            setUser(user);
         }
-        
+
         // If the user clicks through to the Extension Pools page, clicking OK or Cancel on that
         // page should send the user back here
         ICallback callback = new PageCallback(PAGE);
-        ExtensionPoolsPage poolsPage = (ExtensionPoolsPage) event.getRequestCycle().getPage(ExtensionPoolsPage.PAGE);
+        ExtensionPoolsPage poolsPage = (ExtensionPoolsPage) event.getRequestCycle().getPage(
+                ExtensionPoolsPage.PAGE);
         poolsPage.setCallback(callback);
-        
+
         // If no callback was set before navigating to this page, then by
         // default, go back to the ManageUsers page
         if (getCallback() == null) {
             setCallback(new PageCallback(ManageUsers.PAGE));
         }
-                
+
         // Wrap the callback so we can clear state when returning from this page
-        if (!(getCallback() instanceof CallbackWrapper)) {  // wrap it only once
+        if (!(getCallback() instanceof CallbackWrapper)) { // wrap it only once
             setCallback(new CallbackWrapper(getCallback()));
         }
-        
+
     }
 }
