40564daf3a6c8df18b270ef37160b7e961c0d106
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-10-13 17:58:35 +0000

    Changes from the sipXconfig-soap branch merged into mainline.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5327 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/api/UserServiceImpl.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2005 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2005 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.api;
+
+
+
+public class UserServiceImpl implements UserService {
+    
+    public void echo() {
+        System.out.println("nop");
+    }
+}
