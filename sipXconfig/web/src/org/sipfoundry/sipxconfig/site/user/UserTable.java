214adf9893c119f73e9fd40aa1502e7dcaadbdbb
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-10-23 17:01:52 +0000

    XCF-709 - slightly better fix that the one commited to 3.0 branch we now only provide data model (source propery) to users table, column model and all tapestry caching and session management is standard
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5390 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/UserTable.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/UserTable.java
@@ -12,7 +12,7 @@
 package org.sipfoundry.sipxconfig.site.user;
 
 import org.apache.tapestry.BaseComponent;
-import org.apache.tapestry.IComponent;
+import org.apache.tapestry.contrib.table.model.IBasicTableModel;
 import org.apache.tapestry.contrib.table.model.IPrimaryKeyConvertor;
 import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.event.PageRenderListener;
@@ -22,46 +22,30 @@ import org.sipfoundry.sipxconfig.components.ObjectSourceDataSqueezer;
 import org.sipfoundry.sipxconfig.components.SelectMap;
 
 public abstract class UserTable extends BaseComponent implements PageRenderListener {
-    
+
     public static final String COMPONENT = "UserTable";
-    
+
     /** REQUIRED PROPERTY */
     public abstract SelectMap getSelections();
 
     public abstract void setSelections(SelectMap selected);
-    
+
     public abstract CoreContext getCoreContext();
-    
-    public void setGroupId(Integer groupId) {
-        UserTableModel model = getTableModel();
-        model.setGroupId(groupId);                
-    }
-    
-    // not sure why this is called
-    public Integer getGroupId() {
-        UserTableModel model = getTableModel();
-        return model.getGroupId();                
-    }
-    
-    public abstract UserTableModel getTableModel();
-    
-    public void pageBeginRender(PageEvent event_) {
-        if (getSelections() == null) {
-            setSelections(new SelectMap());            
-        }        
+
+    public abstract Integer getGroupId();
+
+    public IBasicTableModel getTableModel() {
+        return new UserTableModel(getCoreContext(), getGroupId());
     }
 
-    /**
-     * I cannot find out how to get this in ognl so I shamelessly defined a this getter
-     * @return this
-     */
-    public IComponent getThis() {
-        return this;
+    public void pageBeginRender(PageEvent event_) {
+        if (getSelections() == null) {
+            setSelections(new SelectMap());
+        }
     }
-    
+
     public IPrimaryKeyConvertor getIdConverter() {
         CoreContext context = getCoreContext();
         return new ObjectSourceDataSqueezer(context, User.class);
     }
 }
-
