30cd77006f860dfd6df21178d09842b9514b2cf7
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-10-31 15:56:23 +0000

    XCF-739 change default for Polycom NTP setting from "clock" to "pool.ntp.org"
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/3.0@5446 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/polycom/CoreConfigurationTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/polycom/CoreConfigurationTest.java
@@ -23,37 +23,33 @@ import org.custommonkey.xmlunit.XMLUnit;
 import org.sipfoundry.sipxconfig.TestHelper;
 import org.sipfoundry.sipxconfig.phone.PhoneTestDriver;
 
-
 public class CoreConfigurationTest extends XMLTestCase {
-    
+
     PolycomPhone phone;
-    
+
     PhoneTestDriver tester;
-    
+
     protected void setUp() {
         XMLUnit.setIgnoreWhitespace(true);
         phone = new PolycomPhone(PolycomModel.MODEL_600);
-        tester = new PhoneTestDriver(phone, "polycom/phone.xml");        
+        tester = new PhoneTestDriver(phone, "polycom/phone.xml");
     }
 
     public void testGenerateProfile() throws Exception {
-        // sampling of settings 
+        // sampling of settings
         phone.getSettings().getSetting("call/rejectBusyOnDnd").setValue("0");
-        
-        CoreConfiguration cfg = new CoreConfiguration(phone);
-        cfg.setVelocityEngine(TestHelper.getVelocityEngine());        
-        CharArrayWriter out = new CharArrayWriter();
-        cfg.generateProfile(phone.getCoreTemplate(), out);       
-        
-        InputStream expectedPhoneStream = getClass().getResourceAsStream("expected-ipmid.cfg");
-        Reader expectedXml = new InputStreamReader(expectedPhoneStream);            
-        Reader generatedXml = new CharArrayReader(out.toCharArray());
 
-        // helpful debug
-        System.out.println(new String(out.toCharArray()));
+        CoreConfiguration cfg = new CoreConfiguration(phone);
+        cfg.setVelocityEngine(TestHelper.getVelocityEngine());
+        CharArrayWriter out = new CharArrayWriter();
+        cfg.generateProfile(phone.getCoreTemplate(), out);
+
+        InputStream expectedPhoneStream = getClass().getResourceAsStream("expected-ipmid.cfg");
+        Reader expectedXml = new InputStreamReader(expectedPhoneStream);
+        Reader generatedXml = new CharArrayReader(out.toCharArray());
 
         Diff phoneDiff = new Diff(expectedXml, generatedXml);
         assertXMLEqual(phoneDiff, true);
-        expectedPhoneStream.close();        
+        expectedPhoneStream.close();
     }
 }
