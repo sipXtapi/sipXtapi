580d4da48e676ae2dbd9e682fbb4ba951c6df6d5
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-10-31 12:39:02 +0000

    User group operations implemented
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXconfig-bulkops@5442 a612230a-c5fa-0310-af8b-88eea846685b

--- neoconf/src/org/sipfoundry/sipxconfig/common/CoreContext.java
+++ neoconf/src/org/sipfoundry/sipxconfig/common/CoreContext.java
@@ -35,23 +35,23 @@ public interface CoreContext extends DataObjectSource, AliasProvider, AliasOwner
     public User loadUser(Integer id);
 
     public List loadUsers();
-    
+
     public int getUsersCount();
-    
+
     public int getUsersInGroupCount(Integer groupId);
-    
+
     /**
-     * Return the number of users who are both in the group and found by search.
-     * Search matches the searchString against user names, first names, last
-     * names, and aliases.  Matching is case-insensitive and also matches substrings.
-     * For example, the search string "cn" will match the last name "McNamara".
+     * Return the number of users who are both in the group and found by search. Search matches
+     * the searchString against user names, first names, last names, and aliases. Matching is
+     * case-insensitive and also matches substrings. For example, the search string "cn" will
+     * match the last name "McNamara".
      * 
      * @param groupId ID of a group, or null to match all groups
      * @param searchString string to search with, or null to not search
      * @return number of users
      */
     public int getUsersInGroupWithSearchCount(Integer groupId, String searchString);
-    
+
     public List loadUsersByPage(String search, Integer groupId, int page, int pageSize,
             String orderBy, boolean orderAscending);
 
@@ -62,15 +62,14 @@ public interface CoreContext extends DataObjectSource, AliasProvider, AliasOwner
     public User loadUserByAlias(String userName);
 
     public User loadUserByUserNameOrAlias(String userNameOrAlias);
-    
+
     /**
-     * Check whether the user has a username or alias that collides with an existing username
-     * or alias.  Check for internal collisions as well, for example, the user has an alias
-     * that is the same as the username.  (Duplication within the aliases is not possible 
-     * because the aliases are stored as a Set.)
-     * If there is a collision, then return the bad name (username or alias).
-     * Otherwise return null.
-     * If there are multiple collisions, then it's arbitrary which name is returned.
+     * Check whether the user has a username or alias that collides with an existing username or
+     * alias. Check for internal collisions as well, for example, the user has an alias that is
+     * the same as the username. (Duplication within the aliases is not possible because the
+     * aliases are stored as a Set.) If there is a collision, then return the bad name (username
+     * or alias). Otherwise return null. If there are multiple collisions, then it's arbitrary
+     * which name is returned.
      * 
      * @param user user to test
      * @return name that collides
@@ -95,16 +94,19 @@ public interface CoreContext extends DataObjectSource, AliasProvider, AliasOwner
     public Collection getGroupMembers(Group group);
 
     /**
-     * Called to create a superadmin user with an empty password,
-     * to recover from a situation where there are no admin users in the DB
+     * Called to create a superadmin user with an empty password, to recover from a situation
+     * where there are no admin users in the DB
      */
     public void createAdminGroupAndInitialUserTask();
-    
+
     /**
-     * Called by the bootstrap page to create the superadmin user, giving it
-     * the specified pin
+     * Called by the bootstrap page to create the superadmin user, giving it the specified pin
      * 
      * @param pin
      */
     public void createAdminGroupAndInitialUser(String pin);
+
+    public void addToGroup(Integer groupId, Collection ids);
+
+    public void removeFromGroup(Integer groupId, Collection ids);
 }
