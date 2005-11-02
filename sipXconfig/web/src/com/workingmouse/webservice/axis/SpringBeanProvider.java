fbb55f64ea6df0bd6cd996abb5e098bf3130c3ed
Douglas Hubler <douglas@hubler.us>
2005-11-02 22:51:45 +0000

    XCF-305 fix another build issues and ...  FINALLY GOT UNIT TESTS WORKING! I didnt realize i could go w/1 wsdl and this is much easier at least in the short term.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5487 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/com/workingmouse/webservice/axis/SpringBeanProvider.java
+++ sipXconfig/web/src/com/workingmouse/webservice/axis/SpringBeanProvider.java
@@ -4,12 +4,11 @@ import javax.servlet.ServletContext;
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.server.ServletEndpointContext;
 
+import org.apache.axis.AxisFault;
 import org.apache.axis.Constants;
 import org.apache.axis.MessageContext;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.axis.utils.Messages;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * Utility class used by the {@link com.workingmouse.webservice.axis.SpringBeanMsgProvider}
@@ -20,14 +19,11 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  */
 public class SpringBeanProvider {
 
-    private Log log = LogFactory.getLog(getClass());
-    
     /**
      * The server-config.wsdd service parameter used to provide the name of the 
      * Spring-managed bean to use as the web service end-point.
      */
     public static final String BEAN_OPTION_NAME = "springBean";
-    public static final String BEAN_CLASS_OPTION_NAME = "springBeanClass";
 
     private WebApplicationContext webAppCtx;
 
@@ -36,53 +32,43 @@ public class SpringBeanProvider {
      */
     public Object getBean(MessageContext msgContext, String beanName) throws Exception {
         initWebAppContext(msgContext);
-        if (webAppCtx != null) {
-            return webAppCtx.getBean(beanName);
-        }
-        else {
-            return null;
-        }
+        return webAppCtx.getBean(beanName);
     }
 
     /**
      * Return the class of a bean bound with the given beanName in the WebApplicationContext.
      */
-    public Class getBeanClass(String className) {
-        Class result = null;
+    public Class getBeanClass(MessageContext msgContext, String beanName) throws AxisFault {
         try {
-            result = Class.forName(className);
+            Object bean = getBean(msgContext, beanName);
+            return bean.getClass();
+
+        } catch (Exception e) {
+            throw new AxisFault(Messages.getMessage("noClassForService00", beanName), e);
         }
-        catch (ClassNotFoundException e) {
-            log.debug("class "+className+" not found");
-        }
-        return result;
     }
 
     private void initWebAppContext(MessageContext msgContext) throws ServiceException {
-        log.info("initializing app context for spring-axis integration");
-        if (webAppCtx == null) {
-            if (msgContext != null) {
-                Object context = msgContext.getProperty(Constants.MC_SERVLET_ENDPOINT_CONTEXT);
-                if (context instanceof ServletEndpointContext) {
-                    ServletEndpointContext servletEndpointContext = (ServletEndpointContext) context;
-                    ServletContext servletCtx = servletEndpointContext.getServletContext();                
-                    webAppCtx = WebApplicationContextUtils.getWebApplicationContext(servletCtx);
-                    if (webAppCtx == null) {
-                        log.info("failed to retrieve webapp context for spring-axis integration");
-                        throw new ServiceException(
-                            "Cannot find WebApplicationContext from org.springframework.web.context.ContextLoaderListener");                                   
-                    }
-                } else {
-                    log.info("failed to retrieve webapp context for spring-axis integration because this is an incorrect servlet context!");
-                    throw new ServiceException("Invalid context - expected ["
-                            + ServletEndpointContext.class.getName()
-                            + "], actual ["
-                            + context
-                            + "]");
+        if (webAppCtx == null && msgContext != null) {
+            Object context = msgContext.getProperty(Constants.MC_SERVLET_ENDPOINT_CONTEXT);
+            if (context instanceof ServletEndpointContext) {
+                ServletEndpointContext servletEndpointContext = (ServletEndpointContext) context;
+                ServletContext servletCtx = servletEndpointContext.getServletContext();
+                webAppCtx = (WebApplicationContext) servletCtx
+                    .getAttribute(SpringAxisServlet.SERVLET_CONTEXT_ATTRIBUTE);
+                if (webAppCtx == null) {
+                    throw new ServiceException(
+                        "Cannot find SpringAxisServlet's WebApplicationContext"
+                                + " as ServletContext attribute ["
+                                + SpringAxisServlet.SERVLET_CONTEXT_ATTRIBUTE
+                                + "]");
                 }
-            }
-            else {
-                log.info("null msg context!");
+            } else {
+                throw new ServiceException("Invalid context - expected ["
+                        + ServletEndpointContext.class.getName()
+                        + "], actual ["
+                        + context
+                        + "]");
             }
         }
     }
--- sipXconfig/web/src/com/workingmouse/webservice/axis/SpringBeanRPCProvider.java
+++ sipXconfig/web/src/com/workingmouse/webservice/axis/SpringBeanRPCProvider.java
@@ -1,7 +1,6 @@
 package com.workingmouse.webservice.axis;
 
 import org.apache.axis.AxisFault;
-import org.apache.axis.Handler;
 import org.apache.axis.MessageContext;
 import org.apache.axis.handlers.soap.SOAPService;
 import org.apache.axis.providers.java.RPCProvider;
@@ -12,71 +11,54 @@ import org.apache.axis.providers.java.RPCProvider;
  * Simply delegates to {@link com.workingmouse.webservice.axis.SpringBeanProvider}.
  * <p>
  * To use this class:<br>
- * 1. Configure web.xml to use the axis servlet
- * 2. Configure web.xml to use the spring @see org.springframework.web.context.ContextLoaderListener.
- * 3. Configure your server-config.wsdd service to use this class as the service handler.
- * 
- * Here is an example:
+ * 1. Configure {@link com.workingmouse.webservice.axis.SpringAxisServlet} as your axis servlet in web.xml.
  * <pre>
- * &lt;service name="hdWebService.jws" provider="Handler" style="rpc"&gt;
- *      &lt;parameter name="handlerClass" value="com.workingmouse.webservice.axis.SpringBeanRPCProvider"/&gt;
- *      &lt;parameter name="wsdlTargetNamespace" value="http://healthdec.com"/&gt;
- *      &lt;parameter name="springBean" value="hdWebService"/&gt;
- *      &lt;parameter name="springBeanClass" value="com.healthdec.hdSOAPServer.WebService"/&gt;
- *      &lt;parameter name="allowedMethods" value="findAllInstances"/&gt;
- *      &lt;beanMapping qname="myNS:Instance" xmlns:myNS="urn:BeanService" languageSpecificType="java:com.healthdec.domain.dm.Instance"/&gt;
- *      &lt;beanMapping qname="myNS:CRFType" xmlns:myNS="urn:BeanService" languageSpecificType="java:com.healthdec.domain.dm.CRFType"/&gt;
- *      &lt;beanMapping qname="myNS:CRFQuestionType" xmlns:myNS="urn:BeanService" languageSpecificType="java:com.healthdec.domain.dm.CRFQuestionType"/&gt;
+ * &lt;servlet&gt;
+ *   &lt;servlet-name&gt;axis&lt;/servlet-name&gt;
+ *   &lt;display-name&gt;Apache-Axis Servlet&lt;/display-name&gt;
+ *   &lt;servlet-class&gt;com.workingmouse.webservice.axis.SpringAxisServlet&lt;/servlet-class&gt;
+ * &lt;/servlet&gt;
+ * </pre>
+ * 2. Configure your server-config.wsdd service to use this class as the service handler.
+ * <pre>
+ * &lt;service name=&quot;products.jws&quot; provider=&quot;Handler&quot; style=&quot;document&quot; use=&quot;literal&quot;&gt;
+ *   &lt;parameter name=&quot;handlerClass&quot; value=&quot;com.workingmouse.webservice.axis.SpringBeanRPCProvider&quot;/&gt;
+ *   &lt;parameter name=&quot;wsdlTargetNamespace&quot; value=&quot;http://www.ioof.com.au/schemas/public&quot;/&gt;
+ *   &lt;parameter name=&quot;springBean&quot; value=&quot;productWS&quot;/&gt;
  * &lt;/service&gt;
- * </pre> 
- * 
- * 4. Configure a Spring-managed bean in your applicationContext xml file that will act as the web service end point.
+ * </pre>
+ * 3. Configure a Spring-managed bean in axis-servlet.xml that will act as the web service end point.
  * <pre>
  * &lt;bean id=&quot;productWS&quot; class=&quot;com.workingmouse.webservice.ProductWebServices&quot;&gt;
+ *   &lt;property name=&quot;productServices&quot;&gt;&lt;ref bean=&quot;productServices&quot;/&gt;&lt;/property&gt;
  * &lt;/bean&gt;
  * </pre>
  * 
  * @author Tom Czarniecki (cThomas AT workingmouse DOT com)
  * 
+ * @see com.workingmouse.webservice.axis.SpringAxisServlet
  * @see com.workingmouse.webservice.axis.SpringBeanProvider
  */
 public class SpringBeanRPCProvider extends RPCProvider {
 
     private final SpringBeanProvider provider = new SpringBeanProvider();
-    private String serviceClassName;
-    private String springBeanName;
-    
-    
-    /* (non-Javadoc)
-     * @see org.apache.axis.providers.BasicProvider#initServiceDesc(org.apache.axis.handlers.soap.SOAPService, org.apache.axis.MessageContext)
-     * 
-     * Get the name and class of the Spring bean that will implement the Webservice methods.
-     * 
-     */
-    public void initServiceDesc(SOAPService service, MessageContext arg1)
-            throws AxisFault {
-        setServiceClassName((String)service.getOption(SpringBeanProvider.BEAN_CLASS_OPTION_NAME));
-        setSpringBeanName((String)service.getOption(SpringBeanProvider.BEAN_OPTION_NAME));
-        super.initServiceDesc(service, arg1);
-    }
+
     /**
      * @see org.apache.axis.providers.java.JavaProvider#makeNewServiceObject(org.apache.axis.MessageContext, java.lang.String)
      */
     protected Object makeNewServiceObject(MessageContext msgContext, String clsName)
         throws Exception {
-        if (msgContext != null) {
-            return provider.getBean(msgContext, getSpringBeanName());
-        }
-        else {
-            return null;
-        }
+
+        return provider.getBean(msgContext, clsName);
     }
 
     /**
      * @see org.apache.axis.providers.java.JavaProvider#getServiceClass(java.lang.String, org.apache.axis.handlers.soap.SOAPService, org.apache.axis.MessageContext)
      */
-    protected Class getServiceClass(String clsName, SOAPService service, MessageContext msgContext) {
-        return provider.getBeanClass(clsName);
+    protected Class getServiceClass(String clsName, SOAPService service, MessageContext msgContext)
+        throws AxisFault {
+
+        return provider.getBeanClass(msgContext, clsName);
     }
 
     /**
@@ -84,37 +66,5 @@ public class SpringBeanRPCProvider extends RPCProvider {
      */
     protected String getServiceClassNameOptionName() {
         return SpringBeanProvider.BEAN_OPTION_NAME;
-    }
-    public void setServiceClassName(String serviceClassName) {
-        this.serviceClassName = serviceClassName;
-    }
-    /**
-     * @see org.apache.axis.providers.java.JavaProvider#getServiceClassName(org.apache.axis.Handler)
-     */
-    protected String getServiceClassName(Handler arg0) {
-        return getServiceClassName();
-    }
-
-    /**
-     * @return Returns the serviceClassName.
-     */
-    public String getServiceClassName() {
-        return serviceClassName;
-    }
-    /**
-     * @param serviceClassName The serviceClassName to set.
-     */
-    
-    /**
-     * @return Returns the springBeanName.
-     */
-    public String getSpringBeanName() {
-        return springBeanName;
-    }
-    /**
-     * @param springBeanName The springBeanName to set.
-     */
-    public void setSpringBeanName(String springBeanName) {
-        this.springBeanName = springBeanName;
     }
 }
