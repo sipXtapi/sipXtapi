c6084c42f5e48e01fbd6ea433654eb8c52b54c71
Douglas Hubler <douglas@hubler.us>
2005-11-03 23:32:27 +0000

    XCF-305 - Get findUser to return array of users
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5500 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/api/UserServiceImpl.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/api/UserServiceImpl.java
@@ -12,6 +12,7 @@
 package org.sipfoundry.sipxconfig.api;
 
 import java.rmi.RemoteException;
+import java.util.List;
 
 import org.sipfoundry.sipxconfig.common.CoreContext;
 
@@ -26,8 +27,31 @@ public class UserServiceImpl implements UserService {
     public void addUser(AddUser addUser) throws RemoteException {
         org.sipfoundry.sipxconfig.common.User u = new org.sipfoundry.sipxconfig.common.User();
         User ut = addUser.getUser();
-        u.setUserName(ut.getUserName());
-        u.setPin(ut.getPin(), m_coreContext.getAuthorizationRealm());
+        fromSoap(u, ut);
         m_coreContext.saveUser(u);
+    }
+
+    public User[] findUser(FindUser findUser) throws RemoteException {
+        List users = m_coreContext.loadUsersByPage(findUser.getByName(), 
+                null, 0, 1000, null, true);
+        
+        User[] arrayOfUsers = new User[users.size()];
+        for (int i = 0; i < users.size(); i++) {
+            org.sipfoundry.sipxconfig.common.User u = (org.sipfoundry.sipxconfig.common.User) users.get(i);
+            arrayOfUsers[i] = new User();
+            toSoap(u, arrayOfUsers[i]);
+        }
+        
+        return arrayOfUsers;
     }    
+    
+    void toSoap(org.sipfoundry.sipxconfig.common.User from, User to) {
+        to.setUserName(from.getUserName());
+        to.setPintoken(from.getPintoken());        
+    }
+    
+    void fromSoap(org.sipfoundry.sipxconfig.common.User to, User from) {
+        to.setUserName(from.getUserName());
+        to.setPin(from.getPin(), m_coreContext.getAuthorizationRealm());                
+    }
 }
