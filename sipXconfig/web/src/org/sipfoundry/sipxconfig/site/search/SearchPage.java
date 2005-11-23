b830158e1f05c57b21c20d276f94593ca589025f
Damian Krzeminski <dkrzeminski@pingtel.com>
2005-11-23 00:04:09 +0000

    XCF-756 search page displays the type of the found item (in addition to label and description)
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5633 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/search/SearchPage.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/search/SearchPage.java
@@ -16,12 +16,14 @@ import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry.IExternalPage;
+import org.apache.tapestry.IMessages;
 import org.apache.tapestry.IPage;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.event.PageRenderListener;
 import org.apache.tapestry.html.BasePage;
 import org.sipfoundry.sipxconfig.components.TapestryUtils;
+import org.sipfoundry.sipxconfig.search.BeanAdaptor;
 import org.sipfoundry.sipxconfig.search.SearchManager;
 
 public abstract class SearchPage extends BasePage implements IExternalPage, PageRenderListener {
@@ -38,6 +40,8 @@ public abstract class SearchPage extends BasePage implements IExternalPage, Page
     public abstract Collection getResults();
 
     public abstract EditPageProvider getEditPageProvider();
+
+    public abstract BeanAdaptor.Identity getResultItem();
 
     public void activateExternalPage(Object[] parameters, IRequestCycle cycle_) {
         String query = (String) TapestryUtils.assertParameter(String.class, parameters, 0);
@@ -74,5 +78,24 @@ public abstract class SearchPage extends BasePage implements IExternalPage, Page
         if (page != null) {
             cycle.activate(page);
         }
+    }
+
+    /**
+     * Iterate klass hierarchy untule it finds the name for the class.
+     * 
+     * Names should be defined in SearchPage.properties file.
+     * 
+     * @return name for the class of found item or label.default.type
+     */
+    public String getResultItemType() {
+        IMessages messages = getMessages();
+        BeanAdaptor.Identity item = getResultItem();
+        for (Class k = item.getBeanClass(); k != Object.class; k = k.getSuperclass()) {
+            String typeName = messages.getMessage(k.getName(), null);
+            if (typeName != null) {
+                return typeName;
+            }
+        }
+        return getMessage("label.default.type");
     }
 }
