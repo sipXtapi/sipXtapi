8156af44cb4a2729f74c1cdb9a011ebc92f3eceb
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-01-06 21:30:43 +0000

    XCF-848 only dialing plans rules XML file are now listed in dropdown box on Activate dial plan page
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@5820 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/dialplan/ActivateDialPlan.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/dialplan/ActivateDialPlan.java
@@ -11,18 +11,25 @@
  */
 package org.sipfoundry.sipxconfig.site.dialplan;
 
+import org.apache.commons.lang.enums.Enum;
 import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.form.IPropertySelectionModel;
 import org.apache.tapestry.html.BasePage;
 import org.sipfoundry.sipxconfig.admin.commserver.SipxProcessContext.Process;
 import org.sipfoundry.sipxconfig.admin.dialplan.DialPlanContext;
 import org.sipfoundry.sipxconfig.admin.dialplan.config.ConfigFileType;
 import org.sipfoundry.sipxconfig.admin.dialplan.config.ConfigGenerator;
+import org.sipfoundry.sipxconfig.components.EnumPropertySelectionModel;
 import org.sipfoundry.sipxconfig.site.admin.commserver.RestartReminderPanel;
 
 /**
  * ActivateDialPlan
  */
 public abstract class ActivateDialPlan extends BasePage {
+    public static final Enum[] OPTIONS = {
+        ConfigFileType.MAPPING_RULES, ConfigFileType.FALLBACK_RULES, ConfigFileType.AUTH_RULES
+    };
+
     public static final String PAGE = "ActivateDialPlan";
 
     public abstract ConfigFileType getSelected();
@@ -43,6 +50,12 @@ public abstract class ActivateDialPlan extends BasePage {
         return new Process[] {
             Process.REGISTRAR, Process.AUTH_PROXY
         };
+    }
+
+    public IPropertySelectionModel getFileSelectionModel() {
+        EnumPropertySelectionModel model = new EnumPropertySelectionModel();
+        model.setOptions(OPTIONS);
+        return model;
     }
 
     public void activate(IRequestCycle cycle) {
