ee37b5e890f3fedd8ebfe876488d03f1fd7191af
Douglas Hubler <douglas@hubler.us>
2006-02-27 23:20:02 +0000

    Manage upload change on submit link not working, firmware dropdown list was scrambled
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXconfig@6263 a612230a-c5fa-0310-af8b-88eea846685b

--- 3.3/web/src/org/sipfoundry/sipxconfig/site/upload/ManageUploads.java
+++ 3.3/web/src/org/sipfoundry/sipxconfig/site/upload/ManageUploads.java
@@ -15,6 +15,7 @@ import java.util.Collection;
 
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.event.PageBeginRenderListener;
+import org.apache.tapestry.event.PageEndRenderListener;
 import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.html.BasePage;
 import org.sipfoundry.sipxconfig.common.DaoUtils;
@@ -24,7 +25,8 @@ import org.sipfoundry.sipxconfig.upload.Upload;
 import org.sipfoundry.sipxconfig.upload.UploadManager;
 import org.sipfoundry.sipxconfig.upload.UploadSpecification;
 
-public abstract class ManageUploads extends BasePage implements PageBeginRenderListener {
+public abstract class ManageUploads extends BasePage 
+        implements PageBeginRenderListener, PageEndRenderListener {
     
     public static final String PAGE = "ManageUploads";
 
