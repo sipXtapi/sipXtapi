f9f7d31ff8c1ae6aaa00889d9fdb3a69754a76da
Douglas Hubler <douglas@hubler.us>
2006-03-14 18:41:48 +0000

    XCF-951 - Show how to change pin and aliases from existing SOAP API, will add convience method in main line (3.4)
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/3.2@6354 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/meta/sample_soap.pl
+++ sipXconfig/web/meta/sample_soap.pl
@@ -7,11 +7,18 @@ use SOAP::Lite;
 
 # helpful when debugging SOAP messages on the wire
 # use SOAP::Lite +trace;
-
+use Digest::MD5 qw(md5_hex); 
 use strict;
 use warnings;
 
-my $services_url = 'https://localhost:8443/sipxconfig/services';
+# Where your server is installed, 
+# CHANGE ME: probably not localhost!
+my $services_url = 'https://eagle.sipfoundry.org:8443/sipxconfig/services';
+
+# be sure this matched config.defs on the server, generally it's the domain name
+# CHANGE ME: it can be anything, and probably not this!
+my $realm = 'sipfoundry.com';
+
 
 sub SOAP::Transport::HTTP::Client::get_basic_credentials { 
    return 'superadmin' => '1234';
@@ -23,14 +30,35 @@ my $user_service = SOAP::Lite
 
 foreach my $i (1 .. 10) {
   my $username = "roberta-$i";
-  my $group = "soap-test";
+  my $group = "employee";
 
   # delete to ensure this is a new user, you can always find and edit too
   delete_user(username => $username);
 
-  add_user(username => $username, pin => '1234', sipPassword => 'DFad56sd!', groups => $group);
+  add_user(username => $username, pin => '4567', sipPassword => 'DFad56sd!', groups => $group, 
+	   aliases => '345' + $i);
+
+  change_pin(username => $username, pin => '1234');
+ 
+  add_group(username => $username, group => "building-1");
 }
 
+
+sub change_pin {
+    my %params = @_;
+    my ($username) = $params{username};
+    my ($pin) = $params{pin};
+
+    my $pintoken = &md5_hex("$username:$realm:$pin");
+
+    my $change_pintoken = SOAP::Data->name(edit =>
+       \SOAP::Data->value(
+	SOAP::Data->type('string')->name(property => 'pintoken'),
+	SOAP::Data->type('string')->name(value => $pintoken)));
+
+    print "changing pintoken for $username to $pintoken\n";
+    manage_user(username => $username, action => $change_pintoken);    
+}
 
 sub find_user {
     my %params = @_;
@@ -39,7 +67,7 @@ sub find_user {
     my $findUser = SOAP::Data->name('FindUser')
       ->attr({xmlns => 'urn:ConfigService'});
 
-    my $byUserName = SOAP::Data->name('byUserName')->value($username);
+    my $byUserName = SOAP::Data->type('string')->name('byUserName')->value($username);
     my $result = $user_service->call($findUser => SOAP::Data->name(search => \$byUserName));
     check_fault($result);
 
@@ -52,12 +80,14 @@ sub add_user {
     my ($pin) = $params{pin};
     my ($sipPassword) = $params{sipPassword};
     my ($groups) = $params{groups};
+    my ($aliases) = $params{aliases};
 
     my $newUser = SOAP::Data->name(user =>
        \SOAP::Data->value(
-	SOAP::Data->name(userName => $username),
-	SOAP::Data->name(sipPassword => $sipPassword),
-        SOAP::Data->name(groups => $groups)));
+	SOAP::Data->type('string')->name(userName => $username),
+	SOAP::Data->type('string')->name(sipPassword => $sipPassword),
+        SOAP::Data->type('string')->name(groups => $groups),
+        SOAP::Data->type('string')->name(aliases => $aliases)));
 
     my $pinData = SOAP::Data->type('string')->name(pin => $pin);
 
@@ -107,7 +137,7 @@ sub manage_user {
     my ($username) = $params{username};
     my ($action) = $params{action};
 
-    my $byUserName = SOAP::Data->name('byUserName')->value($username);
+    my $byUserName = SOAP::Data->type('string')->name('byUserName')->value($username);
     my $search = SOAP::Data->name(search => \$byUserName);
 
     my $manageUser = SOAP::Data->name('ManageUser')
