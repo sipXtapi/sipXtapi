b04d1f62b18b68b25b6d69db2de18b3489d45ad6
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-03-16 22:39:41 +0000

    - added EnumFormat - supports localized display of "commons" Enum objects - added MillisDurationFormat - supports human readable display of duration expressed in millis (i.e. something like 10 hours, 5 minutes) - added AutoRefreshForm component - special form that automatically refreshes its content - you can specify interval or refresh it on demand
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@6382 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/components/MillisDurationFormatTest.java
@@ -0,0 +1,83 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2006 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2006 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.components;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.lang.time.DateUtils;
+
+public class MillisDurationFormatTest extends TestCase {
+    private MillisDurationFormat m_format;
+
+    protected void setUp() throws Exception {
+        m_format = new MillisDurationFormat();
+    }
+
+    public void testFormat() {
+        assertEquals("0 seconds", m_format.format(new Long(0)));
+        assertEquals("0 seconds", m_format.format(new Long(1)));
+        assertEquals("0 seconds", m_format.format(new Long(999)));
+        assertEquals("1 second", m_format.format(new Long(1000)));
+
+        assertEquals("5 seconds", m_format.format(new Long(5000)));
+        assertEquals("5 seconds", m_format.format(new Long(5999)));
+
+        assertEquals("1 minute, 5 seconds", m_format.format(new Long(60000 + 5000)));
+
+        assertEquals("3 hours, 0 minutes, 5 seconds", m_format.format(new Long(
+                3 * DateUtils.MILLIS_PER_HOUR + 5050)));
+        assertEquals("5 days, 0 hours, 1 minute, 7 seconds", m_format.format(new Long(5
+                * DateUtils.MILLIS_PER_DAY + DateUtils.MILLIS_PER_MINUTE + 7070)));
+    }
+
+    public void testFormatSeconds() {
+        m_format.setMaxField(MillisDurationFormat.SECONDS);
+        assertEquals("0 seconds", m_format.format(new Long(0)));
+        assertEquals("0 seconds", m_format.format(new Long(1)));
+        assertEquals("0 seconds", m_format.format(new Long(999)));
+        assertEquals("1 second", m_format.format(new Long(1000)));
+
+        assertEquals("5 seconds", m_format.format(new Long(5000)));
+        assertEquals("5 seconds", m_format.format(new Long(5999)));
+
+        assertEquals("65 seconds", m_format.format(new Long(60000 + 5000)));
+
+        assertEquals("10,805 seconds", m_format.format(new Long(
+                3 * DateUtils.MILLIS_PER_HOUR + 5050)));
+        assertEquals("432,067 seconds", m_format.format(new Long(5 * DateUtils.MILLIS_PER_DAY
+                + DateUtils.MILLIS_PER_MINUTE + 7070)));
+    }
+
+    public void testFormatNoLabels() {
+        m_format.setMaxField(MillisDurationFormat.SECONDS);
+        m_format.setUseLabels(false);
+        assertEquals("0", m_format.format(new Long(0)));
+        assertEquals("1", m_format.format(new Long(1000)));
+
+        assertEquals("5", m_format.format(new Long(5000)));
+        assertEquals("5", m_format.format(new Long(5999)));
+
+        assertEquals("65", m_format.format(new Long(60000 + 5000)));
+
+        assertEquals("10,805", m_format.format(new Long(3 * DateUtils.MILLIS_PER_HOUR + 5050)));
+        assertEquals("432,067", m_format.format(new Long(5 * DateUtils.MILLIS_PER_DAY
+                + DateUtils.MILLIS_PER_MINUTE + 7070)));
+    }
+
+    public void testFormatSeparator() {
+        m_format.setMaxField(MillisDurationFormat.HOURS);
+        m_format.setUseLabels(false);
+        m_format.setSeparator(":");
+        assertEquals("0", m_format.format(new Long(0)));
+        assertEquals("3:0:5", m_format.format(new Long(3 * DateUtils.MILLIS_PER_HOUR + 5050)));
+    }
+}
