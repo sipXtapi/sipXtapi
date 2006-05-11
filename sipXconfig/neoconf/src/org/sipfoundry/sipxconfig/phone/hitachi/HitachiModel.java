269e105aa936d4140df5bab3e411c76f6bc7c4af
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-05-11 22:52:24 +0000

    Jumpstarting HITACHI Cable phones support for Michał. - IP 3000 and IP 5000 model defined - empty HitachiPhone class - nearly empty settings files for phone and line They show up in New Phone dropdown now.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@6815 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/phone/hitachi/HitachiModel.java
@@ -0,0 +1,26 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2006 SIPfoundry Inc.
+ * Licensed by SIPfoundry under the LGPL license.
+ * 
+ * Copyright (C) 2006 Pingtel Corp.
+ * Licensed to SIPfoundry under a Contributor Agreement.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.phone.hitachi;
+
+import org.sipfoundry.sipxconfig.phone.PhoneModel;
+
+public final class HitachiModel extends PhoneModel {
+    public static final HitachiModel MODEL_3000 = new HitachiModel("3000",
+            "Hitachi Wireless IP 3000", 1);
+
+    public static final HitachiModel MODEL_5000 = new HitachiModel("5000",
+            " Hitachi Wireless IP 5000", 1);
+    
+    private HitachiModel(String modelId, String label, int maxLines) {
+        super(HitachiPhone.BEAN_ID, modelId, label, maxLines);
+    }    
+}
