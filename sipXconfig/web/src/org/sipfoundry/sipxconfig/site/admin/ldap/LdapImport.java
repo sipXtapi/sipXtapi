961ceec6f58babd5cf8b54eb6c1ca6bec32c725b
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-05-31 17:43:28 +0000

    XCF-829 Basic UI for scheduling LDAP updates.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@6880 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/ldap/LdapImport.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/ldap/LdapImport.java
@@ -11,17 +11,38 @@
  */
 package org.sipfoundry.sipxconfig.site.admin.ldap;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.apache.tapestry.AbstractPage;
+import org.apache.tapestry.event.PageBeginRenderListener;
+import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.html.BasePage;
+import org.sipfoundry.sipxconfig.admin.DailyBackupSchedule;
 import org.sipfoundry.sipxconfig.bulk.ldap.LdapImportManager;
 import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
 import org.sipfoundry.sipxconfig.components.TapestryUtils;
 
-public abstract class LdapImport extends BasePage {
+public abstract class LdapImport extends BasePage implements PageBeginRenderListener {
 
     public static final String PAGE = "LdapImport";
 
     public abstract LdapImportManager getLdapImportManager();
+
+    public abstract Collection<DailyBackupSchedule> getSchedules();
+
+    public abstract void setSchedules(Collection<DailyBackupSchedule> schedules);
+
+    public void pageBeginRender(PageEvent event) {
+        if (getSchedules() == null) {
+            // temporary - add 3 daily backup schedules
+            ArrayList<DailyBackupSchedule> schedules = new ArrayList<DailyBackupSchedule>();
+            schedules.add(new DailyBackupSchedule());
+            schedules.add(new DailyBackupSchedule());
+            schedules.add(new DailyBackupSchedule());
+            setSchedules(schedules);
+        }
+    }
 
     public void importLdap() {
         if (!TapestryUtils.isValid((AbstractPage) getPage())) {
@@ -32,7 +53,7 @@ public abstract class LdapImport extends BasePage {
                 .getValidator(this);
         validator.recordSuccess(getMessages().getMessage("msg.success"));
     }
-    
+
     public String verifyLdap() {
         return LdapImportPreview.PAGE;
     }
