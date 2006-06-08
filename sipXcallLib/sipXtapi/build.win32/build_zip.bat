73142f1f09b2430a56765f4b4edbb730b74856ca
Bob Andreasen <bandreasen@bpsoft.com>
2006-06-08 19:46:48 +0000

    Tweaked the build_zip and stage_inst batch files for this branch -- targets for binaries have moved since these were last touched.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@6940 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXcallLib/sipXtapi/build.win32/build_zip.bat
+++ sipXcallLib/sipXtapi/build.win32/build_zip.bat
@@ -9,13 +9,7 @@ IF NOT "%SIPXTAPI_WINZIP_BASE%" == "" GOTO BUILD_IT
 SET SIPXTAPI_WINZIP_BASE=C:\Program Files\WinZip
 
 :BUILD_IT
-
-IF "%1" == "CML" GOTO SET_CML
-GOTO BUILD_ZIP
-
-:SET_CML
-SET TARGET_NAME=sipXtapi_WIN32_%2_%date:~10,4%-%date:~4,2%-%date:~7,2%-CML.zip
-GOTO BUILD_ZIP
+SET TARGET_NAME=sipXtapi_WIN32_%date:~10,4%-%date:~4,2%-%date:~7,2%.zip
 
 :BUILD_ZIP
-  "%SIPXTAPI_WINZIP_BASE%\wzzip" -rp %TARGET_NAME% staging\
+"%SIPXTAPI_WINZIP_BASE%\wzzip" -rp %TARGET_NAME% staging\
