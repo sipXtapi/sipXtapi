34923e46341fcd1db1ac643de2d99ae0893f009c
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-07-16 21:23:42 +0000

    Hitachi IP5000  configuration files provided by Martin - added generating TFTP profile - several basic settings modeled (line info, proxy, registrar, realm etc.) Still need to: - model remaining settings - add Velocity expressions to user.ini.vm - write tests (plugins tests that require application context only work from Eclipse now - classpath problems)
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@7086 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/plugins/hitachi/src/org/sipfoundry/sipxconfig/phone/hitachi/HitachiPhone.java
+++ sipXconfig/plugins/hitachi/src/org/sipfoundry/sipxconfig/phone/hitachi/HitachiPhone.java
@@ -11,16 +11,80 @@
  */
 package org.sipfoundry.sipxconfig.phone.hitachi;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+
+import org.apache.commons.io.IOUtils;
+import org.sipfoundry.sipxconfig.phone.Line;
 import org.sipfoundry.sipxconfig.phone.Phone;
 
 public class HitachiPhone extends Phone {
     public static final String BEAN_ID = "hitachi";
-    
+
     public HitachiPhone() {
         super(BEAN_ID);
     }
 
     public HitachiPhone(HitachiModel model) {
         super(model);
+    }
+
+    public String getLoadrunTemplate() {
+        return "hitachi/loadrun.ini";
+    }
+
+    @Override
+    public String getPhoneTemplate() {
+        return "hitachi/user.ini.vm";
+    }
+
+    @Override
+    public void initializeLine(Line line) {
+        HitachiLineDefaults defaults = new HitachiLineDefaults(line);
+        addDefaultBeanSettingHandler(defaults);
+    }
+
+    @Override
+    public void initialize() {
+        HitachiPhoneDefaults defaults = new HitachiPhoneDefaults(getPhoneContext()
+                .getPhoneDefaults());
+        addDefaultBeanSettingHandler(defaults);
+    }
+
+    @Override
+    public void generateProfiles() {
+        super.generateProfiles();
+        // and copy loadrun.ini as well
+        generateLoadrunIni(getTftpRoot());
+    }
+
+    void generateLoadrunIni(String dstDir) {
+        Writer loadrunIniWriter = null;
+        try {
+            InputStream loadrunIniTemplate = getClass().getClassLoader().getResourceAsStream(
+                    getLoadrunTemplate());
+            File loadrunIniFile = new File(dstDir, "loadrun.ini");
+            loadrunIniWriter = new FileWriter(loadrunIniFile);
+            IOUtils.copy(loadrunIniTemplate, loadrunIniWriter);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot generate profiles for " + getSerialNumber(), e);
+        } finally {
+            IOUtils.closeQuietly(loadrunIniWriter);
+        }
+    }
+
+    /**
+     * Check loadrun.ini section [3] for a proper format of the file name.
+     * 
+     * %muser.ini means “3 bytes of MAC address + user.ini”
+     */
+    @Override
+    public String getPhoneFilename() {
+        String serialNumber = getSerialNumber();
+        String prefix = serialNumber.substring(6);
+        return getTftpRoot() + "/" + prefix + "user.ini";
     }
 }
--- sipXconfig/plugins/hitachi/test/org/sipfoundry/sipxconfig/phone/hitachi/HitachiPhoneTest.java
+++ sipXconfig/plugins/hitachi/test/org/sipfoundry/sipxconfig/phone/hitachi/HitachiPhoneTest.java
@@ -15,11 +15,18 @@ import junit.framework.TestCase;
 
 public class HitachiPhoneTest extends TestCase {
     public void testFactoryRegistered() {
-        // FIXME: TestHelper is not accesible here - need to find a way of loading application context
-//        PhoneContext pc = (PhoneContext) TestHelper.getApplicationContext().getBean(
-//             PhoneContext.CONTEXT_BEAN_NAME);
-//        assertNotNull(pc.newPhone(HitachiModel.MODEL_3000));
-//        assertNotNull(pc.newPhone(HitachiModel.MODEL_5000));
-    }    
+        // FIXME: TestHelper is not accesible here - need to find a way of loading application
+        // context
+        // PhoneContext pc = (PhoneContext) TestHelper.getApplicationContext().getBean(
+        // PhoneContext.CONTEXT_BEAN_NAME);
+        // assertNotNull(pc.newPhone(HitachiModel.MODEL_3000));
+        // assertNotNull(pc.newPhone(HitachiModel.MODEL_5000));
+    }
 
+    public void testGetFileName() throws Exception {
+        HitachiPhone phone = new HitachiPhone();
+        phone.setSerialNumber("001122334455");
+        phone.setTftpRoot("abc");
+        assertEquals("abc/334455user.ini", phone.getPhoneFilename());
+    }
 }
