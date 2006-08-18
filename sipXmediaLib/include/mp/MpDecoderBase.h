f9a44b451a3120dc4d7096501425a01aa58fa9ba
Alexander Chemeris <alexander.chemeris@gmail.com>
2006-08-18 08:25:51 +0000

    Little documentation typo.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi-media-update@7291 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/include/mp/MpDecoderBase.h
+++ sipXmediaLib/include/mp/MpDecoderBase.h
@@ -57,7 +57,7 @@ public:
      /// Initializes a codec data structure for use as a decoder
    virtual OsStatus initDecode(MpConnection* pConnection)=0;
      /**<
-     //!param: pConnection - (in) Pointer to the MpConnection container
+     *  @param pConnection - (in) Pointer to the MpConnection container
      *  @returns <b>OS_SUCCESS</b> - Success
      *  @returns <b>OS_NO_MEMORY</b> - Memory allocation failure
      */
