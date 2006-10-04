25908b28784fdc31e5e32afd552b612740da7df1
Dale Worley <dworley@pingtel.com>
2006-10-04 20:28:05 +0000

    Minor update in GRUU processing.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/user_dworley_redirectors@7652 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXregistry/src/SipRegistrarServer.cpp
+++ sipXregistry/src/SipRegistrarServer.cpp
@@ -332,12 +332,6 @@ SipRegistrarServer::applyRegisterToDirectory( const Url& toUrl
                            longestRequested = expires;
                         }
 
-                        // See if the Contact field has a "gruu" URI field..
-                        // :TODO: Have to check whether the gruu URI parameter
-                        // is still favored.  And what exactly this check is about.
-                        UtlBoolean gruuPresent;
-                        UtlString gruuDummy;
-                        gruuPresent = registerContactURL.getUrlParameter( "gruu", gruuDummy );
                         OsSysLog::add( FAC_SIP, PRI_DEBUG,
                                        "SipRegistrarServer::applyRegisterToDirectory"
                                        " instance ID = '%s'",
@@ -367,10 +361,8 @@ SipRegistrarServer::applyRegisterToDirectory( const Url& toUrl
                         UtlString* instanceIdKey = new UtlString( gInstanceIdKey );
                         UtlString* gruuKey = new UtlString( gGruuKey );
 
-                        // Calculate GRUU if gruu is in Supported, +sip.instance is provided, but
-                        // gruu is not a URI parameter.
+                        // Calculate GRUU if gruu is in Supported and +sip.instance is provided.
                         if (!instanceId.isNull() &&
-                            !gruuPresent &&
                             registerMessage.isInSupportedField("gruu"))
                         {
                            // Hash the GRUU base, the AOR, and IID to
