c3a0bfb50c107c3e02f7c7bfc2c168430859279c
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-10-12 14:13:24 +0000

    XCF-1319 MusicOnHold.saveValid was calling itself recursively! - fixed and added UI test
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/3.6@7745 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/test/org/sipfoundry/sipxconfig/site/admin/MusicOnHoldTestUi.java
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/site/admin/MusicOnHoldTestUi.java
@@ -11,6 +11,8 @@
  */
 package org.sipfoundry.sipxconfig.site.admin;
 
+import java.io.File;
+
 import junit.framework.Test;
 import net.sourceforge.jwebunit.WebTestCase;
 
@@ -21,13 +23,20 @@ public class MusicOnHoldTestUi extends WebTestCase {
         return SiteTestHelper.webTestSuite(MusicOnHoldTestUi.class);
     }
 
-    public void setUp() {
+    private File m_tempFile;
+
+    public void setUp() throws Exception {
         getTestContext().setBaseUrl(SiteTestHelper.getBaseUrl());
         SiteTestHelper.home(getTester());
+        m_tempFile = File.createTempFile("MusicOnHoldTestUi", null);
     }
 
     public void testDisplay() throws Exception {
         clickLink("MusicOnHold");
         SiteTestHelper.assertNoException(tester);
+        getDialog().getForm().setParameter("promptUpload", m_tempFile);
+        clickButton("form:apply");
+        SiteTestHelper.assertNoUserError(tester);
+        assertOptionEquals("prompt", m_tempFile.getName());
     }
 }
