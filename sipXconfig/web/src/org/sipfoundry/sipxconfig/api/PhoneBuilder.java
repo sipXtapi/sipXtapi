1d4253899125882d31f1ceb0237cf1af9b29ef22
Douglas Hubler <douglas@hubler.us>
2006-11-07 20:56:54 +0000

    XCF-1367 - Make deviceId available from SOAP
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/3.6@7954 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/api/PhoneBuilder.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/api/PhoneBuilder.java
@@ -18,21 +18,23 @@ import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.sipfoundry.sipxconfig.common.NamedObject;
+import org.sipfoundry.sipxconfig.device.DeviceVersion;
 import org.sipfoundry.sipxconfig.phone.LineInfo;
 
-public class PhoneBuilder extends SimpleBeanBuilder {    
-    private static final String MODEL_ID_PROP = "modelId"; 
-    private static final String GROUPS_PROP = "groups"; 
-    private static final String LINES_PROP = "lines"; 
-    
-    private static final String[] CUSTOM_FIELDS = { 
-        MODEL_ID_PROP, GROUPS_PROP, LINES_PROP
+public class PhoneBuilder extends SimpleBeanBuilder {
+    private static final String MODEL_ID_PROP = "modelId";
+    private static final String GROUPS_PROP = "groups";
+    private static final String LINES_PROP = "lines";
+    private static final String DEVICE_VER = "deviceVersion";
+
+    private static final String[] CUSTOM_FIELDS = {
+        MODEL_ID_PROP, GROUPS_PROP, LINES_PROP, DEVICE_VER
     };
-    
+
     public PhoneBuilder() {
-        getCustomFields().addAll(Arrays.asList(CUSTOM_FIELDS));        
+        getCustomFields().addAll(Arrays.asList(CUSTOM_FIELDS));
     }
-    
+
     public void toApiObject(Object apiObject, Object myObject, Set properties) {
         super.toApiObject(apiObject, myObject, properties);
         Phone phone = (Phone) apiObject;
@@ -41,15 +43,17 @@ public class PhoneBuilder extends SimpleBeanBuilder {
             phone.setModelId(otherPhone.getModelId());
         }
         if (properties.contains(GROUPS_PROP)) {
-            Collection groupNames = CollectionUtils.collect(otherPhone.getGroups(), new NamedObject.ToName());
+            Collection groupNames = CollectionUtils.collect(otherPhone.getGroups(),
+                    new NamedObject.ToName());
             phone.setGroups((String[]) groupNames.toArray(new String[groupNames.size()]));
         }
         if (properties.contains(LINES_PROP)) {
             List myLines = otherPhone.getLines();
             if (myLines.size() > 0) {
-                Line[] apiLines = (Line[]) ApiBeanUtil.newArray(Line.class, myLines.size());            
+                Line[] apiLines = (Line[]) ApiBeanUtil.newArray(Line.class, myLines.size());
                 for (int i = 0; i < apiLines.length; i++) {
-                    org.sipfoundry.sipxconfig.phone.Line myLine = (org.sipfoundry.sipxconfig.phone.Line) myLines.get(i);
+                    org.sipfoundry.sipxconfig.phone.Line myLine = (org.sipfoundry.sipxconfig.phone.Line) myLines
+                            .get(i);
                     LineInfo lineInfo = myLine.getLineInfo();
                     apiLines[i].setUserId(lineInfo.getUserId());
                     apiLines[i].setUri(myLine.getUri());
@@ -57,9 +61,23 @@ public class PhoneBuilder extends SimpleBeanBuilder {
                 phone.setLines(apiLines);
             }
         }
+        DeviceVersion ver = otherPhone.getDeviceVersion();
+        if (ver != null && properties.contains(DEVICE_VER)) {
+            phone.setDeviceVersion(ver.getName());
+        }
     }
 
     public void toMyObject(Object myObject, Object apiObject, Set properties) {
         super.toMyObject(myObject, apiObject, properties);
-    }    
+        Phone phone = (Phone) apiObject;
+        org.sipfoundry.sipxconfig.phone.Phone otherPhone = (org.sipfoundry.sipxconfig.phone.Phone) myObject;
+        String verId = phone.getDeviceVersion();
+        if (verId != null) {
+            DeviceVersion ver = DeviceVersion.getDeviceVersion(verId);
+            if (ver == null) {
+                throw new RuntimeException(String.format("Invalid device version id '%s'", verId));
+            }
+            otherPhone.setDeviceVersion(ver);
+        }
+    }
 }
