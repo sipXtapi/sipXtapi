57c144b45ac2743cddcf143903254e6f2c2d8ff6
Douglas Hubler <douglas@hubler.us>
2006-11-23 04:33:41 +0000

    Fix sipdb-dump to to delim on tab char, add more tests, add more examples to man page
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@8088 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXtools/src/test/sipdb-dump_test.rb
+++ sipXtools/src/test/sipdb-dump_test.rb
@@ -30,24 +30,36 @@ class SipdbDumpTest < Test::Unit::TestCase
        "instance_id=",
        "gruu=",
        "update_number=0x456232bf00000287"]
-      assert_equal expected, out.string.strip.split('\n')
+      assert_equal expected, out.string.strip.split("\t")
   end
   
-  def test_contact_host
+  def test_contact_host_1
       out = StringIO.new
       SipDbDumper.new().dump_contact(out, "contact", "&quot;D K&quot;&lt;sip:154@10.1.1.164;LINEID=7bfd&gt;")
-      assert_equal 'contact=&quot;D K&quot;&lt;sip:154@10.1.1.164;LINEID=7bfd&gt;\ncontact_host=10.1.1.164\n', out.string
+      assert_equal "contact=&quot;D K&quot;&lt;sip:154@10.1.1.164;LINEID=7bfd&gt;\tcontact_host=10.1.1.164\t", out.string
   end
   
-  def test_expires
+  def test_contact_host_2
+      out = StringIO.new
+      SipDbDumper.new().dump_contact(out, "contact", "&lt;sip:154@10.1.1.164&gt;")
+      assert_equal "contact=&lt;sip:154@10.1.1.164&gt;\tcontact_host=10.1.1.164\t", out.string
+  end
+  
+  def test_expired_true
       out = StringIO.new
       SipDbDumper.new().dump_expires(out, "expires", "1")
-      assert_equal 'expires=1\nexpired=true\n', out.string    
-
-      out2 = StringIO.new
+      assert_equal "expires=1\texpired=true\t", out.string    
+  end
+  
+  def test_expired_false
+      out = StringIO.new
       future = Time.now.to_i + 100
-      SipDbDumper.new().dump_expires(out2, "expires", future)
-      assert_equal "expires=#{future}\\nexpired=false\\n", out2.string    
+      SipDbDumper.new().dump_expires(out, "expires", future)
+      assert_equal "expires=#{future}\texpired=false\t", out.string    
+  end
+  
+  def test_cli
+    assert Regexp.new('data dumper', Regexp::MULTILINE).match(`#{DIR}/../sipdb-dump --help`)
   end
   
 end
