e0433568fd68eb1546991fe48fa9380c8b92204a
Damian Krzeminski <dkrzeminski@pingtel.com>
2006-12-12 21:43:38 +0000

    XCF-1312 model localization fix - localized names not pickup up when rendering links for group settings
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@8242 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/setting/SettingsNavigation.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/setting/SettingsNavigation.java
@@ -12,11 +12,42 @@
 package org.sipfoundry.sipxconfig.site.setting;
 
 import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.IRequestCycle;
+import org.sipfoundry.sipxconfig.setting.Setting;
+import org.springframework.context.MessageSource;
 
 public abstract class SettingsNavigation extends BaseComponent {
+    public abstract Setting getSettings();
+
+    public abstract Setting getCurrentSetting();
+
+    public abstract MessageSource getMessageSource();
+
+    public abstract void setMessageSource(MessageSource messageSource);
+
     public abstract void setTab(String section);
 
     public void activateTab(String section) {
         setTab(section);
+    }
+
+    protected void prepareForRender(IRequestCycle cycle) {
+        super.prepareForRender(cycle);
+        // set message source only once and save it into property so that we do not have to
+        // compute it every time
+        if (getMessageSource() == null) {
+            setMessageSource(getSettings().getMessageSource());
+        }
+    }
+
+    public String getLabel() {
+        Setting setting = getCurrentSetting();
+        MessageSource modelMessages = getMessageSource();
+        String defaultMessage = setting.getLabel();
+        if (modelMessages != null) {
+            return modelMessages.getMessage(setting.getLabelKey(), null, defaultMessage,
+                    getPage().getLocale());
+        }
+        return defaultMessage;
     }
 }
