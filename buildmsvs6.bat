56e939a431af2f0c8afd3986471afd37e2518120
Keith Kyzivat <kkyzivat@sipez.com>
2007-01-11 19:15:43 +0000

    Fix for msvc6 automated build.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@8514 a612230a-c5fa-0310-af8b-88eea846685b

--- buildmsvs6.bat
+++ buildmsvs6.bat
@@ -16,15 +16,21 @@ if "%1" == "clean" set doClean="/CLEAN"
 @msdev /? > nul
 @rem error level: %ERRORLEVEL%
 @if %ERRORLEVEL% GTR 0 call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32"
+@rem Add include and library specifiers to the environment, since for some
+@rem reason, VMware execution of a guest command via their api doesn't seem
+@rem to allow access to HKCU properly ("default" user probably)
+set DEPD=c:\build\sipx-depends
+set INCLUDE=%DEPD%\cppunit-1.10.2\include;%DEPD%\pcre\include;%DEPD%\openssl\inc32;%INCLUDE%
+set LIB=%DEPD%\cppunit-1.10.2\lib;%DEPD%\pcre\lib;%DEPD%\openssl\out32.dbg;%LIB%
 
 cd sipXportLib
 del %releaseType%\sipXportLib%libPrefix%.lib
-msdev sipXportLib.dsp /MAKE "sipXportLib - Win32 %releaseType%" %doClean%
+msdev sipXportLib.dsp /USEENV /MAKE "sipXportLib - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXportLib
 del %releaseType%\sipXportLibTest.exe
-msdev sipXportLibTest.dsp /MAKE "sipXportLibTest - Win32 %releaseType%" %doClean%
+msdev sipXportLibTest.dsp /USEENV /MAKE "sipXportLibTest - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXsdpLib
@@ -39,74 +45,74 @@ cd ..
 
 cd sipXtackLib
 del %releaseType%\sipXtackLib%libPrefix%.lib
-msdev sipXtackLib.dsp /MAKE "sipXtackLib - Win32 %releaseType%" %doClean%
+msdev sipXtackLib.dsp /USEENV /MAKE "sipXtackLib - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXtackLib
 del %releaseType%\sipXtackLibTest.exe
-msdev sipXtackLibTest.dsp /MAKE "sipXtackLibTest - Win32 %releaseType%" %doClean%
+msdev sipXtackLibTest.dsp /USEENV /MAKE "sipXtackLibTest - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXmediaLib
 del %releaseType%\sipXmediaLib%libPrefix%.lib
-msdev sipXmediaLib.dsp /MAKE "sipXmediaLib - Win32 %releaseType%" %doClean%
+msdev sipXmediaLib.dsp /USEENV /MAKE "sipXmediaLib - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXmediaLib
 del %releaseType%\sipXmediaLibTest.exe
-msdev sipXmediaLibTest.dsp /MAKE "sipXmediaLibTest - Win32 %releaseType%" %doClean%
+msdev sipXmediaLibTest.dsp /USEENV /MAKE "sipXmediaLibTest - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXmediaAdapterLib
 del %releaseType%\sipXmediaAdapterLib%libPrefix%.lib
-msdev sipXmediaAdapterLib.dsp /MAKE "sipXmediaAdapterLib - Win32 %releaseType%" %doClean%
+msdev sipXmediaAdapterLib.dsp /USEENV /MAKE "sipXmediaAdapterLib - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXmediaAdapterLib
 del %releaseType%\sipXmediaAdapterLibTest.exe
-msdev sipXmediaAdapterLibTest.dsp /MAKE "sipXmediaAdapterLibTest - Win32 %releaseType%" %doClean%
+msdev sipXmediaAdapterLibTest.dsp /USEENV /MAKE "sipXmediaAdapterLibTest - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXcallLib 
 del %releaseType%\sipXcallLib%libPrefix%.lib
-msdev sipXcallLib.dsp /MAKE "sipXcallLib - Win32 %releaseType%" %doClean%
+msdev sipXcallLib.dsp /USEENV /MAKE "sipXcallLib - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXcallLib 
 del %releaseType%\sipXcallLibTest.exe
-msdev sipXcallLibTest.dsp /MAKE "sipXcallLibTest - Win32 %releaseType%" %doClean%
+msdev sipXcallLibTest.dsp /USEENV /MAKE "sipXcallLibTest - Win32 %releaseType%" %doClean%
 cd ..
 
 cd sipXcallLib 
 del sipXtapi\%releaseType%\sipXtapiTest.exe
-msdev sipXtapiTest.dsp /MAKE "sipXtapiTest - Win32 %releaseType%" %doClean%
+msdev sipXtapiTest.dsp /USEENV /MAKE "sipXtapiTest - Win32 %releaseType%" %doClean%
 cd ..
 
 goto end
 
 cd sipXcallLib\examples\sipXezPhone 
 del Debug\sipXezPhone.exe
-msdev sipXezPhone.dsp /MAKE "sipXezPhone - Win32 Debug" %doClean%
+msdev sipXezPhone.dsp /USEENV /MAKE "sipXezPhone - Win32 Debug" %doClean%
 cd ..\..\..
 
 cd sipXcommserverLib 
 del Debug\sipXcommserverLibd.lib
-msdev sipXcommserverLib.dsp /MAKE "sipXcommserverLib - Win32 Debug" %doClean%
+msdev sipXcommserverLib.dsp /USEENV /MAKE "sipXcommserverLib - Win32 Debug" %doClean%
 cd ..
 
 cd sipXregistry 
 del Debug\sipXregistry.exe
-msdev sipXregistry.dsp /MAKE "sipXregistry - Win32 Debug" %doClean%
+msdev sipXregistry.dsp /USEENV /MAKE "sipXregistry - Win32 Debug" %doClean%
 cd ..
 
 cd sipXproxy
 del Debug\sipXforkingProxy.exe
-msdev sipXforkingProxy.dsp /MAKE "sipXforkingProxy - Win32 Debug" %doClean%
+msdev sipXforkingProxy.dsp /USEENV /MAKE "sipXforkingProxy - Win32 Debug" %doClean%
 cd ..
 
 cd sipXproxy
 del Debug\sipXauthproxy.exe
-msdev sipXauthproxy.dsp /MAKE "sipXauthproxy - Win32 Debug" %doClean%
+msdev sipXauthproxy.dsp /USEENV /MAKE "sipXauthproxy - Win32 Debug" %doClean%
 cd ..
 
 :end
