2f206c0aa74496a91ed4aaf964aee5e1c0e11c82
Alexander Chemeris <alexander.chemeris@gmail.com>
2007-02-09 15:17:57 +0000

    Bugfix: Fix bug introduced in rev8293. Due to mistype bRC was set to FALSE instead of TRUE at line 1935 of CpPhoneMediaInterface.cpp (CpPhoneMediaInterface::getNatedAddresses() function).
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@8736 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaAdapterLib/sipXmediaMediaProcessing/src/CpPhoneMediaInterface.cpp
+++ sipXmediaAdapterLib/sipXmediaMediaProcessing/src/CpPhoneMediaInterface.cpp
@@ -1892,7 +1892,7 @@ UtlBoolean CpPhoneMediaInterface::getNatedAddresses(int connectionId,
                     hostIp = host ;
                     rtpAudioPort = port ;
 
-                    bRC = FALSE ;
+                    bRC = TRUE ;
                 }
             
                 // Audio rtcp port (optional) 
