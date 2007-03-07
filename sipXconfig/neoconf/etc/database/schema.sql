f10e43041909d879da0304019ccb24dcc38ff2fc
Douglas Hubler <douglas@hubler.us>
2007-03-07 21:50:30 +0000

    remove afterhour init task from 5.0 schema, will badd back in 6.0
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@9000 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/neoconf/etc/database/schema.sql
+++ sipXconfig/neoconf/etc/database/schema.sql
@@ -602,4 +602,3 @@ insert into initialization_task (name) values ('default-phone-group');
 insert into initialization_task (name) values ('add_default_user_group');
 insert into initialization_task (name) values ('operator');
 insert into initialization_task (name) values ('initialize-domain');
-insert into initialization_task (name) values ('afterhour');
