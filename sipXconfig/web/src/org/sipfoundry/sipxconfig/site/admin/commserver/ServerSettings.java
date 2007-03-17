265839fef543f9f109169847c1298a552ac687ae
Damian Krzeminski <dkrzeminski@pingtel.com>
2007-03-17 03:47:06 +0000

    localization support for Server settings group description
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@9105 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/commserver/ServerSettings.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/commserver/ServerSettings.java
@@ -12,19 +12,38 @@
 package org.sipfoundry.sipxconfig.site.admin.commserver;
 
 import org.apache.tapestry.IPage;
-import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.annotations.Bean;
+import org.apache.tapestry.annotations.InitialValue;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.InjectPage;
+import org.apache.tapestry.annotations.Persist;
 import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.html.BasePage;
 import org.sipfoundry.sipxconfig.admin.commserver.Server;
+import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
+import org.sipfoundry.sipxconfig.components.TapestryUtils;
 import org.sipfoundry.sipxconfig.setting.Setting;
 
+/**
+ * Page presenting sipX server settings: in most cases obtained by reading values from
+ * configuration files in /etc/sipxpbx directory
+ */
 public abstract class ServerSettings extends BasePage implements PageBeginRenderListener {
     public static final String PAGE = "ServerSettings";
 
+    @InjectObject(value = "spring:sipxServer")
     public abstract Server getServer();
 
+    @Bean
+    public abstract SipxValidationDelegate getValidator();
+
+    @Persist(value = "session")
+    @InitialValue(value = "literal:voice-mail")
     public abstract String getParentSettingName();
+
+    @InjectPage(value = RestartReminder.PAGE)
+    public abstract RestartReminder getRestartReminderPage();
 
     public abstract void setParentSettingName(String name);
 
@@ -33,24 +52,31 @@ public abstract class ServerSettings extends BasePage implements PageBeginRender
     public abstract void setParentSetting(Setting parent);
 
     public void pageBeginRender(PageEvent event_) {
+        if (getParentSetting() != null) {
+            return;
+        }
         Server server = getServer();
         Setting root = server.getSettings();
         Setting parent = root.getSetting(getParentSettingName());
         setParentSetting(parent);
     }
-        
-    public void editSettings(Integer serverId, String settingPath) {
+
+    @SuppressWarnings("unused")
+    public void editSettings(Integer serverId_, String settingPath) {
         setParentSettingName(settingPath);
     }
 
-    public IPage apply(IRequestCycle cycle) {
-        getServer().applySettings();
-        RestartReminder restartPage = (RestartReminder) cycle.getPage(RestartReminder.PAGE);
+    public IPage apply() {
+        RestartReminder restartPage = getRestartReminderPage();
         restartPage.setNextPage(PAGE);
         return restartPage;
     }
 
     public void cancel() {
         getServer().resetSettings();
+    }
+
+    public String getParentSettingDescription() {
+        return TapestryUtils.getSettingDescription(this, getParentSetting());
     }
 }
