ab7c252cdaa8bbca3cc8332fd4b1d15a2c726f1c
Douglas Hubler <douglas@hubler.us>
2007-03-26 14:30:01 +0000

    XCF-1585 - incorparate blf into speeddial patch
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@9164 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXconfig/neoconf/etc/database/speeddial.sql
+++ sipXconfig/neoconf/etc/database/speeddial.sql
@@ -8,6 +8,7 @@ create table speeddial_button (
     speeddial_id int4 not null,
     label varchar(255),
     number varchar(255) not null,
+    blf boolean not null default false,
     position int4 not null,
     primary key (speeddial_id, position)
 );
