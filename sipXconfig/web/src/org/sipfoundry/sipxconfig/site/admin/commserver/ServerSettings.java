sipXtapi/sipXtapi

commit 265839fef543f9f109169847c1298a552ac687ae

localization support for Server settings group description
git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@9105 a612230a-c5fa-0310-af8b-88eea846685b

sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/commserver/ServerSettings.java
@@ -12,19 +12,38 @@
 package org.sipfoundry.sipxconfig.site.admin.commserver;
 
 import org.apache.tapestry.IPage;
-import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.annotations.Bean;
+import org.apache.tapestry.annotations.InitialValue;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.InjectPage;
+import org.apache.tapestry.annotations.Persist;
 import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.html.BasePage;
 import org.sipfoundry.sipxconfig.admin.commserver.Server;
+import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
+import org.sipfoundry.sipxconfig.components.TapestryUtils;
 import org.sipfoundry.sipxconfig.setting.Setting;
 
+/**
+ * Page presenting sipX server settings: in most cases obtained by reading values from
+ * configuration files in /etc/sipxpbx directory
+ */
 public abstract class ServerSettings extends BasePage implements PageBeginRenderListener {
     public static final String PAGE = "ServerSettings";
 
+    @InjectObject(value = "spring:sipxServer")
     public abstract Server getServer();
 
+    @Bean
+    public abstract SipxValidationDelegate getValidator();
+
+    @Persist(value = "session")
+    @InitialValue(value = "literal:voice-mail")
     public abstract String getParentSettingName();
+
+    @InjectPage(value = RestartReminder.PAGE)
+    public abstract RestartReminder getRestartReminderPage();
 
     public abstract void setParentSettingName(String name);
 
@@ -33,24 +52,31 @@ public abstract class ServerSettings extends BasePage implements PageBeginRender
     public abstract void setParentSetting(Setting parent);
 
     public void pageBeginRender(PageEvent event_) {
+        if (getParentSetting() != null) {
+            return;
+        }
         Server server = getServer();
         Setting root = server.getSettings();
         Setting parent = root.getSetting(getParentSettingName());
         setParentSetting(parent);
     }
-        
-    public void editSettings(Integer serverId, String settingPath) {
+
+    @SuppressWarnings("unused")
+    public void editSettings(Integer serverId_, String settingPath) {
         setParentSettingName(settingPath);
     }
 
-    public IPage apply(IRequestCycle cycle) {
-        getServer().applySettings();
-        RestartReminder restartPage = (RestartReminder) cycle.getPage(RestartReminder.PAGE);
+    public IPage apply() {
+        RestartReminder restartPage = getRestartReminderPage();
         restartPage.setNextPage(PAGE);
         return restartPage;
     }
 
     public void cancel() {
         getServer().resetSettings();
+    }
+
+    public String getParentSettingDescription() {
+        return TapestryUtils.getSettingDescription(this, getParentSetting());
     }
 }

sipXconfig/web/context/WEB-INF/admin/commserver/ServerSettings.html
@@ -1,13 +1,13 @@
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="../../css/sipxconfig.css"/>
-</head>
-<body jwcid="$content$">
-    <div jwcid="@Border">        
-        <span jwcid="settingsNavigation">
-            <span jwcid="settingDescription"></span>
-        </span>
-        <span jwcid="settingsForm"/>
-    </div>   
-</body>
-</html>
+<div jwcid="@Border">        
+  <span jwcid="@setting/SettingsNavigation"
+      bean="prop:server"
+      activeSetting="prop:parentSetting"
+      editSettingsListener="listener:editSettings">
+    <span jwcid="@Insert" value="prop:parentSettingDescription"/>
+  </span>
+  <span jwcid="@SettingsForm"
+    settings="prop:parentSetting"
+    cancelListener="listener:cancel"
+    applyListener="listener:apply"
+    validator="bean:validator"/>
+</div>   

sipXconfig/web/context/WEB-INF/admin/commserver/ServerSettings.page
@@ -3,32 +3,4 @@
 	"-//Apache Software Foundation//Tapestry Specification 4.0//EN"
 	"http://jakarta.apache.org/tapestry/dtd/Tapestry_4_0.dtd">
 <page-specification class="org.sipfoundry.sipxconfig.site.admin.commserver.ServerSettings">
-  
-  <description>Server settings</description>
-  
-  <!-- P R O P E R T I E S -->
-  <inject property="server" object="spring:sipxServer"/>
-  <property name="parentSettingName" initial-value="literal:voice-mail" persist="session"/>
-  <property name="parentSetting"/>
-  
-  <!-- B E A N S -->
-  <bean name="validator" class="org.sipfoundry.sipxconfig.components.SipxValidationDelegate"/>
-  
-  <!-- C O M P O N E N T S -->
-  
-  <component id="settingsNavigation" type="setting/SettingsNavigation">
-    <binding name="bean" value="server"/>
-    <binding name="activeSetting" value="parentSetting"/>
-    <binding name="editSettingsListener" value="listener:editSettings"/>
-  </component>
-  <component id="settingDescription" type="Insert">
-    <binding name="value" value="parentSetting.description"/>
-  </component>
-  <component id="settingsForm" type="SettingsForm">
-    <binding name="settings" value="parentSetting"/>
-    <binding name="cancelListener" value="listener:cancel"/>
-    <binding name="applyListener" value="listener:apply"/>
-    <binding name="validator" value="bean:validator"/>
-  </component>
-  
 </page-specification>

sipXconfig/web/context/WEB-INF/setting/SettingsNavigation.jwc
@@ -1,5 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE component-specification PUBLIC "-//Apache Software Foundation//Tapestry Specification 4.0//EN"
-  "http://jakarta.apache.org/tapestry/dtd/Tapestry_4_0.dtd">
-<component-specification>
-</component-specification>

commit 278c8655610f69d2db57024dbabe451edc9c5f41

XCF-1591 - Explicitly call save settings to save the configuration, bug was introduced in r9105 3/16/07 by some page refactoring
git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@9179 a612230a-c5fa-0310-af8b-88eea846685b

sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/commserver/ServerSettings.java
@@ -66,6 +66,7 @@ public abstract class ServerSettings extends BasePage implements PageBeginRender
     }
 
     public IPage apply() {
+        getServer().applySettings();
         RestartReminder restartPage = getRestartReminderPage();
         restartPage.setNextPage(PAGE);
         return restartPage;