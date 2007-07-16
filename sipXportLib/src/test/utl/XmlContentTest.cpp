3ba8de1c1eb7c4cd97349f9ae954184d7d87b33e
Alexander Chemeris <alexander.chemeris@gmail.com>
2007-07-16 16:23:37 +0000

    Use UtlBoolean instead of bool in XmlContentTest to get rid of some compilation warnings.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@9831 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXportLib/src/test/utl/XmlContentTest.cpp
+++ sipXportLib/src/test/utl/XmlContentTest.cpp
@@ -49,13 +49,13 @@ public:
 
 #define HANDCHECK 0 /* unconditionally print results */
 
-   bool escape(const char* input, const char* correct)
+   UtlBoolean escape(const char* input, const char* correct)
       {
          UtlString in(input);
          UtlString out;
 
          XmlEscape(out,in);
-         bool result = (out == correct);
+         UtlBoolean result = (out == correct);
          if (!result || HANDCHECK)
          {
             printf("---- Escape\n"
@@ -68,13 +68,13 @@ public:
          return result;
       }
       
-   bool unescape(const char* input, const char* correct)
+   UtlBoolean unescape(const char* input, const char* correct)
       {
          UtlString in(input);
          UtlString out;
 
          XmlUnEscape(out,in);
-         bool result = (out == correct);
+         UtlBoolean result = (out == correct);
          if (!result || HANDCHECK)
          {
             printf("---- UnEscape\n"
