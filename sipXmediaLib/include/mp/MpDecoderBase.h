7e063f187dd8058b5f5d14ca418cccb2e7e9157e
Keith Kyzivat <kkyzivat@sipez.com>
2007-08-30 18:12:44 +0000

    Clean up doxygen comments in MpDecoderBase.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10020 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/include/mp/MpDecoderBase.h
+++ sipXmediaLib/include/mp/MpDecoderBase.h
@@ -31,7 +31,6 @@
 
 // FORWARD DECLARATIONS
 class OsNotification;
-class MprRecorder;
 
 /// Base class for all media processing decoders.
 class MpDecoderBase
@@ -44,28 +43,28 @@ public:
 //@{
 
      /// Constructor
-   MpDecoderBase( int payloadType ///< (in) RTP payload type associated with
-                                  ///<  this decoder
-                , const MpCodecInfo* pInfo ///< (in) pointer to derived class'
-                                           ///<  static const MpCodecInfo
-                );
+   MpDecoderBase(int payloadType,
+                 const MpCodecInfo* pInfo);
+     /**<
+     *  @param[in] payloadType - RTP payload type associated with this decoder
+     *  @param[in] pInfo - Pointer to derived class' static const MpCodecInfo
+     */
 
      /// Destructor
-   virtual
-   ~MpDecoderBase();
+   virtual ~MpDecoderBase();
 
      /// Initializes a codec data structure for use as a decoder
-   virtual OsStatus initDecode()=0;
+   virtual OsStatus initDecode() = 0;
      /**<
-     *  @returns <b>OS_SUCCESS</b> - Success
-     *  @returns <b>OS_NO_MEMORY</b> - Memory allocation failure
+     *  @retval OS_SUCCESS - Success
+     *  @retval OS_NO_MEMORY - Memory allocation failure
      */
 
      /// Frees all memory allocated to the decoder by <i>initDecode</i>
-   virtual OsStatus freeDecode(void)=0;
+   virtual OsStatus freeDecode(void) = 0;
      /**<
-     *  @returns <b>OS_SUCCESS</b> - Success
-     *  @returns <b>OS_DELETED</b> - Object has already been deleted
+     *  @retval OS_SUCCESS - Success
+     *  @retval OS_DELETED - Object has already been deleted
      */
 
 //@}
@@ -75,12 +74,14 @@ public:
 //@{
 
      /// Decode incoming RTP packet
-   virtual int decode(const MpRtpBufPtr &pPacket, ///< (in) Pointer to a media buffer
-                      unsigned decodedBufferLength, ///< (in) Length of the samplesBuffer (in samples)
-                      MpAudioSample *samplesBuffer ///< (out) Buffer for decoded samples
-                     ) =0;
+   virtual int decode(const MpRtpBufPtr &pPacket,
+                      unsigned decodedBufferLength,
+                      MpAudioSample *samplesBuffer) =0;
      /**<
-     *  @return Number of decoded samples.
+     *  @param[in] pPacket - Pointer to a media buffer
+     *  @param[in] decodedBufferLength - Length of the samplesBuffer (in samples)
+     *  @param[out] samplesBuffer - Buffer for decoded samples
+     *  @returns Number of decoded samples.
      */
 
      /// Set notification to be fired when DTMF tone received.
@@ -95,7 +96,7 @@ public:
      /// Get static information about the decoder
    virtual const MpCodecInfo* getInfo(void) const;
      /**<
-     *  @returns a pointer to an <i>MpCodecInfo</i> object that provides
+     *  @returns a pointer to an MpCodecInfo object that provides
      *  static information about the decoder.
      */
 
