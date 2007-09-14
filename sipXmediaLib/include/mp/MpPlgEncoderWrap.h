c0d6b9521fa05f815fde630783b1b779c14444c3
Alexander Chemeris <alexander.chemeris@gmail.com>
2007-09-14 17:08:00 +0000

    Small formating fixes for MpPlgEncoderWrap.h.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10136 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/include/mp/MpPlgEncoderWrap.h
+++ sipXmediaLib/include/mp/MpPlgEncoderWrap.h
@@ -8,27 +8,22 @@
 // $$
 ///////////////////////////////////////////////////////////////////////////////
 
-// Author: 
+// Author: Sergey Kostanbaev <Sergey DOT Kostanbaev AT sipez DOT com>
 
 #ifndef _PlgEncoderWrapper_h_
 #define _PlgEncoderWrapper_h_
 
 #include "mp/MpEncoderBase.h"
-#include "mp/MpCodecFactory.h"
+#include "mp/MpPlgStaffV1.h"
 
 class MpPlgEncoderWrapper : public MpEncoderBase
 {
-   friend class MpCodecFactory;
-
-protected:
-   MpCodecInfo mCodecInfo;
-   const MpCodecCallInfoV1& mplgci;
-   UtlBoolean mInitialized;
-   void* plgHandle;
-   const char* mDefParamString;
-
 public:
-   MpPlgEncoderWrapper(int payloadType, const MpCodecCallInfoV1& plgci, const char* permanentDefaultMode);
+
+   MpPlgEncoderWrapper(int payloadType,
+                       const MpCodecCallInfoV1& plgci,
+                       const char* permanentDefaultMode);
+
    virtual ~MpPlgEncoderWrapper();
 
    virtual OsStatus initEncode(const char* codecFmtString);
@@ -36,15 +31,22 @@ public:
    virtual OsStatus freeEncode();
 
    virtual OsStatus encode(const MpAudioSample* pAudioSamples,
-      const int numSamples,
-      int& rSamplesConsumed,
-      unsigned char* pCodeBuf,
-      const int bytesLeft,
-      int& rSizeInBytes,
-      UtlBoolean& sendNow,
-      MpAudioBuf::SpeechType& rAudioCategory); 
+                           const int numSamples,
+                           int& rSamplesConsumed,
+                           unsigned char* pCodeBuf,
+                           const int bytesLeft,
+                           int& rSizeInBytes,
+                           UtlBoolean& sendNow,
+                           MpAudioBuf::SpeechType& rAudioCategory); 
 
    virtual const MpCodecInfo* getInfo(void) const;
+
+protected:
+   MpCodecInfo mCodecInfo;
+   const MpCodecCallInfoV1& mplgci;
+   UtlBoolean mInitialized;
+   void* plgHandle;
+   const char* mDefParamString;
 
 private:
    UtlBoolean initializeWrapper(const char *fmt);
