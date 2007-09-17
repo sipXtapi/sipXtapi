939667b6efbc5eb92e80af985727eb2465d19810
Alexander Chemeris <alexander.chemeris@gmail.com>
2007-09-17 03:50:30 +0000

    FIx build of G.726 codec wrapper with older versions of SpanDSP.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10190 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/src/mp/codecs/plgg726/plgg726.h
+++ sipXmediaLib/src/mp/codecs/plgg726/plgg726.h
@@ -18,6 +18,13 @@
 #include <spandsp/bitstream.h>
 #include <spandsp/g726.h>
 
+#ifndef G726_PACKING_NONE
+#define G726_PACKING_NONE 0
+#endif
+
+#ifndef G726_ENCODING_LINEAR
+#define G726_ENCODING_LINEAR	0
+#endif
 
 int internal_decode_g726(void* handle, const void* pCodedData, 
                          unsigned cbCodedPacketSize, void* pAudioBuffer, 
