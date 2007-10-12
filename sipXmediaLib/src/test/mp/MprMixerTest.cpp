07acbd27931c6aad8d3a8a412fd2bec03a8ef5f4
Keith Kyzivat <kkyzivat@sipez.com>
2007-10-12 19:15:30 +0000

    Formatting changes for MprMixerTest -- Only whitespace changes.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10323 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/src/test/mp/MprMixerTest.cpp
+++ sipXmediaLib/src/test/mp/MprMixerTest.cpp
@@ -33,202 +33,202 @@ public:
 
    void testCreators()
    {
-       MprMixer*         pMixer     = NULL;
-       OsStatus          res;
+      MprMixer* pMixer = NULL;
+      OsStatus res;
 
-       // when we have a flow graph that contains resources and links,
-       // verify that destroying the flow graph also gets rid of the resources
-       // and links.
-       pMixer = new MprMixer("MprMixer", 2,
-                             getSamplesPerFrame(), getSamplesPerSec());
+      // when we have a flow graph that contains resources and links,
+      // verify that destroying the flow graph also gets rid of the resources
+      // and links.
+      pMixer = 
+         new MprMixer("MprMixer", 2, getSamplesPerFrame(), getSamplesPerSec());
 
-       res = mpFlowGraph->addResource(*pMixer);
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->addResource(*pMixer);
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
    }
 
    void testDisabled()
    {
-       MprMixer*         pMixer     = NULL;
-       OsStatus          res;
+      MprMixer* pMixer = NULL;
+      OsStatus res;
 
-       pMixer = new MprMixer("MprMixer", 2,
-                             getSamplesPerFrame(), getSamplesPerSec());
-       CPPUNIT_ASSERT(pMixer != NULL);
+      pMixer = 
+         new MprMixer("MprMixer", 2, getSamplesPerFrame(), getSamplesPerSec());
+      CPPUNIT_ASSERT(pMixer != NULL);
 
-       setupFramework(pMixer);
+      setupFramework(pMixer);
 
-       // TESTCASE 1:
-       // pMixer disabled, there are no buffers on the input 0.
-       CPPUNIT_ASSERT(mpSourceResource->disable());
-       CPPUNIT_ASSERT(pMixer->disable());
+      // TESTCASE 1:
+      // pMixer disabled, there are no buffers on the input 0.
+      CPPUNIT_ASSERT(mpSourceResource->disable());
+      CPPUNIT_ASSERT(pMixer->disable());
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // We did not generated any buffers
-       CPPUNIT_ASSERT(  !mpSourceResource->mLastDoProcessArgs.outBufs[0].isValid()
+      // We did not generated any buffers
+      CPPUNIT_ASSERT(!mpSourceResource->mLastDoProcessArgs.outBufs[0].isValid()
                      && !mpSourceResource->mLastDoProcessArgs.outBufs[1].isValid()
                      && !mpSinkResource->mLastDoProcessArgs.inBufs[0].isValid()
-                     );
+                    );
 
-       // TESTCASE 2:
-       // pMixer disabled, there are buffers on the input 0 and 1.
-       CPPUNIT_ASSERT(mpSourceResource->enable());
-       CPPUNIT_ASSERT(pMixer->disable());
+      // TESTCASE 2:
+      // pMixer disabled, there are buffers on the input 0 and 1.
+      CPPUNIT_ASSERT(mpSourceResource->enable());
+      CPPUNIT_ASSERT(pMixer->disable());
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // Buffer from input 0 should be passed through to the output.
-       CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[0] ==
-                      mpSinkResource->mLastDoProcessArgs.inBufs[0]);
+      // Buffer from input 0 should be passed through to the output.
+      CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[0] ==
+                     mpSinkResource->mLastDoProcessArgs.inBufs[0]);
 
-       // Stop flowgraph
-       haltFramework();
+      // Stop flowgraph
+      haltFramework();
    }
 
    void testMuted()
    {
-       MprMixer*         pMixer     = NULL;
-       OsStatus          res;
+      MprMixer* pMixer = NULL;
+      OsStatus res;
 
-       pMixer = new MprMixer("MprMixer", 2,
-                             getSamplesPerFrame(), getSamplesPerSec());
-       CPPUNIT_ASSERT(pMixer != NULL);
+      pMixer = 
+         new MprMixer("MprMixer", 2, getSamplesPerFrame(), getSamplesPerSec());
+      CPPUNIT_ASSERT(pMixer != NULL);
 
-       setupFramework(pMixer);
+      setupFramework(pMixer);
 
-       // pMixer enabled, there are buffers on the input 0
-       CPPUNIT_ASSERT(mpSourceResource->enable());
-       CPPUNIT_ASSERT(pMixer->enable());
+      // pMixer enabled, there are buffers on the input 0
+      CPPUNIT_ASSERT(mpSourceResource->enable());
+      CPPUNIT_ASSERT(pMixer->enable());
 
-       // Set all weights to 0
-       CPPUNIT_ASSERT(pMixer->setWeight(0,0));
-       CPPUNIT_ASSERT(pMixer->setWeight(0,1));
+      // Set all weights to 0
+      CPPUNIT_ASSERT(pMixer->setWeight(0,0));
+      CPPUNIT_ASSERT(pMixer->setWeight(0,1));
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // No buffers should be processed
-       CPPUNIT_ASSERT(  mpSourceResource->mLastDoProcessArgs.outBufs[0].isValid()
+      // No buffers should be processed
+      CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[0].isValid()
                      && mpSourceResource->mLastDoProcessArgs.outBufs[1].isValid()
                      && !mpSinkResource->mLastDoProcessArgs.inBufs[0].isValid()
-                     );
+                    );
 
-       // Stop flowgraph
-       haltFramework();
+      // Stop flowgraph
+      haltFramework();
    }
 
    void testEnabledNoData()
    {
-       MprMixer*         pMixer     = NULL;
-       OsStatus          res;
+      MprMixer* pMixer = NULL;
+      OsStatus res;
 
-       pMixer = new MprMixer("MprMixer", 2,
-                             getSamplesPerFrame(), getSamplesPerSec());
-       CPPUNIT_ASSERT(pMixer != NULL);
+      pMixer = 
+         new MprMixer("MprMixer", 2, getSamplesPerFrame(), getSamplesPerSec());
+      CPPUNIT_ASSERT(pMixer != NULL);
 
-       setupFramework(pMixer);
+      setupFramework(pMixer);
 
-       // pMixer enabled, there are no buffers on the input 0
-       CPPUNIT_ASSERT(mpSourceResource->disable());
-       CPPUNIT_ASSERT(pMixer->enable());
+      // pMixer enabled, there are no buffers on the input 0
+      CPPUNIT_ASSERT(mpSourceResource->disable());
+      CPPUNIT_ASSERT(pMixer->enable());
 
-       // Set all weights to 1
-       CPPUNIT_ASSERT(pMixer->setWeight(1,0));
-       CPPUNIT_ASSERT(pMixer->setWeight(1,1));
+      // Set all weights to 1
+      CPPUNIT_ASSERT(pMixer->setWeight(1,0));
+      CPPUNIT_ASSERT(pMixer->setWeight(1,1));
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // Mixer generate new buffer even if no input is available.
-       CPPUNIT_ASSERT(  !mpSourceResource->mLastDoProcessArgs.outBufs[0].isValid()
+      // Mixer generate new buffer even if no input is available.
+      CPPUNIT_ASSERT(!mpSourceResource->mLastDoProcessArgs.outBufs[0].isValid()
                      && !mpSourceResource->mLastDoProcessArgs.outBufs[1].isValid()
                      && mpSinkResource->mLastDoProcessArgs.inBufs[0].isValid()
-                     );
+                    );
 
-       // Stop flowgraph
-       haltFramework();
+      // Stop flowgraph
+      haltFramework();
    }
 
    void testEnabledWithOneWeight()
    {
-       MprMixer*         pMixer     = NULL;
-       OsStatus          res;
+      MprMixer* pMixer = NULL;
+      OsStatus res;
 
-       pMixer = new MprMixer("MprMixer", 2,
-                             getSamplesPerFrame(), getSamplesPerSec());
-       CPPUNIT_ASSERT(pMixer != NULL);
+      pMixer = 
+         new MprMixer("MprMixer", 2, getSamplesPerFrame(), getSamplesPerSec());
+      CPPUNIT_ASSERT(pMixer != NULL);
 
-       setupFramework(pMixer);
+      setupFramework(pMixer);
 
-       // pMixer enabled, there are buffers on the input 0
-       CPPUNIT_ASSERT(mpSourceResource->enable());
-       CPPUNIT_ASSERT(pMixer->enable());
+      // pMixer enabled, there are buffers on the input 0
+      CPPUNIT_ASSERT(mpSourceResource->enable());
+      CPPUNIT_ASSERT(pMixer->enable());
 
-       // Set only first weight to 1
-       CPPUNIT_ASSERT(pMixer->setWeight(1,0));
-       CPPUNIT_ASSERT(pMixer->setWeight(0,1));
+      // Set only first weight to 1
+      CPPUNIT_ASSERT(pMixer->setWeight(1,0));
+      CPPUNIT_ASSERT(pMixer->setWeight(0,1));
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // Buffer from input 0 should be passed through to the output.
-       CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[0] ==
-                      mpSinkResource->mLastDoProcessArgs.inBufs[0]);
+      // Buffer from input 0 should be passed through to the output.
+      CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[0] ==
+                     mpSinkResource->mLastDoProcessArgs.inBufs[0]);
 
-       // Set only second weight to 1
-       CPPUNIT_ASSERT(pMixer->setWeight(0,0));
-       CPPUNIT_ASSERT(pMixer->setWeight(1,1));
+      // Set only second weight to 1
+      CPPUNIT_ASSERT(pMixer->setWeight(0,0));
+      CPPUNIT_ASSERT(pMixer->setWeight(1,1));
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // Buffer from input 1 should be passed through to the output.
-       CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[1] ==
-                      mpSinkResource->mLastDoProcessArgs.inBufs[0]);
+      // Buffer from input 1 should be passed through to the output.
+      CPPUNIT_ASSERT(mpSourceResource->mLastDoProcessArgs.outBufs[1] ==
+                     mpSinkResource->mLastDoProcessArgs.inBufs[0]);
 
-       // Stop flowgraph
-       haltFramework();
+      // Stop flowgraph
+      haltFramework();
    }
 
    void testEnabledWithManyWeights()
    {
-       MprMixer*         pMixer     = NULL;
-       MpBufPtr          pBuf;
-       OsStatus          res;
+      MprMixer* pMixer = NULL;
+      MpBufPtr pBuf;
+      OsStatus res;
 
-       pMixer = new MprMixer("MprMixer", 2,
-                             getSamplesPerFrame(), getSamplesPerSec());
-       CPPUNIT_ASSERT(pMixer != NULL);
+      pMixer = 
+         new MprMixer("MprMixer", 2, getSamplesPerFrame(), getSamplesPerSec());
+      CPPUNIT_ASSERT(pMixer != NULL);
 
-       setupFramework(pMixer);
+      setupFramework(pMixer);
 
-       // pMixer enabled, there are buffers on the input 0
-       CPPUNIT_ASSERT(mpSourceResource->enable());
-       CPPUNIT_ASSERT(pMixer->enable());
+      // pMixer enabled, there are buffers on the input 0
+      CPPUNIT_ASSERT(mpSourceResource->enable());
+      CPPUNIT_ASSERT(pMixer->enable());
 
-       // Set all weights to 1
-       CPPUNIT_ASSERT(pMixer->setWeight(1,0));
-       CPPUNIT_ASSERT(pMixer->setWeight(1,1));
+      // Set all weights to 1
+      CPPUNIT_ASSERT(pMixer->setWeight(1,0));
+      CPPUNIT_ASSERT(pMixer->setWeight(1,1));
 
-       res = mpFlowGraph->processNextFrame();
-       CPPUNIT_ASSERT(res == OS_SUCCESS);
+      res = mpFlowGraph->processNextFrame();
+      CPPUNIT_ASSERT(res == OS_SUCCESS);
 
-       // Store output buffer for convenience
-       pBuf = mpSinkResource->mLastDoProcessArgs.inBufs[0];
+      // Store output buffer for convenience
+      pBuf = mpSinkResource->mLastDoProcessArgs.inBufs[0];
 
-       // New buffer should be generated.
-       CPPUNIT_ASSERT(  pBuf.isValid()
+      // New buffer should be generated.
+      CPPUNIT_ASSERT(pBuf.isValid()
                      && (mpSourceResource->mLastDoProcessArgs.outBufs[0] != pBuf)
                      && (mpSourceResource->mLastDoProcessArgs.outBufs[1] != pBuf)
-                     );
+                    );
 
-       // Free stored buffer
-       pBuf.release();
+      // Free stored buffer
+      pBuf.release();
 
-       // Stop flowgraph
-       haltFramework();
+      // Stop flowgraph
+      haltFramework();
    }
 
 };
