12a584786a71b5c3385cb2fd87a12cbee62878e0
Alexander Chemeris <alexander.chemeris@gmail.com>
2007-11-21 11:49:26 +0000

    Slightly more correct handling of size of encoded data in G.726 codec.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10379 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/src/mp/codecs/plgg726/plgg726.c
+++ sipXmediaLib/src/mp/codecs/plgg726/plgg726.c
@@ -23,8 +23,13 @@ int internal_decode_g726(void* handle, const void* pCodedData,
                          const struct RtpHeader* pRtpHeader)
 {
    int samples;
+   int maxBytesToDecode;
+
+   maxBytesToDecode = min(cbCodedPacketSize, cbBufferSize*8/((g726_state_t*)handle)->bits_per_sample);
+   assert(maxBytesToDecode == cbCodedPacketSize);
    
-   samples = g726_decode((g726_state_t*)handle, (int16_t*)pAudioBuffer, pCodedData, cbCodedPacketSize);
+   samples = g726_decode((g726_state_t*)handle, (int16_t*)pAudioBuffer, pCodedData, maxBytesToDecode);
+//   printf("G726 decoded %d frames.\n", samples);
    *pcbCodedSize = samples;
 
    return RPLG_SUCCESS;
@@ -39,7 +44,11 @@ int internal_encode_g726(void* handle, const void* pAudioBuffer,
    int bytes;
 
    bytes = g726_encode((g726_state_t*)handle, pCodedData, pAudioBuffer, cbAudioSamples);
-   *pcbCodedSize = cbMaxCodedData;
+   assert(bytes <= cbMaxCodedData);
+   *pcbCodedSize = bytes;
+
+//   printf("G726 encoder: samples consumed: %d, bytes in a frame: %d, bytes wrote: %d\n",
+//          cbAudioSamples, bytes, cbMaxCodedData);
 
    *pbSendNow = FALSE;
    *rSamplesConsumed = cbAudioSamples;
