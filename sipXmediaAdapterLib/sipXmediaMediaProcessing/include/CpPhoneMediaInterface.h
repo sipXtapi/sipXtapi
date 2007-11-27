sipXtapi/sipXtapi

commit 9a9399000b832c3ba03ec7273f8c26b2ae14ca79

Fixed VC6 project header files path for changed toplogy header path. No doubt broke linux and VC >6 versions to be fixed on later iterations.
git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10327 a612230a-c5fa-0310-af8b-88eea846685b

sipXmediaAdapterLib/sipXmediaMediaProcessing/include/CpTopologyGraphInterface.h
@@ -21,7 +21,7 @@
 #include <sdp/SdpCodecList.h>
 #include <mi/CpMediaInterface.h>
 #include <net/QoS.h>
-#include "include/MaNotfTranslatorDispatcher.h"
+#include <MaNotfTranslatorDispatcher.h>
 
 // DEFINES
 // MACROS

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/test/CpPhoneMediaInterfaceTest.cpp
@@ -14,6 +14,7 @@
 #include <cppunit/TestCase.h>
 #include <mi/CpMediaInterfaceFactory.h>
 #include <mi/CpMediaInterfaceFactoryFactory.h>
+#include <CpTopologyGraphInterface.h>
 #include <mi/CpMediaInterface.h>
 #include <mi/MiNotification.h>
 #include <mi/MiDtmfNotf.h>
@@ -269,6 +270,14 @@ class CpPhoneMediaInterfaceTest : public CppUnit::TestCase
         int connectionId = -1;
         CPPUNIT_ASSERT(mediaInterface->createConnection(connectionId, NULL) == OS_SUCCESS);
         CPPUNIT_ASSERT(connectionId > 0);
+
+        // Test that we can get the bridge mixer port on which this new connection
+        // is connected to
+        int portOnBridge;
+        ((CpTopologyGraphInterface*)mediaInterface)->getConnectionPortOnBridge(connectionId, 
+                                                  portOnBridge);
+        CPPUNIT_ASSERT_EQUAL(3, portOnBridge);
+
 
         propertyName = "connectionLabel";
         setPropertyValue = "connection1";
@@ -645,7 +654,7 @@ class CpPhoneMediaInterfaceTest : public CppUnit::TestCase
 
     void testTones()
     {
-        RTL_START(1600000);
+        RTL_START(3000000);
 
         CPPUNIT_ASSERT(mpMediaFactory);
 

sipXmediaAdapterLib/sipXmediaAdapterLib.dsp
@@ -65,7 +65,7 @@ LIB32=link.exe -lib
 # PROP Intermediate_Dir "Debug"
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_MBCS" /D "_LIB" /YX /FD /GZ /c
-# ADD CPP /nologo /MDd /W3 /Gm /GR /GX /ZI /Od /I "include" /I "..\sipXportLib\include" /I "..\sipXtackLib\include" /I "..\sipXsdpLib\include" /I "sipXmediaMediaProcessing" /I "interface" /I "..\sipXmediaLib\include" /D "_DEBUG" /D "HAVE_SPEEX" /D "HAVE_GSM" /D "_LIB" /D "WIN32" /D "_MBCS" /D "SIPXTAPI_STATIC" /D "DDDDISABLE_DEFAULT_PHONE_MEDIA_INTERFACE_FACTORY" /D "DDDENABLE_TOPOLOGY_FLOWGRAPH_INTERFACE_FACTORY" /FD /GZ /c
+# ADD CPP /nologo /MDd /W3 /Gm /GR /GX /ZI /Od /I "include" /I "..\sipXportLib\include" /I "..\sipXtackLib\include" /I "..\sipXsdpLib\include" /I "sipXmediaMediaProcessing" /I "interface" /I "..\sipXmediaLib\include" /D "_DEBUG" /D "HAVE_SPEEX" /D "HAVE_GSM" /D "_LIB" /D "WIN32" /D "_MBCS" /D "SIPXTAPI_STATIC" /D "DISABLE_DEFAULT_PHONE_MEDIA_INTERFACE_FACTORY" /D "ENABLE_TOPOLOGY_FLOWGRAPH_INTERFACE_FACTORY" /FD /GZ /c
 # SUBTRACT CPP /YX
 # ADD BASE RSC /l 0x409 /d "_DEBUG"
 # ADD RSC /l 0x409 /d "_DEBUG"

sipXmediaAdapterLib/sipXmediaAdapterLibTest.dsp
@@ -42,7 +42,7 @@ RSC=rc.exe
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
-# ADD CPP /nologo /MD /W3 /GX /O1 /I "..\sipXportLib\src\test" /I "..\sipXportLib\include" /I "interface" /I "include" /I "..\sipXtackLib\include" /I "..\sipXsdpLib\include" /I "..\sipXmediaLib\include" /I "..\CPPUnit\include" /D "NDEBUG" /D "WIN32" /D "_CONSOLE" /D "_MBCS" /D "SIPXTAPI_STATIC" /FD /c
+# ADD CPP /nologo /MD /W3 /GX /O1 /I "..\sipXportLib\src\test" /I "..\sipXportLib\include" /I "interface" /I "sipXmediaMediaProcessing\include" /I "..\sipXtackLib\include" /I "..\sipXsdpLib\include" /I "..\sipXmediaLib\include" /I "..\CPPUnit\include" /D "NDEBUG" /D "WIN32" /D "_CONSOLE" /D "_MBCS" /D "SIPXTAPI_STATIC" /FD /c
 # SUBTRACT CPP /YX
 # ADD BASE RSC /l 0x409 /d "NDEBUG"
 # ADD RSC /l 0x409 /d "NDEBUG"
@@ -67,7 +67,7 @@ LINK32=link.exe
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /GZ /c
-# ADD CPP /nologo /MDd /W3 /Gm /GR /GX /ZI /Od /I "..\sipXportLib\src\test" /I "..\sipXportLib\include" /I "interface" /I "include" /I "..\sipXtackLib\include" /I "..\sipXsdpLib\include" /I "..\sipXmediaLib\include" /I "..\CPPUnit\include" /D "_DEBUG" /D "WIN32" /D "_CONSOLE" /D "_MBCS" /D "SIPXTAPI_STATIC" /FD /GZ /GZ /c
+# ADD CPP /nologo /MDd /W3 /Gm /GR /GX /ZI /Od /I "..\sipXportLib\src\test" /I "..\sipXportLib\include" /I "interface" /I "sipXmediaMediaProcessing\include" /I "..\sipXtackLib\include" /I "..\sipXsdpLib\include" /I "..\sipXmediaLib\include" /I "..\CPPUnit\include" /D "_DEBUG" /D "WIN32" /D "_CONSOLE" /D "_MBCS" /D "SIPXTAPI_STATIC" /FD /GZ /GZ /c
 # SUBTRACT CPP /YX
 # ADD BASE RSC /l 0x409 /d "_DEBUG"
 # ADD RSC /l 0x409 /d "_DEBUG"

commit 8b59e33d1497c381c67e2af02ac4372936910e02

Fix breakage of specifying include files caused by r10327 under gnu/autoconf, VS2003, and vs2005.
Replaced all sipXmediaAdapterLib include specifiers of type:
#include <include/xxx.h>    with
#include <xxx.h>

Also fixed improper uses of <> and "".



git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10380 a612230a-c5fa-0310-af8b-88eea846685b

sipXmediaAdapterLib/sipXmediaAdapterLib-msvc8.vcproj
@@ -55,7 +55,7 @@
 				Name="VCCLCompilerTool"
 				Optimization="2"
 				InlineFunctionExpansion="0"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="NDEBUG;HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;WIN32;_LIB;_CRT_SECURE_NO_WARNINGS"
 				StringPooling="false"
 				MinimalRebuild="false"
@@ -136,7 +136,7 @@
 				ExecutionBucket="7"
 				Optimization="1"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="NDEBUG;WIN32;_LIB;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				StringPooling="true"
 				RuntimeLibrary="0"
@@ -223,7 +223,7 @@
 				ExecutionBucket="7"
 				Optimization="1"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="NDEBUG;WIN32;_LIB;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				StringPooling="true"
 				MinimalRebuild="true"
@@ -309,7 +309,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="HAVE_SPEEX;HAVE_GSM;xxHAVE_ILBC;WIN32;_DEBUG;_LIB;_CRT_SECURE_NO_DEPRECATE;xxENABLE_TOPOLOGY_FLOWGRAPH_INTERFACE_FACTORY;xxDISABLE_DEFAULT_PHONE_MEDIA_INTERFACE_FACTORY"
 				MinimalRebuild="true"
 				BasicRuntimeChecks="3"
@@ -388,7 +388,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories=";include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories=";sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="_DEBUG;WIN32;_LIB;SIPX_CONFDIR=\&quot;.\&quot;;SIPX_LOGDIR=\&quot;.\&quot;;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				MinimalRebuild="true"
 				RuntimeLibrary="1"
@@ -475,7 +475,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories=";include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="_DEBUG;WIN32;_LIB;SIPX_CONFDIR=\&quot;.\&quot;;SIPX_LOGDIR=\&quot;.\&quot;;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				MinimalRebuild="true"
 				RuntimeLibrary="1"
@@ -560,7 +560,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;HAVE_INTEL_IPP;WIN32;_DEBUG;_LIB;_CRT_SECURE_NO_WARNINGS"
 				MinimalRebuild="true"
 				BasicRuntimeChecks="3"
@@ -639,7 +639,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories=";include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="_DEBUG;WIN32;_LIB;SIPX_CONFDIR=\&quot;.\&quot;;SIPX_LOGDIR=\&quot;.\&quot;;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				MinimalRebuild="true"
 				RuntimeLibrary="1"
@@ -726,7 +726,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories=";include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="_DEBUG;WIN32;_LIB;SIPX_CONFDIR=\&quot;.\&quot;;SIPX_LOGDIR=\&quot;.\&quot;;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				MinimalRebuild="true"
 				RuntimeLibrary="1"
@@ -812,7 +812,7 @@
 				Name="VCCLCompilerTool"
 				Optimization="2"
 				InlineFunctionExpansion="0"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="NDEBUG;HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;HAVE_INTEL_IPP;WIN32;_LIB;_CRT_SECURE_NO_WARNINGS"
 				StringPooling="false"
 				MinimalRebuild="false"
@@ -893,7 +893,7 @@
 				ExecutionBucket="7"
 				Optimization="1"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="NDEBUG;WIN32;_LIB;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				StringPooling="true"
 				RuntimeLibrary="0"
@@ -980,7 +980,7 @@
 				ExecutionBucket="7"
 				Optimization="1"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="NDEBUG;WIN32;_LIB;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				StringPooling="true"
 				MinimalRebuild="true"
@@ -1069,7 +1069,7 @@
 				ExecutionBucket="7"
 				Optimization="1"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="NDEBUG;WIN32;_LIB;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				StringPooling="true"
 				RuntimeLibrary="0"
@@ -1156,7 +1156,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories=";include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="_DEBUG;WIN32;_LIB;SIPX_CONFDIR=\&quot;.\&quot;;SIPX_LOGDIR=\&quot;.\&quot;;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				MinimalRebuild="true"
 				RuntimeLibrary="1"
@@ -1244,7 +1244,7 @@
 				ExecutionBucket="7"
 				Optimization="1"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="include,..\sipXportLib\include,..\sipXtackLib\include,sipXmediaMediaProcessing,interface,..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="NDEBUG;_LIB;WIN32;SIPXTAPI_STATIC"
 				StringPooling="true"
 				RuntimeLibrary="2"
@@ -1329,7 +1329,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories="include,..\sipXportLib\include,..\sipXtackLib\include,sipXmediaMediaProcessing,interface,..\sipXmediaLib\include"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include"
 				PreprocessorDefinitions="_DEBUG;_LIB;WIN32;SIPXTAPI_STATIC"
 				MinimalRebuild="true"
 				RuntimeLibrary="3"

sipXmediaAdapterLib/sipXmediaAdapterLib.vcproj
@@ -19,7 +19,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;SIPX_VIDEO;WIN32;_DEBUG;_LIB;xDISABLE_DEFAULT_PHONE_MEDIA_INTERFACE_FACTORY;xENABLE_TOPOLOGY_FLOWGRAPH_INTERFACE_FACTORY"
 				MinimalRebuild="TRUE"
 				BasicRuntimeChecks="3"
@@ -60,7 +60,7 @@
 			CharacterSet="2">
 			<Tool
 				Name="VCCLCompilerTool"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;SIPX_VIDEO;WIN32;NDEBUG;_LIB"
 				RuntimeLibrary="1"
 				RuntimeTypeInfo="TRUE"
@@ -100,7 +100,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;WIN32;_DEBUG;_LIB"
 				MinimalRebuild="TRUE"
 				BasicRuntimeChecks="3"
@@ -141,7 +141,7 @@
 			CharacterSet="2">
 			<Tool
 				Name="VCCLCompilerTool"
-				AdditionalIncludeDirectories="sipXmediaMediaProcessing;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\sipXcallLib\include;..\sipXtackLib\include;interface"
 				PreprocessorDefinitions="HAVE_SPEEX;HAVE_GSM;HAVE_ILBC;WIN32;NDEBUG;_LIB"
 				RuntimeLibrary="1"
 				RuntimeTypeInfo="TRUE"

sipXmediaAdapterLib/sipXmediaAdapterLibTest-msvc8.vcproj
@@ -56,7 +56,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="..\sipXportLib\src\test;..\sipXportLib\include;..\sipXcallLib\include;..\sipXportLib\include\glib;interface;include;..\sipXtackLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\CPPUnit\include"
+				AdditionalIncludeDirectories="..\sipXportLib\src\test;..\sipXportLib\include;..\sipXcallLib\include;..\sipXportLib\include\glib;interface;sipXmediaMediaProcessing\include;..\sipXtackLib\include;..\sipXsdpLib\include;..\sipXmediaLib\include;..\CPPUnit\include"
 				PreprocessorDefinitions="_DEBUG;WIN32;_CONSOLE;_CRT_SECURE_NO_DEPRECATE"
 				BasicRuntimeChecks="3"
 				RuntimeLibrary="3"
@@ -145,7 +145,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
-				AdditionalIncludeDirectories="..\cppunit-1.12.0\include;..\pcre\pcre-7.0;include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test"
+				AdditionalIncludeDirectories="..\cppunit-1.12.0\include;..\pcre\pcre-7.0;sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test"
 				PreprocessorDefinitions="WIN32;_DEBUG;_CONSOLE;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				RuntimeLibrary="1"
 				ForcedIncludeFiles="os/WinCE/wincefixups.h"
@@ -214,7 +214,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
-				AdditionalIncludeDirectories="&quot;..\cppunit-1.12.0\include&quot;;&quot;..\pcre\pcre-7.0&quot;;include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test;..\sipXmediaAdapterLib\interface"
+				AdditionalIncludeDirectories="&quot;..\cppunit-1.12.0\include&quot;;&quot;..\pcre\pcre-7.0&quot;;sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test;..\sipXmediaAdapterLib\interface"
 				PreprocessorDefinitions="WIN32;_DEBUG;_CONSOLE;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				MinimalRebuild="true"
 				RuntimeLibrary="1"
@@ -288,7 +288,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
-				AdditionalIncludeDirectories="include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test;..\sipXmediaAdapterLib\interface"
+				AdditionalIncludeDirectories="sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test;..\sipXmediaAdapterLib\interface"
 				PreprocessorDefinitions="WIN32;NDEBUG;_CONSOLE;SIP_STACK_VERSION=\&quot;1.13.0\&quot;;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				ForcedIncludeFiles="os/WinCE/wincefixups.h"
 			/>
@@ -358,7 +358,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
-				AdditionalIncludeDirectories="&quot;..\cppunit-1.12.0\include&quot;;&quot;..\pcre\pcre-7.0&quot;;include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;sipXmediaMediaProcessing;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test;..\sipXmediaAdapterLib\interface"
+				AdditionalIncludeDirectories="&quot;..\cppunit-1.12.0\include&quot;;&quot;..\pcre\pcre-7.0&quot;;sipXmediaMediaProcessing\include;..\sipXportLib\include;..\sipXsdpLib\include;..\sipXtackLib\include;interface;..\sipXmediaLib\include;..\sipXportLib\src\test;src\test;..\sipXmediaAdapterLib\interface"
 				PreprocessorDefinitions="WIN32;_DEBUG;_CONSOLE;WINCE;$(ARCHFAM);$(_ARCHFAM_);_WIN32_WCE=$(CEVER);UNDER_CE"
 				RuntimeLibrary="1"
 				ForcedIncludeFiles="os/WinCE/wincefixups.h"
@@ -438,7 +438,7 @@
 				ExecutionBucket="7"
 				Optimization="2"
 				InlineFunctionExpansion="1"
-				AdditionalIncludeDirectories="..\sipXportLib\src\test,..\sipXportLib\include,..\sipXportLib\include\glib,include,..\sipXcallLib\include"
+				AdditionalIncludeDirectories="..\sipXportLib\src\test,..\sipXportLib\include,..\sipXportLib\include\glib,sipXmediaMediaProcessing\include,..\sipXcallLib\include"
 				PreprocessorDefinitions="NDEBUG;DONT_USE_LONG_LONG;WIN32;_CONSOLE;DISABLE_MEM_POOLS"
 				StringPooling="true"
 				RuntimeLibrary="2"
@@ -530,7 +530,7 @@
 				Name="VCCLCompilerTool"
 				ExecutionBucket="7"
 				Optimization="0"
-				AdditionalIncludeDirectories="..\sipXportLib\src\test,..\sipXportLib\include,..\sipXportLib\include\glib,interface,include,..\sipXtackLib\include"
+				AdditionalIncludeDirectories="..\sipXportLib\src\test,..\sipXportLib\include,..\sipXportLib\include\glib,interface,sipXmediaMediaProcessing\include,..\sipXtackLib\include"
 				PreprocessorDefinitions="_DEBUG;DONT_USE_LONG_LONG;WIN32;_CONSOLE;DISABLE_MEM_POOLS"
 				RuntimeLibrary="3"
 				UsePrecompiledHeader="0"

sipXmediaAdapterLib/sipXmediaMediaProcessing/include/CpPhoneMediaInterface.h
@@ -25,7 +25,7 @@
 #include <net/QoS.h>
 #include <sdp/SdpCodecList.h>
 #include "mi/CpMediaInterface.h"
-#include "include/MaNotfTranslatorDispatcher.h"
+#include "MaNotfTranslatorDispatcher.h"
 
 // DEFINES
 // MACROS

sipXmediaAdapterLib/sipXmediaMediaProcessing/include/CpTopologyGraphFactoryImpl.h
@@ -15,7 +15,7 @@
 
 // SYSTEM INCLUDES
 // APPLICATION INCLUDES
-#include <include/sipXmediaFactoryImpl.h>
+#include <sipXmediaFactoryImpl.h>
 
 // DEFINES
 // MACROS

sipXmediaAdapterLib/sipXmediaMediaProcessing/include/CpTopologyGraphInterface.h
@@ -19,9 +19,9 @@
 // SYSTEM INCLUDES
 // APPLICATION INCLUDES
 #include <sdp/SdpCodecList.h>
-#include <mi/CpMediaInterface.h>
+#include "mi/CpMediaInterface.h"
 #include <net/QoS.h>
-#include <MaNotfTranslatorDispatcher.h>
+#include "MaNotfTranslatorDispatcher.h"
 
 // DEFINES
 // MACROS

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/CpPhoneMediaInterface.cpp
@@ -23,8 +23,8 @@
 #include <os/OsNatDatagramSocket.h>
 #include <os/OsMulticastSocket.h>
 #include <os/OsProtectEventMgr.h>
-#include <include/CpPhoneMediaInterface.h>
-#include <mi/CpMediaInterfaceFactoryImpl.h>
+#include "CpPhoneMediaInterface.h"
+#include "mi/CpMediaInterfaceFactoryImpl.h"
 #include <mp/MpMediaTask.h>
 #include <mp/MpCallFlowGraph.h>
 #include <mp/MpStreamPlayer.h>

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/CpTopologyGraphFactoryImpl.cpp
@@ -29,9 +29,9 @@
 #include <mp/MprBufferRecorderConstructor.h>
 #include <mp/MprSplitterConstructor.h>
 #include <mp/MprNullAecConstructor.h>
-#include <include/CpTopologyGraphFactoryImpl.h>
-#include <mi/CpMediaInterfaceFactory.h>
-#include <include/CpTopologyGraphInterface.h>
+#include "CpTopologyGraphFactoryImpl.h"
+#include "mi/CpMediaInterfaceFactory.h"
+#include "CpTopologyGraphInterface.h"
 #include <os/OsSysLog.h>
 
 // REMOVE THIS when device enumerator/monitor would be implemented

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/CpTopologyGraphInterface.cpp
@@ -36,8 +36,8 @@
 #include <mp/dtmflib.h>
 #include <mp/MpMediaTask.h>
 #include <mp/MpCodecFactory.h>
-#include <include/CpTopologyGraphInterface.h>
-#include <include/CpTopologyGraphFactoryImpl.h>
+#include "CpTopologyGraphInterface.h"
+#include "CpTopologyGraphFactoryImpl.h"
 
 #if defined(_VXWORKS)
 #   include <socket.h>

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/MaNotfTranslatorDispatcher.cpp
@@ -16,7 +16,7 @@
 #include <mp/MprnProgressMsg.h>
 
 // APPLICATION INCLUDES
-#include "include/MaNotfTranslatorDispatcher.h"
+#include "MaNotfTranslatorDispatcher.h"
 #include "mi/MiNotification.h"
 #include "mi/MiDtmfNotf.h"
 #include "mi/MiProgressNotf.h"

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/Makefile.am
@@ -4,7 +4,7 @@ SUBDIRS = . test
 
 INCLUDES = \
 	-I$(top_srcdir)/interface \
-	-I$(top_srcdir)/sipXmediaMediaProcessing \
+	-I$(top_srcdir)/sipXmediaMediaProcessing/include \
 	-I$(top_srcdir)/../sipXcallLib/include
 
 lib_LTLIBRARIES = libsipXmediaProcessing.la

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/sipXmediaFactoryImpl.cpp
@@ -16,25 +16,25 @@
 #include <stdlib.h>
 
 // APPLICATION INCLUDES
-#include "os/OsConfigDb.h"
-#include "include/sipXmediaFactoryImpl.h"
-#include "include/CpPhoneMediaInterface.h"
-#include "sdp/SdpCodec.h"
-#include "mp/MpMediaTask.h"
-#include "mp/MpMisc.h"
-#include "mp/MpCodec.h"
-#include "mp/MpCallFlowGraph.h"
-#include "mp/dmaTask.h"
-#include "mp/MpCodecFactory.h"
-#include "sdp/SdpCodecList.h"
+#include <os/OsConfigDb.h>
+#include "sipXmediaFactoryImpl.h"
+#include "CpPhoneMediaInterface.h"
+#include <sdp/SdpCodec.h>
+#include <mp/MpMediaTask.h>
+#include <mp/MpMisc.h>
+#include <mp/MpCodec.h>
+#include <mp/MpCallFlowGraph.h>
+#include <mp/dmaTask.h>
+#include <mp/MpCodecFactory.h>
+#include <sdp/SdpCodecList.h>
 #include "mi/CpMediaInterfaceFactoryFactory.h"
 
 #ifdef INCLUDE_RTCP /* [ */
-#include "rtcp/RTCManager.h"
+#include <rtcp/RTCManager.h>
 #endif /* INCLUDE_RTCP ] */
 
 #ifdef ENABLE_TOPOLOGY_FLOWGRAPH_INTERFACE_FACTORY
-#include "mp/NetInTask.h"
+#include <mp/NetInTask.h>
 #endif
 
 

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/test/CpPhoneMediaInterfaceTest.cpp
@@ -12,12 +12,12 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/TestCase.h>
-#include <mi/CpMediaInterfaceFactory.h>
-#include <mi/CpMediaInterfaceFactoryFactory.h>
-#include <CpTopologyGraphInterface.h>
-#include <mi/CpMediaInterface.h>
-#include <mi/MiNotification.h>
-#include <mi/MiDtmfNotf.h>
+#include "mi/CpMediaInterfaceFactory.h"
+#include "mi/CpMediaInterfaceFactoryFactory.h"
+#include "CpTopologyGraphInterface.h"
+#include "mi/CpMediaInterface.h"
+#include "mi/MiNotification.h"
+#include "mi/MiDtmfNotf.h"
 #include <os/OsTask.h>
 #include <utl/UtlSList.h>
 #include <utl/UtlInt.h>

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/test/Makefile.am
@@ -2,7 +2,7 @@
 TESTS = testsuite sandbox
 
 INCLUDES = -I$(top_srcdir)/interface \
-           -I$(top_srcdir)/sipXmediaMediaProcessing
+           -I$(top_srcdir)/sipXmediaMediaProcessing/include
 
 check_PROGRAMS = testsuite sandbox
 

sipXmediaAdapterLib/sipXmediaMediaProcessing/src/test/PlayAudioOldNotificationTest.cpp
@@ -12,9 +12,9 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/TestCase.h>
-#include <mi/CpMediaInterfaceFactory.h>
-#include <mi/CpMediaInterfaceFactoryFactory.h>
-#include <mi/CpMediaInterface.h>
+#include "mi/CpMediaInterfaceFactory.h"
+#include "mi/CpMediaInterfaceFactoryFactory.h"
+#include "mi/CpMediaInterface.h"
 #include <os/OsTask.h>
 #include <utl/UtlSList.h>
 #include <utl/UtlInt.h>