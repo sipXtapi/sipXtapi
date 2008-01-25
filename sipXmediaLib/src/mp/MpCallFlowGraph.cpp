6b3bb3fc8bd99da0bfff974ae9ac103a71d2b6e9
Keith Kyzivat <kkyzivat@sipez.com>
2008-01-25 21:28:50 +0000

    Update copyright info missing from previous checkin.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10498 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/src/mp/MpCallFlowGraph.cpp
+++ sipXmediaLib/src/mp/MpCallFlowGraph.cpp
@@ -1,8 +1,8 @@
 //  
-// Copyright (C) 2006-2007 SIPez LLC. 
+// Copyright (C) 2006-2008 SIPez LLC. 
 // Licensed to SIPfoundry under a Contributor Agreement. 
 //
-// Copyright (C) 2004-2007 SIPfoundry Inc.
+// Copyright (C) 2004-2008 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
 //
 // Copyright (C) 2004-2006 Pingtel Corp.  All rights reserved.
