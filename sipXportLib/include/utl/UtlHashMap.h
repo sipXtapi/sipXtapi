08694d78961a8dfe44cef56058ce2d5e56f588d7
Alexander Chemeris <alexander.chemeris@gmail.com>
2008-02-06 20:04:25 +0000

    FIx style of Doxygen comments in UtlHashMap.h
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10582 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXportLib/include/utl/UtlHashMap.h
+++ sipXportLib/include/utl/UtlHashMap.h
@@ -1,5 +1,8 @@
+//  
+// Copyright (C) 2008 SIPez LLC. 
+// Licensed to SIPfoundry under a Contributor Agreement. 
 //
-// Copyright (C) 2004-2006 SIPfoundry Inc.
+// Copyright (C) 2004-2008 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
 //
 // Copyright (C) 2004-2006 Pingtel Corp.  All rights reserved.
@@ -31,10 +34,11 @@ class UtlContainable;
 class UtlPair;
 
 /**
- * UtlHashMap is a container object that allows you to store keys and 
- * values.  Key must be unique (testing for equality using the
- * UtlContainer::isEquals(...) method).
- */
+*  UtlHashMap is a container object that allows you to store keys and values.
+*
+*  Key must be unique (testing for equality using the UtlContainer::isEquals()
+*  method).
+*/
 class UtlHashMap : public UtlContainer
 {
 /* //////////////////////////// PUBLIC //////////////////////////////////// */
@@ -43,133 +47,109 @@ public:
 
 /* ============================ CREATORS ================================== */
 
-    /**
-     * Default Constructor
-     */
+      /// Default Constructor
     UtlHashMap();
 
-    /**
-     * Destructor
-     */
+      /// Destructor
     virtual ~UtlHashMap();
 
 /* ============================ MANIPULATORS ============================== */
 
-    /**
-     * Inserts a key and value pair into the hash map.
-     *
-     * If the inserted key is already in the table, this method 
-     * fails (returns NULL - note that this means if value is NULL,
-     * then you can't tell whether  there was an error or not).
-     * To replace the value for a given key, the old value must
-     * be Removed before the new value is inserted.
-     *
-     * @return the key on success, otherwise NULL
-     */
+      /// Inserts a key and value pair into the hash map.
     UtlContainable* insertKeyAndValue(UtlContainable* key, UtlContainable* value);
+      /**<
+      *  If the inserted key is already in the table, this method 
+      *  fails (returns NULL - note that this means if value is NULL,
+      *  then you can't tell whether  there was an error or not).
+      *  To replace the value for a given key, the old value must
+      *  be Removed before the new value is inserted.
+      *
+      *  @return the key on success, otherwise NULL
+      */
 
 
-    /**
-     * Inserts the designated containable object into the list
-     * with a NULL value (see note regarding use of NULL value
-     * in insertKeyAndValue).
-     * If there is an equal key in the UtlHashMap already,
-     * the insert will fail.
-     * 
-     * @return the object if successful, otherwise NULL
-     */
+      /// @brief Inserts the designated containable object into the list
+      /// with a NULL value.
     UtlContainable* insert(UtlContainable* obj);
+      /**<
+      *  If there is an equal key in the UtlHashMap already,
+      *  the insert will fail.
+      *
+      *  @see note regarding use of NULL value in insertKeyAndValue().
+      *
+      *  @return the object if successful, otherwise NULL
+      */
 
 
-    /**
-     * Remove the designated key and its associated value.
-     *
-     * @return the key or NULL if not found
-     */
+      /// Remove the designated key and its associated value.
     UtlContainable* remove(UtlContainable* key);
+      /**<
+      *  @return the key or NULL if not found
+      */
 
 
-    /**
-     * Remove the designated key and its associated value.
-     *
-     * @return the key or NULL if not found
-     */
+      /// Remove the designated key and its associated value.
     UtlContainable* removeReference(const UtlContainable* key);
+      /**<
+      *  @return the key or NULL if not found
+      */
     
-
-    /**
-     * Remove the designated key and its associated value.  The pointer of value
-     * is returned as part of the call if successful.
-     *
-     * @return the key or NULL if not found
-     */
+      /// Remove the designated key and its associated value.
     UtlContainable* removeKeyAndValue(const UtlContainable* key, UtlContainable*& value);
+      /**<
+      *  The pointer of value is returned as part of the call if successful.
+      *
+      *  @return the key or NULL if not found
+      */
 
 
-    /**
-     * Removes the designated key and its associated value from the map
-     * and frees the key and the value (if not NULL) by calling delete.
-     */ 
+      /// @brief Removes the designated key and its associated value from the map
+      /// and frees the key and the value (if not NULL) by calling delete.
     virtual UtlBoolean destroy(UtlContainable* key);    
 
 
-    /**
-     * Removes all elements from the hash map and deletes each element.
-     */
+      /// Removes all elements from the hash map and deletes each element.
     virtual void destroyAll();
 
 
-    /**
-     * Removes all elements from the hash map without deleting the elements
-     */
+      /// Removes all elements from the hash map without deleting the elements
     virtual void removeAll();
 
 /* ============================ ACCESSORS ================================= */
 
-    /**
-     * Return the value for a given key or NULL if not found.
-     */
+      /// Return the value for a given key or NULL if not found.
     UtlContainable* findValue(const UtlContainable* key) const;
 
 
-    /**
-     * Return the designated key if found otherwise NULL.
-     */
+      /// Return the designated key if found otherwise NULL.
     virtual UtlContainable* find(const UtlContainable* key) const;
 
 /* ============================ INQUIRY =================================== */
 
-    /**
-     * Return the total number of keys in the hash map
-     */
+      /// Return the total number of keys in the hash map
     size_t entries() const;
 
 
-    /**
-     * Return true if the hash map is empty (entries() == 0), otherwise false.
-     */
+      /// Return true if the hash map is empty (entries() == 0), otherwise false.
     UtlBoolean isEmpty() const;
 
 
-    /**
-     * Return true if the hash map includes an entry with the specified key.
-     */
+      /// Return true if the hash map includes an entry with the specified key.
     UtlBoolean contains(const UtlContainable* key) const;
   
 
-    /**
-     * Get the ContainableType for the hash bag as a contained object.
-     */
+      /// Get the ContainableType for the hash bag as a contained object.
     virtual UtlContainableType getContainableType() const;
 
-    /**
-     * Make a copy of all of the items BY POINTER in (*this) instance
-     * into the given map. It does not clear the given map. IF USING
-     * destroyAll call, be sure to call this on only ONE map instance.
-     */
+      /// @brief Make a copy of all of the items BY POINTER in (*this) instance
+      /// into the given map.
     void copyInto(UtlHashMap& map) const;
+      /**<
+      *  It does not clear the given map. IF USING destroyAll call, be sure
+      *  to call this on only ONE map instance.
+      */
 
-    /// The current number of buckets in the hash.
+      /// The current number of buckets in the hash.
     size_t numberOfBuckets() const
        {
           return NUM_HASHMAP_BUCKETS(mBucketBits);
@@ -182,25 +162,14 @@ protected:
 
     static const UtlContainable* INTERNAL_NULL;
 
-    /// If the Hash is too full, add additional buckets.
-    /**
-     * Assumes that the caller is holding the mContainerLock.
-     *
-     * This calls resize to actually do the resize if it is safe.
-     */
-    void resizeIfNeededAndSafe()
-       {
-          if (   ( mElements / NUM_HASHMAP_BUCKETS(mBucketBits) >= 3 ) // mean bucket 3 or more
-              && ( mIteratorList.isUnLinked() )   /* there are no iterators -
-                                                   * resizing moves elements to new buckets,
-                                                   * which could cause an iterator to miss some
-                                                   * and to return others more than once.
-                                                   */
-              )
-          {
-             resize();
-          }
-       }
+      /// If the Hash is too full, add additional buckets.
+    inline
+    void resizeIfNeededAndSafe();
+      /**<
+      * Assumes that the caller is holding the mContainerLock.
+      *
+      * This calls resize to actually do the resize if it is safe.
+      */
     
     size_t    mElements;   ///< number of UtlContainable objects in this UtlHashMap
     size_t    mBucketBits; ///< number of bits used to index the buckets
@@ -209,45 +178,58 @@ protected:
 /* //////////////////////////// PRIVATE /////////////////////////////////// */
 private:
 
-    /// Allocate additional buckets and redistribute existing contents.
+      /// Allocate additional buckets and redistribute existing contents.
     void resize();
-    /**
-     * This should only be called through resizeIfNeededAndSafe.
-     */          
+      /**<
+      *  This should only be called through resizeIfNeededAndSafe.
+      */          
 
-    /// Search for a given key value and return the the UtlPair and bucket for it.
-    bool lookup(const UtlContainable* key, ///< The key to locate.
-                UtlChain*&      bucket,    /**< The bucket list header in which it belongs.
-                                            *   This is set regardless of whether or not the
-                                            *   key was found in the table. */
-                UtlPair*&       pair       /**< If the key was found, the UtlPair for the entry.
-                                            *   If the key was not found, this is NULL. */
-                ) const;
-    /**<
-     * @return true if the key was found, and false if not.
-     */
+      /// Search for a given key value and return the the UtlPair and bucket for it.
+    bool lookup(const UtlContainable* key,
+                UtlChain*&      bucket,
+                UtlPair*&       pair) const;
+      /**<
+      *  @param[in]  key - The key to locate.
+      *  @param[out] bucket - The bucket list header in which it belongs.
+      *              This is set regardless of whether or not the
+      *              key was found in the table.
+      *  @param[out] pair - If the key was found, the UtlPair for the entry.
+      *              If the key was not found, this is NULL
+      *  @return true if the key was found, and false if not.
+      */
     
-    /// Insert a pair into a bucket.
-    void insert(UtlPair*        pair,   /**< The UtlPair for the entry - data, value, and hash
-                                         *   are already set. */
-                UtlChain*       bucket  ///< The bucket list header where the entry belongs.
-                );
+      /// Insert a pair into a bucket.
+    void insert(UtlPair*        pair,
+                UtlChain*       bucket);
+      /**<
+      *  @param[in] pair - The UtlPair for the entry - data, value, and hash
+      *             are already set.
+      *  @param[in] bucket - The bucket list header where the entry belongs.
+      */
 
-    /// Calculate the bucket number for a given hash.
+      /// Calculate the bucket number for a given hash.
     size_t bucketNumber(unsigned hash) const;
     
-    // no copy constructor is provided
+      /// No copy constructor is provided.
     UtlHashMap(UtlHashMap&);
 
-    /** Use copyInto instead */
+      /// Use copyInto instead
     UtlHashMap& operator=(const UtlHashMap&);
     
-    /**
-     * notifyIteratorsOfRemove - called before removing any entry from the UtlHashMap
-     */
+      /// Called before removing any entry from the UtlHashMap
     void notifyIteratorsOfRemove(const UtlPair* pair);
 };
 
+void UtlHashMap::resizeIfNeededAndSafe()
+{
+   // IF mean bucket 3 or more AND there are no iterators.
+   // Resizing moves elements to new buckets, which could cause an iterator
+   // to miss some and to return others more than once.
+   if (   ( mElements / NUM_HASHMAP_BUCKETS(mBucketBits) >= 3 ) 
+       && ( mIteratorList.isUnLinked() ))
+   {
+      resize();
+   }
+}
+
 #endif    // _UtlHashMap_h_
-
-
