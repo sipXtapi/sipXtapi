02cded825f5ea48eaeb0fc068d4cf04564dc7c5c
Alexander Chemeris <alexander.chemeris@gmail.com>
2008-09-08 09:27:09 +0000

    Reformat MpResourceTopology.h to follow style guidelines. No code changes.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/branches/sipXtapi@10795 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/include/mp/MpResourceTopology.h
+++ sipXmediaLib/include/mp/MpResourceTopology.h
@@ -83,83 +83,81 @@ public:
 
 /* ============================ CREATORS ================================== */
 
-    /** Constructor
-     */
+      /// Constructor
     MpResourceTopology();
 
-    /** Destructor
-     */
+      /// Destructor
     virtual ~MpResourceTopology();
 
 /* ============================ MANIPULATORS ============================== */
 
-    /// Add a new resource definition to the topology
-    /** 
-      * @param resourceType - type for MpResourceFactory construction
-      * @param resourceName - unique name for resource in this topology and
-      *        ultimately in flowgraph.
-      * @returns OS_NAME_IN_USE if resourceName already exists in this topology
-      */
-    OsStatus addResource(const UtlString& resourceType,
-                         const UtlString& resourceName);
+     /// Add a new resource definition to the topology
+   OsStatus addResource(const UtlString& resourceType,
+                        const UtlString& resourceName);
+     /**<
+     *  @param resourceType - type for MpResourceFactory construction
+     *  @param resourceName - unique name for resource in this topology and
+     *         ultimately in flowgraph.
+     *  @returns OS_NAME_IN_USE if resourceName already exists in this topology
+     */
 
-    /// Add a new connection definition to the topology
-    OsStatus addConnection(const UtlString& outputResourceName,
-                           int outputPortIndex,
-                           const UtlString& inputResourceName,
-                           int inputPortIndex);
+     /// Add a new connection definition to the topology
+   OsStatus addConnection(const UtlString& outputResourceName,
+                          int outputPortIndex,
+                          const UtlString& inputResourceName,
+                          int inputPortIndex);
 
-    /// Validate that the connections are connect the resources
-    /**
-      * a full topology MUST NOT have connections that reference resources
-      * outside this topology AND all resource MUST be connected to such that
-      * there are no dangling resources.  An incremental topology MAY have
-      * connections that reference resources outside of this topology AND
-      * all resources in this topology MUST have at least one connection.
-      *
-      * @param firstUnconnectedResourceName - first resource found to not
-      *        have any connections.
-      * @param firstDanglingResourceName - first resource found to not be
-      *        connected topologically to the rest of the resources.  The
-      *        can only be tested in full topologies.  We do not know the
-      *        pre-existing topology in the flowgraph for incremental topologies.
-      * @param allowExternalResources - FALSE = full topology, TRUE = increment
-      *        topology (see definitions at top of this class)
-      */
-    OsStatus validateConnections(UtlString& firstUnconnectedResourceName,
-                                 UtlString&  firstDanglingResourceName,
-                                 UtlBoolean allowExternalResources = FALSE) const;
+     /// Validate that the connections are connect the resources
+   OsStatus validateConnections(UtlString& firstUnconnectedResourceName,
+                                UtlString&  firstDanglingResourceName,
+                                UtlBoolean allowExternalResources = FALSE) const;
+     /**<
+     *  A full topology MUST NOT have connections that reference resources
+     *  outside this topology AND all resource MUST be connected to such that
+     *  there are no dangling resources.  An incremental topology MAY have
+     *  connections that reference resources outside of this topology AND
+     *  all resources in this topology MUST have at least one connection.
+     *
+     *  @param firstUnconnectedResourceName - first resource found to not
+     *         have any connections.
+     *  @param firstDanglingResourceName - first resource found to not be
+     *         connected topologically to the rest of the resources.  The
+     *         can only be tested in full topologies.  We do not know the
+     *         pre-existing topology in the flowgraph for incremental topologies.
+     *  @param allowExternalResources - FALSE = full topology, TRUE = increment
+     *         topology (see definitions at top of this class)
+     */
 
-    /// Validate that all the resource types in this topology have a constructor in the factory
-    OsStatus validateResourceTypes(MpResourceFactory& resourceFactory,
-                                   int& firstInvalidResourceIndex) const;
+     /// Validate that all the resource types in this topology have a constructor in the factory
+   OsStatus validateResourceTypes(MpResourceFactory& resourceFactory,
+                                  int& firstInvalidResourceIndex) const;
 
 
-    /// Utility to replace "%d" in resource name with an integer
-    static void replaceNumInName(UtlString& resourceName,
-                          int resourceNum);
+     /// Utility to replace "%d" in resource name with an integer
+   static void replaceNumInName(UtlString& resourceName,
+                                int resourceNum);
 
 /* ============================ ACCESSORS ================================= */
 
-    /// Get the resource definition indicated by the resourceIndex
-    OsStatus getResource(int resourceIndex,
-                         UtlString& resourceType,
-                         UtlString& resourceName) const;
+     /// Get the resource definition indicated by the resourceIndex
+   OsStatus getResource(int resourceIndex,
+                        UtlString& resourceType,
+                        UtlString& resourceName) const;
 
-    /// Get the connection definition indicated by the connectionIndex
-    OsStatus getConnection(int connectionIndex,
-                           UtlString& outputResourceName,
-                           int& outputPortIndex,
-                           UtlString& inputResourceName,
-                           int& inputPortIndex);
+     /// Get the connection definition indicated by the connectionIndex
+   OsStatus getConnection(int connectionIndex,
+                          UtlString& outputResourceName,
+                          int& outputPortIndex,
+                          UtlString& inputResourceName,
+                          int& inputPortIndex);
 
-    /// Get the next logical port, used when the same port number
-    /** 
-      * used when more than one connection should refer to the same
-      * port number, but the port number is not yet known (e.g. use
-      * the next available port).
-      */
-    int getNextLogicalPortNumber();
+     /// Get the next logical port, used when the same port number
+   int getNextLogicalPortNumber();
+     /**<
+     *  Used when more than one connection should refer to the same
+     *  port number, but the port number is not yet known (e.g. use
+     *  the next available port).
+     */
 
 /* ============================ INQUIRY =================================== */
 
@@ -169,22 +167,18 @@ protected:
 /* //////////////////////////// PRIVATE /////////////////////////////////// */
 private:
 
-    int findResourceConnections(const UtlString& resourceName,
-                                UtlContainer& connectionsToTraverse) const;
+   int mPriorLogicalPort;
+   UtlDList mResources;
+   UtlDList mConnections;
 
-    /** Disabled copy constructor
-     */
-    MpResourceTopology(const MpResourceTopology& rMpResourceTopology);
+   int findResourceConnections(const UtlString& resourceName,
+                               UtlContainer& connectionsToTraverse) const;
 
+     /// Disabled copy constructor
+   MpResourceTopology(const MpResourceTopology& rMpResourceTopology);
 
-    /** Disable assignment operator
-     */
-    MpResourceTopology& operator=(const MpResourceTopology& rhs);
-
-    
-    int mPriorLogicalPort;
-    UtlDList mResources;
-    UtlDList mConnections;
+     /// Disabled assignment operator
+   MpResourceTopology& operator=(const MpResourceTopology& rhs);
 };
 
 /* ============================ INLINE METHODS ============================ */
