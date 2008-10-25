a4142c3913410d21264d3635cfd7bea2ec653c80
Alexander Chemeris <alexander.chemeris@gmail.com>
2008-10-25 05:36:40 +0000

    Fix Linux build: OsTask::syslog() had wrong __attribute__ format.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@10862 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXportLib/include/os/OsTask.h
+++ sipXportLib/include/os/OsTask.h
@@ -168,7 +168,7 @@ public:
                                                   ...)
 #ifdef __GNUC__
        // with the -Wformat switch, this enables format string checking
-       __attribute__ ((format (printf, 3, 4)))
+       __attribute__ ((format(printf, 4, 5)))
 #endif
        ;
 
