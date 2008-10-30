b093eaed0697b656de95040d0f212746bf0c9a90
Alexander Chemeris <alexander.chemeris@gmail.com>
2008-10-30 03:34:48 +0000

    Checking in update_version.sh script.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@10863 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ update_version.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+MAJOR_VER=3
+MINOR_VER=2
+REV_NUM=0
+BUILD_NUM=0
+
+LIBRARIES="sipXcallLib sipXmediaAdapterLib sipXmediaLib sipXsdpLib sipXtackLib sipXportLib"
+MAILING_LIST="sipxtapi-dev@list.sipfoundry.org"
+
+echo "Setting sipX libs version to $MAJOR_VER.$MINOR_VER.$REV_NUM.$BUILD_NUM"
+
+# sipXtapi.h
+sed -i -e "s/\(#define\s\+SIPXTAPI_VERSION\s\+\)\".*\"/\1\"$MAJOR_VER.$MINOR_VER.$REV_NUM\"/g" sipXcallLib/include/tapi/sipXtapi.h
+sed -i -e "s/\(#define\s\+SIPXTAPI_BUILDNUMBER\s\+\)\".*\"/\1\"$BUILD_NUM\"/g" sipXcallLib/include/tapi/sipXtapi.h
+sed -i -e "s/\(#define\s\+SIPXTAPI_BUILD_WORD\s\+\)[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+/\1$MAJOR_VER,$MINOR_VER,$REV_NUM,$BUILD_NUM/g" sipXcallLib/include/tapi/sipXtapi.h
+sed -i -e "s/\(#define\s\+SIPXTAPI_FULL_VERSION\s\+\)\".*\"/\1\"$MAJOR_VER.$MINOR_VER.$REV_NUM.$BUILD_NUM\"/g" sipXcallLib/include/tapi/sipXtapi.h
+
+# configure.ac files
+for libdir in $LIBRARIES ; do
+	sed -i -e "s/AC_INIT(.*)/AC_INIT($libdir, $MAJOR_VER.$MINOR_VER, $MAILING_LIST/g" $libdir/configure.ac
+done
+
+# spec.in files
+for libdir in $LIBRARIES ; do
+	sed -i -e "s/Requires:\s\+\(sipx.*\lib\)\s\+>=\s\+[0-9]\+\(.[0-9]\+\)*/Requires: \1 >= $MAJOR_VER.$MINOR_VER/g" $libdir/sipx*.spec.in
+done
