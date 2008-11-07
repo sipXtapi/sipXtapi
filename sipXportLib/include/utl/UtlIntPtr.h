fd5eb2cc47fdd77ed26debfaf74ecdbed5c9c4d6
Alexander Chemeris <alexander.chemeris@gmail.com>
2008-11-07 16:57:21 +0000

    Adding UtlIntPtr class to store intptr_t in UTL containers.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@10905 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXportLib/include/utl/UtlIntPtr.h
@@ -0,0 +1,123 @@
+//
+// Copyright (C) 2004-2006 SIPfoundry Inc.
+// Licensed by SIPfoundry under the LGPL license.
+//
+// Copyright (C) 2004-2006 Pingtel Corp.  All rights reserved.
+// Licensed to SIPfoundry under a Contributor Agreement.
+//
+// $$
+///////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _UtlIntPtr_h_
+#define _UtlIntPtr_h_
+
+// SYSTEM INCLUDES
+// APPLICATION INCLUDES
+#include "utl/UtlDefs.h"
+#include "utl/UtlCopyableContainable.h"
+
+// DEFINES
+// MACROS
+// EXTERNAL FUNCTIONS
+// EXTERNAL VARIABLES
+// CONSTANTS
+// STRUCTS
+// TYPEDEFS
+// FORWARD DECLARATIONS
+
+/**
+ * UtlIntPtr is a UtlContainable wrapper for an intptr_t.
+ */
+class UtlIntPtr : public UtlCopyableContainable
+{
+/* //////////////////////////// PUBLIC //////////////////////////////////// */
+public:
+    static const UtlContainableType TYPE ;    /** < Class type used for runtime checking */ 
+
+/* ============================ CREATORS ================================== */
+
+    /**
+     * Constructor accepting an optional default value.
+     */
+    UtlIntPtr(intptr_t initialValue = 0) ;
+    UtlIntPtr(const UtlIntPtr& rhs) ;
+      
+    /**
+     * Destructor
+     */
+    virtual ~UtlIntPtr();
+
+    UtlCopyableContainable* clone() const;
+
+/* ============================ OPERATORS ============================== */
+
+    // Declare prefix and postfix increment operators.
+    UtlIntPtr& operator++();       // Prefix increment operator
+    UtlIntPtr operator++(int);     // Postfix increment operator
+
+    // Declare prefix and postfix decrement operators.
+    UtlIntPtr& operator--();       // Prefix decrement operator
+    UtlIntPtr operator--(int);     // Postfix decrement operator
+
+    UtlIntPtr& operator=(const UtlIntPtr& rhs); // assigment operator
+
+    // Conversion to int
+    operator intptr_t() { return mValue; }
+
+/* ============================ MANIPULATORS ============================== */
+
+    /**
+     * Set a new int value for this object.
+     *
+     * @returns the old value
+     */
+    intptr_t setValue(intptr_t iValue) ;
+
+/* ============================ ACCESSORS ================================= */
+
+    /**
+     * Get the int wrapped by this object.
+     */
+    intptr_t getValue() const ;    
+
+    /**
+     * Calculate a unique hash code for this object.  If the equals
+     * operator returns true for another object, then both of those
+     * objects must return the same hashcode.
+     */
+    virtual unsigned hash() const ;
+
+    /**
+     * Get the ContainableType for a UtlContainable derived class.
+     */
+    virtual UtlContainableType getContainableType() const;
+         
+/* ============================ INQUIRY =================================== */
+
+    /**
+     * Compare this object to another like-object.  Results for 
+     * comparing to a non-like object are undefined.
+     *
+     * @returns 0 if equal, < 0 if less then and >0 if greater.
+     */
+    virtual int compareTo(UtlContainable const *) const ;    
+
+    /**
+     * Test this object to another like-object for equality.  This method 
+     * returns false if unlike-objects are specified.
+     */
+    virtual UtlBoolean isEqual(UtlContainable const *) const ; 
+
+/* //////////////////////////// PROTECTED ///////////////////////////////// */
+protected:
+
+/* //////////////////////////// PRIVATE /////////////////////////////////// */
+private:
+    intptr_t mValue ;    ///< The int wrapped by this object 
+
+} ;
+
+/* ============================ INLINE METHODS ============================ */
+
+#endif    // _UtlIntPtr_h_
---
+++ sipXportLib/src/utl/UtlIntPtr.cpp
@@ -0,0 +1,148 @@
+//
+// Copyright (C) 2004-2006 SIPfoundry Inc.
+// Licensed by SIPfoundry under the LGPL license.
+//
+// Copyright (C) 2004-2006 Pingtel Corp.  All rights reserved.
+// Licensed to SIPfoundry under a Contributor Agreement.
+//
+// $$
+///////////////////////////////////////////////////////////////////////////////
+
+
+// SYSTEM INCLUDES
+#include <string.h>
+#include <limits.h>
+
+// APPLICATION INCLUDES
+#include "utl/UtlIntPtr.h"
+
+// EXTERNAL FUNCTIONS
+// EXTERNAL VARIABLES
+// CONSTANTS
+// STATIC VARIABLE INITIALIZATIONS
+UtlContainableType UtlIntPtr::TYPE = "UtlIntPtr";
+
+/* //////////////////////////// PUBLIC //////////////////////////////////// */
+
+/* ============================ CREATORS ================================== */
+
+// Constructor accepting an optional default value.
+UtlIntPtr::UtlIntPtr(intptr_t value) : mValue(value)
+{
+} 
+
+
+// Copy constructor
+UtlIntPtr::UtlIntPtr(const UtlIntPtr& rhs) : mValue(rhs.mValue)
+{
+}
+
+// Destructor
+UtlIntPtr::~UtlIntPtr()
+{
+}
+
+UtlCopyableContainable* UtlIntPtr::clone() const
+{
+   return new UtlIntPtr(*this); 
+}
+
+/* ============================ OPERATORS ============================== */
+
+// Prefix increment operator
+UtlIntPtr& UtlIntPtr::operator++() {
+    mValue++;
+    return *this;
+}
+
+// Postfix increment operator
+UtlIntPtr UtlIntPtr::operator++(int) {
+    UtlIntPtr temp = *this;
+    ++*this;
+    return temp;
+}
+
+// Prefix decrement operator
+UtlIntPtr& UtlIntPtr::operator--() {
+    mValue--;
+    return *this;
+}
+
+// Postfix decrement operator
+UtlIntPtr UtlIntPtr::operator--(int) {
+    UtlIntPtr temp = *this;
+    --*this;
+    return temp;
+}
+
+UtlIntPtr& UtlIntPtr::operator=(const UtlIntPtr& rhs)
+{
+   if (this == &rhs)            // handle the assignment to self case
+      return *this;
+
+   // Assign values
+   mValue = rhs.mValue;
+
+   return *this;
+}
+
+/* ============================ MANIPULATORS ============================== */
+
+intptr_t UtlIntPtr::setValue(intptr_t iValue)
+{
+    int iOldValue = mValue ;
+    mValue = iValue ;
+
+    return iOldValue ;
+}
+
+/* ============================ ACCESSORS ================================= */
+
+intptr_t UtlIntPtr::getValue() const 
+{
+    return mValue ; 
+}
+
+
+unsigned UtlIntPtr::hash() const
+{
+   return mValue ; 
+}
+
+
+UtlContainableType UtlIntPtr::getContainableType() const
+{
+    return UtlIntPtr::TYPE ;
+}
+
+/* ============================ INQUIRY =================================== */
+
+int UtlIntPtr::compareTo(UtlContainable const * inVal) const
+{
+   int result ; 
+   
+   if (inVal->isInstanceOf(UtlIntPtr::TYPE))
+    {
+        UtlIntPtr* temp = (UtlIntPtr*)inVal ; 
+        intptr_t inInt = temp->getValue() ; 
+        result = mValue - inInt ; 
+    }
+    else
+    {
+        result = INT_MAX ; 
+    }
+
+    return result ;
+}
+
+
+UtlBoolean UtlIntPtr::isEqual(UtlContainable const * inVal) const
+{
+    return (compareTo(inVal) == 0) ; 
+}
+
+/* //////////////////////////// PROTECTED ///////////////////////////////// */
+
+/* //////////////////////////// PRIVATE /////////////////////////////////// */
+
+/* ============================ FUNCTIONS ================================= */
