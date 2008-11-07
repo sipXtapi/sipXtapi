28ffd14511ca652dd1a0f4e47ec29a941ef7da7c
Alexander Chemeris <alexander.chemeris@gmail.com>
2008-11-07 17:36:55 +0000

    Fix compiler warning in UtlString.cpp.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@10909 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXportLib/src/utl/UtlString.cpp
+++ sipXportLib/src/utl/UtlString.cpp
@@ -729,9 +729,9 @@ size_t UtlString::capacity(size_t N)
         }
         else
         {
-            osPrintf("******** ERROR******* : UtlString::capacity failed (%d). Memory not allocated!\n", N);
+            osPrintf("******** ERROR******* : UtlString::capacity failed (%"PRIuMAX"). Memory not allocated!\n", N);
 #ifdef _VXWORKS
-            osPrintf("******** ERROR******* : Largest block = %d, requested size = %d\n",maxFreeBlockSize, N);
+            osPrintf("******** ERROR******* : Largest block = %"PRIuMAX", requested size = %"PRIuMAX"\n",maxFreeBlockSize, N);
 #endif
             newSize = 0;
         }
