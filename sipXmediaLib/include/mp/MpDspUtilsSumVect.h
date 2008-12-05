sipXtapi/sipXtapi

commit f430a59119beb28138200216ca92256c4c4e2c75

Use proper C99 integer types in dsp utils sumvect implementation - for maxabs and maximum implementation.
(VC6 was confused by this - specifying ambiguous reference to maximum)


git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11025 a612230a-c5fa-0310-af8b-88eea846685b

sipXmediaLib/include/mp/MpDspUtilsSumVect.h
@@ -195,21 +195,21 @@ OsStatus MpDspUtils::mulLinear(const int16_t *pSrc, float valStart, float valEnd
 
 int MpDspUtils::maxAbs(const int16_t *pSrc, int dataLength)
 {
-   int startValue = pSrc[0];
+   int16_t startValue = pSrc[0];
    if (startValue < 0)
       startValue = -startValue;
 
    for (int i = 1; i < dataLength; i++)
    {
       startValue = maximum(startValue, 
-         ((int)pSrc[i] > 0) ? (int)pSrc[i] : -((int)pSrc[i]));
+         ((int16_t)pSrc[i] > 0) ? (int16_t)pSrc[i] : -((int16_t)pSrc[i]));
    }
    return startValue;
 }
 
 int32_t MpDspUtils::maximum(const int32_t *pSrc, int dataLength)
 {
-   int val = pSrc[0];
+   int32_t val = pSrc[0];
    for (int i = 0; i < dataLength; i++)
       if (pSrc[i] > val) 
          val = pSrc[i];

commit 87d7ca81641ea4487572086d98950195317e24c0

Fix Linux breakage I introduced in r11025. While VC6 compiler was now happy - satisfied that the maximum() call was now unambiguous, Gnu GCC compiler now was confused and found ambiguity.
This now should remove all ambiguity and make everyone happy.
I think the problem lied in the negation -- the negation may have been taking
an int16_t and making it something else.




git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11028 a612230a-c5fa-0310-af8b-88eea846685b

sipXmediaLib/include/mp/MpDspUtilsSumVect.h
@@ -202,7 +202,7 @@ int MpDspUtils::maxAbs(const int16_t *pSrc, int dataLength)
    for (int i = 1; i < dataLength; i++)
    {
       startValue = maximum(startValue, 
-         ((int16_t)pSrc[i] > 0) ? (int16_t)pSrc[i] : -((int16_t)pSrc[i]));
+         (int16_t)( (pSrc[i] > 0) ? pSrc[i] : -(pSrc[i]) ));
    }
    return startValue;
 }