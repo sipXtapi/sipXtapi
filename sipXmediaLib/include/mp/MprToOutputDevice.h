a188f1bf934051d6dee316389d15b1bf7c34af01
Alexander Chemeris <alexander.chemeris@gmail.com>
2009-06-04 20:46:48 +0000

    sipXmediaLib: Add queue to MprToOutputDevice for use as AEC reference data.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11345 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/include/mp/MprToOutputDevice.h
+++ sipXmediaLib/include/mp/MprToOutputDevice.h
@@ -17,7 +17,9 @@
 
 // APPLICATION INCLUDES
 #include <mp/MpAudioResource.h>
+#include <mp/MpResourceMsg.h>
 #include <mp/MpResampler.h>
+#include <os/OsMsgPool.h>
 
 // DEFINES
 // MACROS
@@ -39,6 +41,11 @@ class MpOutputDeviceManager;
 *  responsibility to decide how current to keep (e.g. delay to prevent
 *  starvation) with the frames in the MpOutputDeviceManager for the specific
 *  device to which is sent output.
+*
+*  The MprToOutputDevice also maintains so called "copy queue" - a queue to
+*  which a copies of buffers are sent. Only copies of buffers accepted by
+*  the device are sent to the copy queue. This queue is usually used by echo
+*  canceler.
 */
 class MprToOutputDevice : public MpAudioResource
 {
@@ -64,11 +71,33 @@ public:
 ///@name Manipulators
 //@{
 
+     /// Send message to enable/disable copy queue.
+   static OsStatus enableCopyQ(const UtlString& namedResource, 
+                               OsMsgQ& fgQ,
+                               UtlBoolean enable);
+     /**<
+     *  @param[in] namedResource - the name of the resource to send a message to.
+     *  @param[in] fgQ - the queue of the flowgraph containing the resource which
+     *             the message is to be received by.
+     *  @param[in] enable - TRUE to enable copy queue, FALSE to disable it.
+     *  @returns the result of attempting to queue the message to this resource.
+     */
+
+     /// Send message to enable/disable copy queue.
+   OsStatus enableCopyQ(UtlBoolean enable);
+     /**<
+     *  @param[in] enable - TRUE to enable copy queue, FALSE to disable it.
+     *  @returns the result of attempting to queue the message to this resource.
+     */
+
 //@}
 
 /* ============================ ACCESSORS ================================= */
 ///@name Accessors
 //@{
+
+     /// Get pointer to the copy queue.
+   inline OsMsgQ *getCopyQ();
 
 //@}
 
@@ -85,6 +114,12 @@ protected:
 /* //////////////////////////// PRIVATE /////////////////////////////////// */
 private:
 
+   typedef enum
+   {
+      MPRM_ENABLE_COPY_QUEUE = MpResourceMsg::MPRM_EXTERNAL_MESSAGE_START,
+      MPRM_DISABLE_COPY_QUEUE
+   } AddlResMsgTypes;
+
    virtual UtlBoolean doProcessFrame(MpBufPtr inBufs[],
                                      MpBufPtr outBufs[],
                                      int inBufsSize,
@@ -100,6 +135,14 @@ private:
    MpOutputDeviceHandle mDeviceId;
    MpResamplerBase *mpResampler;
 
+   OsMsgQ mCopyQ;    ///< Queue for the copy of data sent to output device.
+                     ///< It is usually used by acoustic echo canceler.
+   OsMsgPool mCopyQPool; ///< Pool for messages to be sent to mCopyQ.
+   UtlBoolean mIsCopyQEnabled; ///< Should we copy data to mCopyQ or not?
+
+     /// @copydoc MpResource::handleMessage()
+   virtual UtlBoolean handleMessage(MpResourceMsg& rMsg);
+
      /// Copy constructor (not implemented for this class)
    MprToOutputDevice(const MprToOutputDevice& rMprToOutputDevice);
 
@@ -109,5 +152,10 @@ private:
 };
 
 /* ============================ INLINE METHODS ============================ */
+
+OsMsgQ *MprToOutputDevice::getCopyQ()
+{
+   return &mCopyQ;
+}
 
 #endif  // _MprToOutputDevice_h_
--- sipXmediaLib/src/mp/MprToOutputDevice.cpp
+++ sipXmediaLib/src/mp/MprToOutputDevice.cpp
@@ -20,6 +20,7 @@
 #include <mp/MprToOutputDevice.h>
 #include <mp/MpFlowGraphBase.h>
 #include <mp/MpMisc.h>
+#include <mp/MpBufferMsg.h>
 #include <mp/MpResampler.h>
 
 #ifdef RTL_ENABLED
@@ -33,6 +34,8 @@
 // EXTERNAL VARIABLES
 // CONSTANTS
 // STATIC VARIABLE INITIALIZATIONS
+static const UtlString classname="MprToOutputDevice";
+
 // DEFINES
 #define DEBUG_PRINT
 #undef  DEBUG_PRINT
@@ -61,6 +64,9 @@ MprToOutputDevice::MprToOutputDevice(const UtlString& rName,
 , mMixerBufferPosition(0)
 , mDeviceId(deviceId)
 , mpResampler(MpResamplerBase::createResampler(1, 8000, 8000))
+, mCopyQPool(rName+"-copy-pool", MpBufferMsg(MpBufferMsg::AUD_PLAY),
+             20, 0, 0, 1, OsMsgPool::SINGLE_CLIENT)
+, mIsCopyQEnabled(FALSE)
 {
 }
 
@@ -71,6 +77,24 @@ MprToOutputDevice::~MprToOutputDevice()
 }
 
 /* ============================ MANIPULATORS ============================== */
+
+OsStatus MprToOutputDevice::enableCopyQ(const UtlString& namedResource,
+                                        OsMsgQ& fgQ,
+                                        UtlBoolean enable)
+{
+   MpResourceMsg msg(enable?(MpResourceMsg::MpResourceMsgType)MPRM_ENABLE_COPY_QUEUE:
+                            (MpResourceMsg::MpResourceMsgType)MPRM_DISABLE_COPY_QUEUE,
+                     namedResource);
+   return fgQ.send(msg, sOperationQueueTimeout);
+}
+
+OsStatus MprToOutputDevice::enableCopyQ(UtlBoolean enable)
+{
+   MpResourceMsg msg(enable?(MpResourceMsg::MpResourceMsgType)MPRM_ENABLE_COPY_QUEUE:
+                            (MpResourceMsg::MpResourceMsgType)MPRM_DISABLE_COPY_QUEUE,
+                     getName());
+   return postMessage(msg);
+}
 
 /* ============================ ACCESSORS ================================= */
 
@@ -92,6 +116,8 @@ UtlBoolean MprToOutputDevice::doProcessFrame(MpBufPtr inBufs[],
    OsStatus status = OS_SUCCESS;
    // Create a bufPtr that points to the data we want to push to the device.
    MpAudioBufPtr inAudioBuffer = inBufs[0];
+   MpBufferMsg *pCopyMsg = NULL;
+   UtlBoolean isCopyMsgSet = FALSE;
 
    if (!isEnabled)
    {
@@ -106,18 +132,23 @@ UtlBoolean MprToOutputDevice::doProcessFrame(MpBufPtr inBufs[],
       return FALSE;
    }
 
+   if (mIsCopyQEnabled)
+   {
+      pCopyMsg = (MpBufferMsg*)mCopyQPool.findFreeMsg();
+   }
+
    // Milliseconds per frame:
    frameTimeInterval = samplesPerFrame * 1000 / samplesPerSecond;
-
-   // Get the name of the device we're writing to.
-   UtlString devName = "Unknown device";
-   mpOutputDeviceManager->getDeviceName(mDeviceId, devName);
 
    uint32_t devSampleRate = 0;
    OsStatus stat = mpOutputDeviceManager->getDeviceSamplesPerSec(mDeviceId, devSampleRate);
    assert(stat == OS_SUCCESS);
    if(stat != OS_SUCCESS)
    {
+      // Get the name of the device we're writing to.
+      UtlString devName = "Unknown device";
+      mpOutputDeviceManager->getDeviceName(mDeviceId, devName);
+
       OsSysLog::add(FAC_MP, PRI_ERR, "MprToOutputDevice::doProcessFrame "
          "- Couldn't get device sample rate from output device manager!  "
          "Device - \"%s\"", devName.data());
@@ -137,15 +168,21 @@ UtlBoolean MprToOutputDevice::doProcessFrame(MpBufPtr inBufs[],
       // leave the buffer pointer unchanged, and return OS_SUCCESS, which is what
       // we want.
       if(mpResampler->resampleBufPtr(inAudioBuffer, resampledBuffer, 
-            samplesPerSecond, devSampleRate, devName) != OS_SUCCESS)
+            samplesPerSecond, devSampleRate, classname) != OS_SUCCESS)
       {
-         // Error messages have already been logged. No need to do so here.
+         // Error messages have already been logged. No need to log it again here.
          return FALSE;
       }
 
       // If the resampled buffer is valid, then use it.
       if(resampledBuffer.isValid())
       {
+         if (pCopyMsg != NULL)
+         {
+            // If copy queue is activated save original buffer to it.
+            pCopyMsg->ownBuffer(inAudioBuffer);
+            isCopyMsgSet = TRUE;
+         }
          // To optimize for speed a bit, we use MpBufPtr's swap() method 
          // instead of assignment -- make sure we don't use resampledBuffer
          // after this!
@@ -214,7 +251,54 @@ UtlBoolean MprToOutputDevice::doProcessFrame(MpBufPtr inBufs[],
       mFrameTime += frameTimeInterval;
    }
 
+   // Send buffer to copy queue if copy queue is enabled
+   if (pCopyMsg != NULL)
+   {
+      if (status != OS_SUCCESS)
+      {
+         // Push failed - send empty buffer
+         inAudioBuffer.release();
+         pCopyMsg->ownBuffer(inAudioBuffer);
+      }
+      else if (!isCopyMsgSet)
+      {
+         // Push was successful and no resampling occurred.
+         // If there was resampling pCopyMsg already has original buffer in it.
+         pCopyMsg->ownBuffer(inAudioBuffer);
+      }
+      if (mCopyQ.send(*pCopyMsg, OsTime::NO_WAIT_TIME) != OS_SUCCESS)
+      {
+         OsSysLog::add(FAC_MP, PRI_WARNING,
+                       "Sending message to copy queue in %s failed!", getName().data());
+         pCopyMsg->releaseMsg();
+      }
+   }
+
    return (status == OS_SUCCESS);
+}
+
+UtlBoolean MprToOutputDevice::handleMessage(MpResourceMsg& rMsg)
+{
+   UtlBoolean msgHandled = FALSE;
+
+   switch (rMsg.getMsg()) 
+   {
+   case MPRM_ENABLE_COPY_QUEUE:
+      mIsCopyQEnabled = TRUE;
+      msgHandled = TRUE;
+      break;
+
+   case MPRM_DISABLE_COPY_QUEUE:
+      mIsCopyQEnabled = FALSE;
+      msgHandled = TRUE;
+      break;
+
+   default:
+      // If we don't handle the message here, let our parent try.
+      msgHandled = MpResource::handleMessage(rMsg); 
+      break;
+   }
+   return msgHandled;
 }
 
 /* ============================ FUNCTIONS ================================= */
