4e4de902bc6d53ad26e477e810658ac3307ea453
Dan Petrie <dpetrie@sipez.com>
2009-12-31 03:50:55 +0000

    Make file for sipXsdpLib for Android NDK
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11484 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXsdpLib/Android.mk
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2009 SIPfoundry Inc.
+# Licensed by SIPfoundry under the LGPL license.
+#
+# Copyright (C) 2009 SIPez LLC.
+# Licensed to SIPfoundry under a Contributor Agreement.
+#
+#
+#//////////////////////////////////////////////////////////////////////////
+#
+# Author: Dan Petrie (dpetrie AT SIPez DOT com)
+#
+#
+# This Makefile is for building sipXsdpLib as a part of Android NDK.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+# Set up the target identity.
+# LOCAL_MODULE/_CLASS are required for local-intermediates-dir to work.
+LOCAL_MODULE := libsipXsdp
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#intermediates := $(call local-intermediates-dir)
+
+LOCAL_SRC_FILES := \
+    src/sdp/Sdp.cpp \
+    src/sdp/SdpCandidatePair.cpp \
+    src/sdp/SdpCodecList.cpp \
+    src/sdp/SdpHelperResip.cpp \
+    src/sdp/SdpCandidate.cpp \
+    src/sdp/SdpCodec.cpp \
+    src/sdp/SdpDefaultCodecFactory.cpp \
+    src/sdp/SdpMediaLine.cpp
+
+LOCAL_CXXFLAGS += -D__pingtel_on_posix__ \
+                  -DANDROID \
+                  -DDEFINE_S_IREAD_IWRITE \
+                  -DSIPX_TMPDIR=\"/usr/var/tmp\" -DSIPX_CONFDIR=\"/etc/sipxpbx\"
+
+#ifeq ($(TARGET_ARCH),arm)
+#	LOCAL_CFLAGS += -DARMv5_ASM
+#endif
+
+#ifeq ($(TARGET_BUILD_TYPE),debug)
+#	LOCAL_CFLAGS += -DDEBUG
+#endif
+
+LOCAL_C_INCLUDES += \
+    $(SIPX_HOME)/libpcre \
+    $(SIPX_HOME)/sipXportLib/include \
+    $(SIPX_HOME)/sipXsdpLib/include
+
+LOCAL_SHARED_LIBRARIES := libpcre libsipXport
+
+#LOCAL_STATIC_LIBRARIES := libpcre
+
+LOCAL_LDLIBS += -lstdc++ -ldl
+
+include $(BUILD_SHARED_LIBRARY)
