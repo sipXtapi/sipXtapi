092ec24cbe9112e69e016b8f5d881f8020685151
Dan Petrie <dpetrie@sipez.com>
2010-01-15 15:52:59 +0000

    Created script to generate create build timestamp source files for Android
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11504 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ scripts/makeBuildTimestamps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SIPX_REV="3.3"
+SVN_REV=`svn info | grep Revision | awk '{print $2}'`
+
+LIB_NAMES="sipXportLib sipxSdpLib sipXtackLib sipXmediaLib sipXmediaAdapterLib sipXcallLib"
+
+for LIB_NAME in ${LIB_NAMES}
+do
+  if [ ! -d $LIB_NAME ]
+  then
+    echo Must be in sipX root directory containing: $LIB_NAME
+    exit 1
+  fi
+  LOWER_LIB_NAME=`echo ${LIB_NAME} | tr "[:upper:]" "[:lower:]"`
+  #echo svn: ${SVN_REV} sipX rev: ${SIPX_REV}
+  #echo Name: ${LIB_NAME} \(${LOWER_LIB_NAME}\)
+  if [ ! -f ${LIB_NAME}/config/${LOWER_LIB_NAME}-buildstamp.h ]
+  then
+    echo creating: ${LIB_NAME}/config/${LOWER_LIB_NAME}-buildstamp.h:
+    sed -e "s/@PACKAGE@/${LIB_NAME}/" ${LIB_NAME}/config/sipX-buildstamp.h.in | sed -e 's/sipX/SipX/' -e 's/Lib/lib/' > ${LIB_NAME}/config/${LOWER_LIB_NAME}-buildstamp.h
+  fi
+
+  echo creating: ${LIB_NAME}/config/${LOWER_LIB_NAME}-buildstamp.cpp
+  sed -e "s/@PACKAGE@/${LIB_NAME}/" ${LIB_NAME}/config/sipX-buildstamp.cpp.in | \
+    sed -e "s/@SIPX_BUILDSTAMP@/${SVN_REV}/" | \
+    sed -e "s/@VERSION@/${SIPX_REV}/" | \
+    sed -e 's/sipX/SipX/' -e 's/Lib/lib/' > ${LIB_NAME}/config/${LOWER_LIB_NAME}-buildstamp.cpp
+
+
+done
