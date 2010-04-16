5347a9c5cebe3af44ddb893995f1e7823f64ebe2
Alexander Chemeris <alexander.chemeris@gmail.com>
2010-04-16 13:34:49 +0000

    sipXmediaLib: Optimize Speex build on Android. Now Speex DSP utils work there.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11643 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/contrib/android/speex/Android.mk
+++ sipXmediaLib/contrib/android/speex/Android.mk
@@ -1,12 +1,40 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE:= libspeex
-LOCAL_ARM_MODE := arm
+# SPEEX CONFIG START
 
 SPEEX_LT_CURRENT := 6
 SPEEX_LT_REVISION := 0
 SPEEX_LT_AGE := 5
+
+SPEEX_CFLAGS += -DEXPORT="" -DUSE_ALLOCA
+SPEEX_CFLAGS += -fstrict-aliasing -fprefetch-loop-arrays
+#SPEEX_CFLAGS += -march=armv7-a -mtune=cortex-a8 -mfpu=vfpv3
+#SPEEX_CFLAGS += -march=armv7-a -mtune=cortex-a8 -mfpu=neon
+#SPEEX_CFLAGS += -D__ARM_ARCH_6__ -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__
+SPEEX_CFLAGS += -ftree-vectorize -ffast-math -mvectorize-with-neon-quad -mno-apcs-stack-check
+
+SPEEX_LDFLAGS = -no-undefined -version-info $(SPEEX_LT_CURRENT):$(SPEEX_LT_REVISION):$(SPEEX_LT_AGE)
+
+ifndef SPEEX_MATH
+  SPEEX_MATH := FIXED_POINT
+endif
+ifeq ($(SPEEX_MATH),FIXED_POINT)
+  SPEEX_CFLAGS += -DFIXED_POINT
+  ifndef SPEEX_FFT
+    SPEEX_FFT := KISS_FFT
+  endif
+else
+  SPEEX_CFLAGS += -DFLOATING_POINT
+  ifndef SPEEX_FFT
+    SPEEX_FFT := SMALLFT
+  endif
+endif
+
+# SPEEX CONFIG END
+
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libspeex
+LOCAL_ARM_MODE := arm
 
 LOCAL_SRC_FILES := libspeex/cb_search.c
 LOCAL_SRC_FILES += libspeex/exc_10_32_table.c
@@ -45,12 +73,8 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_PRELINK_MODULE := false
 
-#LOCAL_CFLAGS += -DFLOATING_POINT
-LOCAL_CFLAGS += -DFIXED_POINT
-LOCAL_CFLAGS += -DEXPORT="" -DUSE_SMALLFT
-LOCAL_CFLAGS+= -O3 -fstrict-aliasing -fprefetch-loop-arrays 
-
-LOCAL_LDFLAGS = -no-undefined -version-info $(SPEEX_LT_CURRENT):$(SPEEX_LT_REVISION):$(SPEEX_LT_AGE)
+LOCAL_CFLAGS += $(SPEEX_CFLAGS)
+LOCAL_LDFLAGS = $(SPEEX_LDFLAGS)
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -58,10 +82,6 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libspeexdsp
 LOCAL_ARM_MODE := arm
-
-SPEEX_LT_CURRENT := 6
-SPEEX_LT_REVISION := 0
-SPEEX_LT_AGE := 5
 
 LOCAL_SRC_FILES := libspeex/preprocess.c
 LOCAL_SRC_FILES += libspeex/jitter.c
@@ -72,24 +92,22 @@ LOCAL_SRC_FILES += libspeex/resample.c
 LOCAL_SRC_FILES += libspeex/buffer.c
 LOCAL_SRC_FILES += libspeex/scal.c
 
-#if BUILD_KISS_FFT
-#  FFTSRC=kiss_fft.c _kiss_fft_guts.h kiss_fft.h kiss_fftr.c kiss_fftr.h 
-#elif BUILD_SMALLFT
-LOCAL_SRC_FILES += libspeex/smallft.c
-#endif
-
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/include \
 	$(LOCAL_PATH)
 
+ifeq ($(SPEEX_FFT),KISS_FFT)
+  SPEEX_CFLAGS += -DUSE_KISS_FFT
+  LOCAL_SRC_FILES += libspeex/kiss_fft.c libspeex/kiss_fftr.c
+else ifeq ($(SPEEX_FFT),SMALLFT)
+  SPEEX_CFLAGS += -DUSE_SMALLFT
+  LOCAL_SRC_FILES += libspeex/smallft.c
+endif
+
 LOCAL_PRELINK_MODULE := false
 
-#LOCAL_CFLAGS += -DFLOATING_POINT
-LOCAL_CFLAGS += -DFIXED_POINT
-LOCAL_CFLAGS += -DEXPORT="" -DUSE_SMALLFT
-LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays 
-
-LOCAL_LDFLAGS = -no-undefined -version-info $(SPEEX_LT_CURRENT):$(SPEEX_LT_REVISION):$(SPEEX_LT_AGE)
+LOCAL_CFLAGS += $(SPEEX_CFLAGS)
+LOCAL_LDFLAGS = $(SPEEX_LDFLAGS)
 
 include $(BUILD_STATIC_LIBRARY)
 
