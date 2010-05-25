9b44f368f4ab67e0c992a9739379e5a23df54e0d
Alexander Chemeris <alexander.chemeris@gmail.com>
2010-05-25 19:11:25 +0000

    Tune compilation options in Application.mk as an attempt to make it working on G1, Eris and Moment phones.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11663 a612230a-c5fa-0310-af8b-88eea846685b

--- scripts/Application.mk
+++ scripts/Application.mk
@@ -28,6 +28,8 @@ APP_MODULES := $(SIPX_LIBS) \
    $(JNI_UNIT_LIBS) \
    $(UNIT_TESTS)
 
+#APP_DEBUG_FLAGS := -fno-omit-frame-pointer -fno-strict-aliasing -marm
+
 APP_PROJECT_PATH := $(call my-dir)/project
 #APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
 #APP_OPTIM        := release
@@ -39,11 +41,23 @@ APP_CFLAGS      := -D__pingtel_on_posix__ \
                    -DSIPX_CONFDIR=\"/etc/sipx\" \
                    -DTEST_DIR=\"/sdcard\" \
                    -include apps/sipxtapi/project/jni/sipXportLib/include/os/OsIntTypes.h \
-                   -O2 -g
+                   -O2 -g \
+                   $(APP_DEBUG_FLAGS)
 
 # Optimizations for ARM Cortex-A8, used in Motorola Droid/Milestone, HTC Nexus One, etc
+# This optimizations don't give any spped improvement over ARM11-optimized code, so we
+# don't use it by default.
 # This enables real FPU instructions too.
-APP_CFLAGS += -march=armv7-a -mtune=cortex-a8 -mfpu=neon
-APP_CFLAGS += -D__ARM_ARCH_6__ -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__
-APP_CFLAGS += -ftree-vectorize -ffast-math -mvectorize-with-neon-quad -mno-apcs-stack-check
+#APP_CFLAGS += -march=armv7-a -mtune=cortex-a8 -mfpu=neon
+#APP_CFLAGS += -D__ARM_ARCH_6__ -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__
+#APP_CFLAGS += -ftree-vectorize -ffast-math -mvectorize-with-neon-quad -mno-apcs-stack-check
 
+APP_CFLAGS_FPU = -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad 
+
+# Optimizations for ARMv11, used in HTC G1, HTC Eris, etc
+APP_CFLAGS += -march=armv6j -mtune=arm1136j-s
+APP_CFLAGS += -D__ARM_ARCH_6__
+APP_CFLAGS += -ffast-math -mno-apcs-stack-check
+
+# Enable real FPU instructions too.
+APP_CFLAGS += $(APP_CFLAGS_FPU)
