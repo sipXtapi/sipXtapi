62328846387607dce1aa674b957dd485394f3c4f
Dan Petrie <dpetrie@sipez.com>
2011-07-13 06:40:38 +0000

    Ported XmlRpcTest to portable unit test framework.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11737 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXtackLib/src/test/net/XmlRpcTest.cpp
+++ sipXtackLib/src/test/net/XmlRpcTest.cpp
@@ -1,4 +1,7 @@
 //
+// Copyright (C) 2010-2011 SIPez LLC.  All rights reserved.
+// Licensed to SIPfoundry under a Contributor Agreement.
+//
 // Copyright (C) 2004-2006 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
 //
@@ -8,9 +11,7 @@
 // $$
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/TestCase.h>
-#include <sipxunit/TestUtilities.h>
+#include <sipxunittests.h>
 
 #include <os/OsDefs.h>
 #include <utl/UtlInt.h>
@@ -148,7 +149,7 @@ private:
 /**
  * Unit test for XmlRpc
  */
-class XmlRpcTest : public CppUnit::TestCase
+class XmlRpcTest : public SIPX_UNIT_BASE_CLASS
 {
    CPPUNIT_TEST_SUITE(XmlRpcTest);
    CPPUNIT_TEST(testXmlRpcRequestCreation);
