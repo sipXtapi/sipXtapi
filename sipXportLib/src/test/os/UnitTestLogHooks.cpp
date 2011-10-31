bca07c0d3bcffa9a32cc7440f1ef965488f4d4ce
Dan Petrie <dpetrie@sipez.com>
2011-10-31 16:58:20 +0000

    Hook to create logs for each unit test class in portable unit test framework
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11759 a612230a-c5fa-0310-af8b-88eea846685b

---
+++ sipXportLib/src/test/os/UnitTestLogHooks.cpp
@@ -0,0 +1,48 @@
+//
+// Copyright (C) 2007-2011 SIPez LLC  All rights reserved.
+//
+//
+// $$
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include <os/OsIntTypes.h>
+
+#include <os/OsSysLog.h>
+#include <os/OsFileSystem.h>
+
+#include <SipxPortUnitTestEnvironment.h>
+
+#define LOG_DIR "./"
+
+void beginLogHook(const char* testClassName)
+{
+    UtlString testLogFilename(LOG_DIR);
+    testLogFilename.append(testClassName);
+    testLogFilename.append(".log");
+
+    OsSysLog::initialize(0, "UnitTest");
+    OsSysLog::setLoggingPriority(PRI_DEBUG);
+
+    // Remove old log before starting test
+    OsFileSystem::remove(testLogFilename, FALSE, TRUE);
+
+    OsSysLog::setOutputFile(0, testLogFilename);
+}
+
+void endLogHook(const char* testClassName)
+{
+    OsSysLog::flush();
+}
+
+
+static int initLogHooks()
+{
+    SipxPortUnitTestEnvironment::setLogHookBegin(beginLogHook);
+    SipxPortUnitTestEnvironment::setLogHookEnd(endLogHook);
+
+    return(0);
+}
+
+int sLogHookFoo = initLogHooks();
+
