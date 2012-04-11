dce2c0074eb3208da68350aa377149c01119a8fb
Dan Petrie <dpetrie@sipez.com>
2012-04-11 15:20:23 +0000

    Copy Record-Route fields from INVITEs in siptest simulator. Also fixed indentation
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11910 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXtackLib/siptest/src/siptest/CommandProcessor.cpp
+++ sipXtackLib/siptest/src/siptest/CommandProcessor.cpp
@@ -1,9 +1,8 @@
 //
+// Copyright (C) 2007-2012 SIPez LLC.  All rights reserved.
+//
 // Copyright (C) 2004-2007 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
-//
-// Copyright (C) 2007 SIPez LLC. 
-// Licensed to SIPfoundry under a Contributor Agreement. 
 //
 // Copyright (C) 2004-2006 Pingtel Corp.  All rights reserved.
 // Licensed to SIPfoundry under a Contributor Agreement.
