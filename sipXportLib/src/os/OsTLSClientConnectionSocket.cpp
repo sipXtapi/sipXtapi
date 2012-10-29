8f5aabdcd05091fbef0ff7bd0ac516ea40084100
Dan Petrie <dpetrie@sipez.com>
2012-10-29 10:55:48 +0000

    Removed superfluous setting of socket descriptor after close
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@11986 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXportLib/src/os/OsTLSClientConnectionSocket.cpp
+++ sipXportLib/src/os/OsTLSClientConnectionSocket.cpp
@@ -1,4 +1,6 @@
 //
+// Copyright (C) 2006-2012 SIPez LLC.  All rights reserved.
+//
 // Copyright (C) 2004-2006 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
 //
@@ -249,7 +251,6 @@ void OsTLSClientConnectionSocket::NSSInitSocket(int socket, long timeoutInSecs, 
             {
                 mIsConnected = FALSE;
                 OsConnectionSocket::close();
-                socketDescriptor = OS_INVALID_SOCKET_DESCRIPTOR;          
             }
         }
 TlsFailure:
