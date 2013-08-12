c08e85411704de3c2c0d5570868cf556f0aad6ef
Dan Petrie <dpetrie@sipez.com>
2013-08-12 20:18:12 +0000

    Added include to fix windows build for MpAudioUtils.cpp
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@12136 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/src/mp/MpAudioUtils.cpp
+++ sipXmediaLib/src/mp/MpAudioUtils.cpp
@@ -1,6 +1,5 @@
 //  
-// Copyright (C) 2006-2010 SIPez LLC. 
-// Licensed to SIPfoundry under a Contributor Agreement. 
+// Copyright (C) 2006-2013 SIPez LLC.  All rights reserved.
 //
 // Copyright (C) 2004-2007 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
@@ -35,6 +34,7 @@
 #include "mp/MpTypes.h"
 #include "mp/MpAudioUtils.h"
 #include "mp/StreamHttpDataSource.h"
+#include <mp/MpAudioWaveFileRead.h>
 
 #ifndef DISABLE_STREAM_PLAYER // [
 #  include <os/fstream>
