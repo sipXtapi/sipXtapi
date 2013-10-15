85bc76f1ad99af68aedb42869d12865ae8989c26
Dan Petrie <dpetrie@sipez.com>
2013-10-15 23:27:41 +0000

    Fixed use of OsTaskId type.  Compile error on Android NDK 8.
    git-svn-id: http://scm.sipfoundry.org/rep/sipX/main@12168 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaAdapterLib/sipXmediaMediaProcessing/src/test/CpPhoneMediaInterfaceTest.cpp
+++ sipXmediaAdapterLib/sipXmediaMediaProcessing/src/test/CpPhoneMediaInterfaceTest.cpp
@@ -1,6 +1,5 @@
 // 
-// Copyright (C) 2005-2010 SIPez LLC
-// Licensed to SIPfoundry under a Contributor Agreement.
+// Copyright (C) 2005-2013 SIPez LLC.  All rights reserved.
 //
 // Copyright (C) 2005-2009 SIPfoundry Inc.
 // Licensed by SIPfoundry under the LGPL license.
@@ -466,7 +465,7 @@ class CpPhoneMediaInterfaceTest : public SIPX_UNIT_BASE_CLASS
 
         mediaInterface->giveFocus() ;
 
-        int taskId;
+        OsTaskId_t taskId;
         OsTask::getCurrentTaskId(taskId);
 
         // Record the entire "call" - all connections.
@@ -696,7 +695,7 @@ class CpPhoneMediaInterfaceTest : public SIPX_UNIT_BASE_CLASS
 
         mediaInterface->giveFocus() ;
 
-        int taskId;
+        OsTaskId_t taskId;
         OsTask::getCurrentTaskId(taskId);
 
         // Record the entire "call" - all connections.
