0bdaafcec044b8a626ac30e0267c53f069c6a62f
Dan Petrie <dpetrie@sipez.com>
2017-10-02 15:00:45 +0000

    Make MpidAlsa destructor virtual as well
    git-svn-id: https://sipxsvn.sipez.com/rep/sipX/main@12483 a612230a-c5fa-0310-af8b-88eea846685b

--- sipXmediaLib/include/mp/MpidAlsa.h
+++ sipXmediaLib/include/mp/MpidAlsa.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2007-2015 SIPez LLC.  All rights reserved.
+// Copyright (C) 2007-2017 SIPez LLC.  All rights reserved.
 //
 // $$
 ///////////////////////////////////////////////////////////////////////////////
@@ -51,7 +51,7 @@ public:
      */
 
      /// @brief Destructor
-   ~MpidAlsa();
+   virtual ~MpidAlsa();
 
 //@}
 
